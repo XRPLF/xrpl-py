67daf2d4b301a234eef88a176bf86aab8c355d1f
Mayukha Vadari <mvadari@ripple.com>
2021-01-08 13:46:28 -0800

    improve naming + commenting

--- xrpl/addresscodec/codec.py
+++ xrpl/addresscodec/codec.py
@@ -3,22 +3,25 @@ import base58
 import struct
 
 # base58 encodings: https://xrpl.org/base58-encodings.html
-ACCOUNT_ID = 0 # Account address (20 bytes)
-ACCOUNT_PUBLIC_KEY = 0x23 # Account public key (33 bytes)
-FAMILY_SEED = 0x21 # 33; Seed value (for secret keys) (16 bytes)
-NODE_PUBLIC = 0x1C # 28; Validation public key (33 bytes)
+ACCOUNT_ID_PREFIX = 0x0 # Account address (20 bytes)
+ACCOUNT_PUBLIC_KEY_PREFIX = 0x23 # value is 35; Account public key (33 bytes)
+FAMILY_SEED_PREFIX = 0x21 # value is 33; Seed value (for secret keys) (16 bytes)
+NODE_PUBLIC_PREFIX = 0x1C # value is 28; Validation public key (33 bytes)
 
-ED25519_SEED = [0x01, 0xE1, 0x4B] # [1, 225, 75]
+ED25519_SEED_PREFIX = [0x01, 0xE1, 0x4B] # [1, 225, 75]
 
 XRPL_ALPHABET = b'rpshnaf39wBUDNEGHJKLM4PQRST7VWXYZ2bcdeCg65jkm8oFqi1tuvAxyz'
 
-def encode(bytestring, versions, expected_length):
+def encode(bytestring, prefixes, expected_length):
     """
-    Returns the base58 encoding of the bytestring, with the given data version type and 
+    bytestring: bytes
+    prefixes: list of ints (each int < 256)
+    expected_length: int
+    Returns the base58 encoding of the bytestring, with the given data prefix (which indicates type) and 
     while ensuring the bytestring is the expected length. 
     """
     if expected_length and len(bytestring) != expected_length:
         raise Exception('unexpected_payload_length: len(bytestring) does not match expected_length. Ensure that the bytes are a bytestring.')
-    encoded_versions = bytes(versions).ljust(1, b'\0')
-    payload = encoded_versions + bytestring
+    encoded_prefixes = bytes(prefixes).ljust(1, b'\0') # convert prefix ints to bytes
+    payload = encoded_prefixes + bytestring
     return base58.b58encode_check(payload, alphabet=XRPL_ALPHABET).decode("utf-8")
