5a6a6f23b849f6a17d62cabaad0154e35200b43f
Greg Weisbrod <greg.weisbrod@gmail.com>
2021-01-07 20:03:39 -0500

    add basic setup.py

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # xrpl-py
 
-## Running Tests
+## Contributing 
+### Installing Dependencies
+```bash
+python setup.py develop
+```
+
+### Running Tests
 ```bash
 python -m unittest discover tests
 ```
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name="xrpl",
+        version="0.1",
+        description="Foundational XRPL ledger tools",
+        url="https://github.com/xpring-eng/xrpl-py",
+        # seriously - TODO
+        license="TODO",
+        packages=["xrpl"],
+        install_requires=[
+            "ECPy",
+        ],
+        zip_safe=False)
