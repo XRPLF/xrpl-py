ea342eb5cac9686a0f47b8e7c8ee67a1ff82171e
Mayukha Vadari <mvadari@ripple.com>
2021-01-11 12:01:57 -0800

    rename param

--- xrpl/addresscodec/codec.py
+++ xrpl/addresscodec/codec.py
@@ -13,16 +13,16 @@ ED25519_SEED_PREFIX = [0x01, 0xE1, 0x4B] # [1, 225, 75]
 
 XRPL_ALPHABET = b'rpshnaf39wBUDNEGHJKLM4PQRST7VWXYZ2bcdeCg65jkm8oFqi1tuvAxyz'
 
-def encode(bytestring, prefixes, expected_length):
+def encode(bytestring, prefix, expected_length):
     """
     bytestring: bytes
-    prefixes: list of ints (each int < 256)
+    prefix: list of ints (each int < 256)
     expected_length: int
     Returns the base58 encoding of the bytestring, with the given data prefix (which indicates type) and 
     while ensuring the bytestring is the expected length. 
     """
     if expected_length and len(bytestring) != expected_length:
         raise XRPLAddressCodecException('unexpected_payload_length: len(bytestring) does not match expected_length. Ensure that the bytes are a bytestring.')
-    encoded_prefixes = bytes(prefixes)
-    payload = encoded_prefixes + bytestring
+    encoded_prefix = bytes(prefix)
+    payload = encoded_prefix + bytestring
     return base58.b58encode_check(payload, alphabet=XRPL_ALPHABET).decode("utf-8")
