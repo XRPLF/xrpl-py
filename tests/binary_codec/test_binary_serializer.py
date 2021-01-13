0d52487a6467aaaa80af9df3dce683418d7ed67a
Amie Corso <acorso@ripple.com>
2021-01-13 06:57:02 -0800

    encode_variable_length test

--- tests/binary_codec/test_binary_serializer.py
+++ tests/binary_codec/test_binary_serializer.py
@@ -8,4 +8,23 @@ class TestBinarySerializer(unittest.TestCase):
         self.binary_serializer = binary_codec.BinarySerializer()
 
     def test_encode_variable_length_prefix(self):
-        pass
+        # 0 - 192, 193 - 12480, 12481 - 918744
+        length_cases = [100, 1000, 20000]
+        for case in length_cases:
+            prefix = self.binary_serializer.encode_variable_length_prefix(case)
+            decoded_length = calculate_length_from_prefix(prefix)
+            self.assertEqual(case, decoded_length)
+
+
+def calculate_length_from_prefix(length_prefix_bytes):
+    """
+    A test utility to calculate the length specified by the given length_prefix, as described
+    in https://xrpl.org/serialization.html#length-prefixing.
+    """
+    if len(length_prefix_bytes) == 1:
+        return length_prefix_bytes[0]
+    elif len(length_prefix_bytes) == 2:
+        return 193 + ((length_prefix_bytes[0] - 193) * 256) + length_prefix_bytes[1]
+    elif len(length_prefix_bytes) == 3:
+        return 12481 + ((length_prefix_bytes[0] - 241) * 65536) + (length_prefix_bytes[1] * 256) + length_prefix_bytes[2]
+    raise Exception("Length prefix must contain between 1 and 3 bytes.")
