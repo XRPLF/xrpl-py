f5549f1add01284fd44e959c56b268a78e4e3f97
Amie Corso <acorso@ripple.com>
2021-01-13 08:54:43 -0800

    fixtures infrastructure

---
+++ tests/binary_codec/fixtures/field_test.py
@@ -0,0 +1,7 @@
+class FieldTest:
+    def __init__(self, type_name, name, nth_of_type, type, expected_hex):
+        self.type_name = type_name
+        self.name = name
+        self.nth_of_type = nth_of_type
+        self.type = type
+        self.expected_hex = expected_hex
