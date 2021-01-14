9385cefbec4c55db3d7ce817ff9b64831d4fa15a
Ted Kalaw <ted.kalaw@gmail.com>
2021-01-14 12:35:55 -0600

    Fixes lint violations in binary_codec (#34)
    TSIA. Super close to being able to land CI.

--- xrpl/binary_codec/definitions/field_info.py
+++ xrpl/binary_codec/definitions/field_info.py
@@ -2,13 +2,19 @@ class FieldInfo:
     """
     Model object for field info metadata from the "fields" section of definitions.json.
     """
-    def __init__(self, nth, is_variable_length_encoded, is_serialized, is_signing_field, type):
+
+    def __init__(
+        self, nth, is_variable_length_encoded, is_serialized, is_signing_field, type
+    ):
         """
         :param nth: The field code -- sort order position for fields of the same type.
-        :param is_variable_length_encoded: Whether the serialized length of this field varies.
-        :param is_serialized: If the field is presented in binary serialized representation.
+        :param is_variable_length_encoded: Whether the serialized length of this field
+        varies.
+        :param is_serialized: If the field is presented in binary serialized
+        representation.
         :param is_signing_field: If the field should be included in signed transactions.
-        :param type: The name of this field's serialization type, e.g. UInt32, AccountID, etc.
+        :param type: The name of this field's serialization type,
+        e.g. UInt32, AccountID, etc.
         """
         self.nth = nth
         self.is_variable_length_encoded = is_variable_length_encoded
