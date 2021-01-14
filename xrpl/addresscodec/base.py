69be72d58339e399e8f499a309f3137954bed168
Mayukha Vadari <mvadari@ripple.com>
2021-01-13 17:15:31 -0800

    first implementation try

--- xrpl/addresscodec/base.py
+++ xrpl/addresscodec/base.py
@@ -25,3 +25,32 @@ def encode_xaddress(account_id, tag, test):
     bytestring += encoded_tag
 
     return base58.b58encode_check(bytestring, alphabet=XRPL_ALPHABET).decode("utf-8")
+
+def decode_xaddress(xaddress):
+    decoded = base58.b58decode_check(xaddress)
+    is_test = _is_test_address(decoded)
+    classic_address = decoded[2:22]
+    tag = _get_tag_from_buffer(decoded)
+
+    return (classic_address, tag, is_test)
+
+def _is_test_address(buffer):
+    decoded_prefix = buffer[:2]
+    if PREFIX_BYTES_MAIN == decoded_prefix:
+        return False
+    elif PREFIX_BYTES_TEST == decoded_prefix:
+        return True
+    else:
+        raise XRPLAddressCodecException('Invalid X-Address: bad prefix')
+
+def _get_tag_from_buffer(buffer):
+    flag = buffer[22]
+    if flag >= 2:
+        raise XRPLAddressCodecException('Unsupported X-Address')
+    if flag == 1: # Little-endian to big-endian
+        return buffer[23] + buffer[24] * 0x100 + buffer[25] * 0x10000 + buffer[26] * 0x1000000
+    if flag != 0:
+        raise XRPLAddressCodecException('Flag must be zero to indicate no tag')
+    if bytes.fromhex('0000000000000000') != buffer[23:23+8]:
+        raise XRPLAddressCodecException('Remaining bytes must be zero')
+    return None
