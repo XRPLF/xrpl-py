e4519c0d129f40c48596dc20b8598906ee3817f6
Greg Weisbrod <ledhed2222@users.noreply.github.com>
2021-01-13 19:02:59 -0500

    specify desired development python version using pyenv (#28)

--- tests/keypairs/test_base.py
+++ tests/keypairs/test_base.py
@@ -1,18 +1,29 @@
-import unittest
-import hashlib
+from unittest import TestCase
+from unittest.mock import patch
+from xrpl import keypairs, addresscodec
 
-from xrpl import keypairs
+RANDOM_BYTES = b",\x1f\xd4\xca'\xdf\x0b\xe7\x9c\x8b\xfeC\t\xdfX\x8e"
 
 
-class TestBase(unittest.TestCase):
-    def test_hash_uses_sha512(self):
-        message = "hi"
-        hasher = hashlib.sha512()
-        hasher.update(bytes(message, "UTF-8"))
-        sha512_expectation = hasher.digest()
-        output = keypairs.hash(message)
-        self.assertEqual(output, sha512_expectation[: len(output)])
+@patch("xrpl.addresscodec.encode_seed", autospec=True)
+class TestGenerateSeed(TestCase):
+    @patch("random.randbytes", autospec=True, return_value=RANDOM_BYTES)
+    def test_no_params(self, _randbytes, encode_seed):
+        keypairs.generate_seed()
+        encode_seed.assert_called_once_with(
+            RANDOM_BYTES,
+            addresscodec.ED25519,
+        )
 
-    def test_hash_length_is_capped(self):
-        output = keypairs.hash("hi")
-        self.assertEqual(len(output), 32)
+    def test_valid_entropy(self, encode_seed):
+        entropy = "0123456789012345"
+        keypairs.generate_seed(entropy)
+        encode_seed.assert_called_once_with(
+            entropy,
+            addresscodec.ED25519,
+        )
+
+    def test_invalid_entropy(self, _encode_seed):
+        entropy = "0"
+        with self.assertRaises(keypairs.exceptions.XRPLKeypairsException):
+            keypairs.generate_seed(entropy)
