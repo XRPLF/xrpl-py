309e525b99a0c3ff3db1811d3c3a6d834b9a4ed8
Mayukha Vadari <mvadari@ripple.com>
2021-01-15 11:01:08 -0800

    Implements `decode_xaddress` in `addresscodec` (#30)
    * start encoding
    
    * first crack at implementation
    
    * add test cases, reorg code, fix bugs
    
    * first implementation try
    
    * add docstring
    
    * add tests, fix bugs
    
    * rename base -> main
    
    * more renaming
    
    * add docstrings
    
    * improve docstrings
    
    * respond to comments
    
    * make code more readable
    
    * add helpful comments
    
    * remove unused file

--- xrpl/addresscodec/main.py
+++ xrpl/addresscodec/main.py
@@ -7,6 +7,12 @@ MAX_32_BIT_UNSIGNED_INT = 4294967295
 PREFIX_BYTES_MAIN = bytes([0x05, 0x44])  # 5, 68
 PREFIX_BYTES_TEST = bytes([0x04, 0x93])  # 4, 147
 
+# To better understand the cryptographic details, visit
+# https://github.com/xrp-community/standards-drafts/issues/6
+
+# General format of an X-Address:
+# [← 2 byte prefix →|← 160 bits of account ID →|← 8 bits of flags →|← 64 bits of tag →]
+
 
 def encode_xaddress(classic_address_bytes, tag, test):
     """
@@ -14,7 +20,7 @@ def encode_xaddress(classic_address_bytes, tag, test):
     tag: int, the destination tag
     test: boolean, whether it is the test network or not (aka the main network)
 
-    Returns the X-address representation of the data
+    Returns the X-Address representation of the data
     """
     if len(classic_address_bytes) != 20:
         raise XRPLAddressCodecException("Account ID must be 20 bytes")
@@ -44,3 +50,55 @@ def encode_xaddress(classic_address_bytes, tag, test):
     bytestring += encoded_tag
 
     return base58.b58encode_check(bytestring, alphabet=XRPL_ALPHABET).decode("utf-8")
+
+
+def decode_xaddress(xaddress):
+    """
+    xaddress: string, a base58-encoded X-Address.
+
+    Returns:
+        classic_address: the byte-encoded classic address
+        tag: the destination tag
+        is_test: whether the address is on the test network
+    """
+    decoded = base58.b58decode_check(
+        xaddress, alphabet=XRPL_ALPHABET
+    )  # convert b58 to bytes
+    is_test = _is_test_address(decoded[:2])
+    classic_address = decoded[2:22]
+    tag = _get_tag_from_buffer(decoded[22:])  # extracts the destination tag
+
+    return (classic_address, tag, is_test)
+
+
+def _is_test_address(prefix):
+    """
+    prefix: the first 2 bytes of an X-Address
+
+    Returns whether a decoded X-Address is a test address.
+    """
+    if PREFIX_BYTES_MAIN == prefix:
+        return False
+    if PREFIX_BYTES_TEST == prefix:
+        return True
+    raise XRPLAddressCodecException("Invalid X-Address: bad prefix")
+
+
+def _get_tag_from_buffer(buffer):
+    """
+    buffer: bytes
+
+    Returns the destination tag extracted from the suffix of the X-Address.
+    """
+    flag = buffer[0]
+    if flag >= 2:
+        raise XRPLAddressCodecException("Unsupported X-Address")
+    if flag == 1:  # Little-endian to big-endian
+        return (
+            buffer[1] + buffer[2] * 0x100 + buffer[3] * 0x10000 + buffer[4] * 0x1000000
+        )  # inverse of what happens in encode
+    if flag != 0:
+        raise XRPLAddressCodecException("Flag must be zero to indicate no tag")
+    if bytes.fromhex("0000000000000000") != buffer[1:9]:
+        raise XRPLAddressCodecException("Remaining bytes must be zero")
+    return None
