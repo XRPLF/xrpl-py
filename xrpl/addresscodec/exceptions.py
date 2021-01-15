371864a8be78ba4f9d8c022e71efe5ff61b46da3
Ted Kalaw <ted.kalaw@gmail.com>
2021-01-15 12:04:55 -0600

    Fixes addresscodec/exceptions.py

--- xrpl/addresscodec/exceptions.py
+++ xrpl/addresscodec/exceptions.py
@@ -1,6 +1,7 @@
+"""TODO: D100 Missing docstring in public module."""
+
+
 class XRPLAddressCodecException(Exception):
-    """
-    General XRPL Address Codec Exception
-    """
+    """General XRPL Address Codec Exception."""
 
     pass
