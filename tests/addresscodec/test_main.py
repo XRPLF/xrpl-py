00790245f09110df630db90cf500475e56323a11
Mayukha Vadari <mvadari@ripple.com>
2021-01-14 16:00:18 -0800

    respond to comments

--- tests/addresscodec/test_main.py
+++ tests/addresscodec/test_main.py
@@ -1,14 +1,23 @@
 import unittest
 
 from xrpl import addresscodec
-from .test_main_testcases import testcases
+from .test_main_test_cases import test_cases
+
 
 class TestMain(unittest.TestCase):
     def test_encode_xaddress(self):
-        for testcase in testcases:
-            classic_address, tag, expected_main_xaddress, expected_test_xaddress = testcase
-            decoded_classic_address = addresscodec.decode_classic_address(classic_address)
-            
+        # TODO: make these separate tests somehow
+        for test_case in test_cases:
+            (
+                classic_address,
+                tag,
+                expected_main_xaddress,
+                expected_test_xaddress,
+            ) = test_case
+            decoded_classic_address = addresscodec.decode_classic_address(
+                classic_address
+            )
+
             # test
             xaddress = addresscodec.encode_xaddress(decoded_classic_address, tag, True)
             self.assertEqual(xaddress, expected_test_xaddress)
