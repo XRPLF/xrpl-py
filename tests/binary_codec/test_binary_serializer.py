48730860d91bb817724354dd02d6334a3f577dca
Amie Corso <acorso@ripple.com>
2021-01-15 09:50:32 -0800

    fix linting errors

--- tests/binary_codec/test_binary_serializer.py
+++ tests/binary_codec/test_binary_serializer.py
@@ -18,13 +18,18 @@ class TestBinarySerializer(unittest.TestCase):
 
 def calculate_length_from_prefix(length_prefix_bytes):
     """
-    A test utility to calculate the length specified by the given length_prefix, as described
-    in https://xrpl.org/serialization.html#length-prefixing.
+    A test utility to calculate the length specified by the given length_prefix,
+    as described in https://xrpl.org/serialization.html#length-prefixing.
     """
     if len(length_prefix_bytes) == 1:
         return length_prefix_bytes[0]
     elif len(length_prefix_bytes) == 2:
         return 193 + ((length_prefix_bytes[0] - 193) * 256) + length_prefix_bytes[1]
     elif len(length_prefix_bytes) == 3:
-        return 12481 + ((length_prefix_bytes[0] - 241) * 65536) + (length_prefix_bytes[1] * 256) + length_prefix_bytes[2]
+        return (
+            12481
+            + ((length_prefix_bytes[0] - 241) * 65536)
+            + (length_prefix_bytes[1] * 256)
+            + length_prefix_bytes[2]
+        )
     raise Exception("Length prefix must contain between 1 and 3 bytes.")
