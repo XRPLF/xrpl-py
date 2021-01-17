5310781bc03d9b7149b02f6954bf8965f37ad0d9
Amie Corso <acorso@ripple.com>
2021-01-17 07:51:22 -0800

    noqa

--- xrpl/binary_codec/__init__.py
+++ xrpl/binary_codec/__init__.py
@@ -1,7 +1,7 @@
 from xrpl.binary_codec.definitions import *  # noqa F401
 from xrpl.binary_codec.field_id_codec import encode, decode  # noqa F401
 from xrpl.binary_codec.binary_wrappers import (  # noqa F401
-    BinarySerializer,  # noqa F401
-    BinaryParser,  # noqa F401
+    BinarySerializer,
+    BinaryParser,
 )
 from xrpl.binary_codec.exceptions import XRPLBinaryCodecException  # noqa F401
