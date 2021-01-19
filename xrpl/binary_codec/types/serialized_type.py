64cf7bf27eebe44ed64de5a0c2ba90b44e56dae6
Amie Corso <acorso@ripple.com>
2021-01-19 09:39:50 -0800

    docstring TODOs

--- xrpl/binary_codec/types/serialized_type.py
+++ xrpl/binary_codec/types/serialized_type.py
@@ -1,18 +1,22 @@
+"""TODO: D100 Missing docstring in public module."""
 import abc
 
 
 class SerializedType:
-    """ The base class for all binary codec types. """
+    """The base class for all binary codec types."""
 
     def __init__(self):
+        """TODO: D107 Missing docstring in __init__."""
         self.bytes = bytes()
 
     @abc.abstractmethod
     def from_parser(self, parser, length_hint=None):
+        """TODO: D102 Missing docstring in public method."""
         raise NotImplementedError("SerializedType.from_parser not implemented.")
 
     @abc.abstractmethod
     def from_value(self, value):
+        """TODO: D102 Missing docstring in public method."""
         raise NotImplementedError("SerializedType.from_value not implemented.")
 
     def to_byte_sink(self, bytesink):
@@ -23,9 +27,7 @@ class SerializedType:
         bytesink.append(self.bytes)
 
     def to_bytes(self):
-        """
-        Get the bytes representation of a SerializedType.
-        """
+        """Get the bytes representation of a SerializedType."""
         return self.bytes
 
     def to_json(self):
@@ -36,13 +38,9 @@ class SerializedType:
         return self.to_hex()
 
     def to_string(self):
-        """
-        Returns the hex string representation of self.bytes.
-        """
+        """Returns the hex string representation of self.bytes."""
         return self.to_hex()
 
     def to_hex(self):
-        """
-        Get the hex representation of a SerializedType's bytes.
-        """
+        """Get the hex representation of a SerializedType's bytes."""
         return self.bytes.hex()
