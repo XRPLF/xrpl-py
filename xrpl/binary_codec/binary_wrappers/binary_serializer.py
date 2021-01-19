f0a8d00981a39d9481d117f53dd5ec484882c376
Amie Corso <acorso@ripple.com>
2021-01-19 08:28:43 -0800

    debug tests

--- xrpl/binary_codec/binary_wrappers/binary_serializer.py
+++ xrpl/binary_codec/binary_wrappers/binary_serializer.py
@@ -9,6 +9,41 @@ MAX_SECOND_BYTE_VALUE = 240
 MAX_LENGTH_VALUE = 918744
 
 
+def _encode_variable_length_prefix(length):
+    """
+    Helper function for length-prefixed fields including Blob types
+    and some AccountID types. Calculates the prefix of variable length bytes.
+
+    The length of the prefix is 1-3 bytes depending on the length of the contents:
+    Content length <= 192 bytes: prefix is 1 byte
+    192 bytes < Content length <= 12480 bytes: prefix is 2 bytes
+    12480 bytes < Content length <= 918744 bytes: prefix is 3 bytes
+
+    `See Length Prefixing <https://xrpl.org/serialization.html#length-prefixing>`_
+    """
+    if length <= MAX_SINGLE_BYTE_LENGTH:
+        return length.to_bytes(1, byteorder="big", signed=False)
+    if length < MAX_DOUBLE_BYTE_LENGTH:
+        length -= MAX_SINGLE_BYTE_LENGTH + 1
+        byte1 = ((length >> 8) + (MAX_SINGLE_BYTE_LENGTH + 1)).to_bytes(
+            1, byteorder="big", signed=False
+        )
+        byte2 = (length & 0xFF).to_bytes(1, byteorder="big", signed=False)
+        return byte1 + byte2
+    if length <= MAX_LENGTH_VALUE:
+        length -= MAX_DOUBLE_BYTE_LENGTH
+        byte1 = ((MAX_SECOND_BYTE_VALUE + 1) + (length >> 16)).to_bytes(
+            1, byteorder="big", signed=False
+        )
+        byte2 = ((length >> 8) & 0xFF).to_bytes(1, byteorder="big", signed=False)
+        byte3 = (length & 0xFF).to_bytes(1, byteorder="big", signed=False)
+        return byte1 + byte2 + byte3
+
+    raise ValueError(
+        "VariableLength field must be <= {} bytes long".format(MAX_LENGTH_VALUE)
+    )
+
+
 class BinarySerializer:
     """
     Serializes JSON to XRPL binary format.
@@ -17,54 +52,20 @@ class BinarySerializer:
     def __init__(self):
         self.bytesink = bytes()
 
-    def put(self, hex_bytes):
-        self.bytesink.append(bytes.fromhex(hex_bytes))
-
-    def write(self, byte_array):
-        self.bytesink.append(byte_array)
-
-    def encode_variable_length_prefix(self, length):
-        """
-        Helper function for length-prefixed fields including Blob types
-        and some AccountID types. Calculates the prefix of variable length bytes.
-
-        The length of the prefix is 1-3 bytes depending on the length of the contents:
-        Content length <= 192 bytes: prefix is 1 byte
-        192 bytes < Content length <= 12480 bytes: prefix is 2 bytes
-        12480 bytes < Content length <= 918744 bytes: prefix is 3 bytes
-
-        `See Length Prefixing <https://xrpl.org/serialization.html#length-prefixing>`_
-        """
-        if length <= MAX_SINGLE_BYTE_LENGTH:
-            return length.to_bytes(1, byteorder="big", signed=False)
-        if length < MAX_DOUBLE_BYTE_LENGTH:
-            length -= MAX_SINGLE_BYTE_LENGTH + 1
-            byte1 = ((length >> 8) + (MAX_SINGLE_BYTE_LENGTH + 1)).to_bytes(
-                1, byteorder="big", signed=False
-            )
-            byte2 = (length & 0xFF).to_bytes(1, byteorder="big", signed=False)
-            return byte1 + byte2
-        if length <= MAX_LENGTH_VALUE:
-            length -= MAX_DOUBLE_BYTE_LENGTH
-            byte1 = ((MAX_SECOND_BYTE_VALUE + 1) + (length >> 16)).to_bytes(
-                1, byteorder="big", signed=False
-            )
-            byte2 = ((length >> 8) & 0xFF).to_bytes(1, byteorder="big", signed=False)
-            byte3 = (length & 0xFF).to_bytes(1, byteorder="big", signed=False)
-            return byte1 + byte2 + byte3
-
-        raise ValueError(
-            "VariableLength field must be <= {} bytes long".format(MAX_LENGTH_VALUE)
-        )
+    def put(self, bytes_object):
+        self.bytesink += bytes_object
 
     # TODO: this method depends on the SerializedType class.
+    # Complete when SerializedType is implemented
     def write_length_encoded(self, value):
         """
         Write a variable length encoded value to the BinarySerializer.
         """
-        pass
+        length_prefix = _encode_variable_length_prefix(value)
+        self.bytesink += length_prefix
 
     # TODO: this method depends on the SerializedType and FieldInstance classes.
+    # Complete when both classes are implemented
     def write_field_and_value(self, field, value):
         """
         Write field and value to the buffer.
