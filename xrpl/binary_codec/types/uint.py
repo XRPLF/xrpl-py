4c12eb558aa51952ffd2439280d12f51214970dc
Mayukha Vadari <mvadari@ripple.com>
2021-01-20 18:36:20 -0500

    docstrings

--- xrpl/binary_codec/types/uint.py
+++ xrpl/binary_codec/types/uint.py
@@ -1,30 +1,40 @@
-"""TODO"""
+"""Base class for serializing and deserializing unsigned integers."""
 from abc import ABC, abstractmethod
 
 
 class UInt(ABC):
+    """Base class for serializing and deserializing unsigned integers."""
+
     def __init__(self, uint_bytes):
+        """Construct a new UInt type from a `bytes` value."""
         self.bytes = uint_bytes
 
     def __eq__(self, other):
+        """Determine whether two UInt objects are equal."""
         return self.value == other.value
 
     def __ne__(self, other):
+        """Determine whether two UInt objects are unequal."""
         return self.value != other.value
 
     def __lt__(self, other):
+        """Determine whether one UInt object is less than another."""
         return self.value < other.value
 
     def __le__(self, other):
+        """Determine whether one UInt object is less than or equal to another."""
         return self.value <= other.value
 
     def __gt__(self, other):
+        """Determine whether one UInt object is greater than another."""
         return self.value > other.value
 
     def __ge__(self, other):
+        """Determine whether one UInt object is greater than or equal to another."""
         return self.value >= other.value
 
     def to_json(self):
+        """Convert a UInt object to JSON."""
         if isinstance(self.value, (int, float)):
             return self.value
         else:
@@ -33,4 +43,5 @@ class UInt(ABC):
     @property
     @abstractmethod
     def value(self):
+        """Get the value of the UInt represented by `this.bytes`."""
         raise NotImplementedError("UInt.value not implemented.")
