370da8334a6a1117365be112f5a01571fbc13f82
Amie Corso <acorso@ripple.com>
2021-01-21 08:38:32 -0800

    classmethod constructors

--- xrpl/binary_codec/types/hash.py
+++ xrpl/binary_codec/types/hash.py
@@ -1,35 +1,42 @@
 """Base class for XRPL Hash types."""
+from abc import ABC
 from xrpl.binary_codec.types.serialized_type import SerializedType
 from xrpl.binary_codec.binary_wrappers.binary_parser import BinaryParser
 from xrpl.binary_codec.exceptions import XRPLBinaryCodecException
 
 
-class Hash(SerializedType):
-    """Base class for XRPL Hash types."""
+class Hash(SerializedType, ABC):
+    """
+    Base class for XRPL Hash types.
 
-    def __init__(self, buffer: bytes, width: int):
+    Attributes:
+        width:  The length of this hash in bytes.
+    """
+
+    width: int
+
+    def __init__(self, buffer: bytes):
         """
         Construct a Hash.
 
         :param buffer: The byte buffer that will be used to store
-                        the serialized version of this field.
-        :param width: The length of the hash in bytes.
+                        the serialized encoding of this field.
         """
-        super().__init__(buffer)
-        if len(buffer) != width:
+        if len(buffer) != self.width:
             raise XRPLBinaryCodecException("Invalid hash length {}".format(len(buffer)))
-        self.width = width
+        super().__init__(buffer)
 
     def __str__(self):
         """Returns a hex-encoded string representation of the bytes buffer."""
         return self.to_hex()
 
-    # TODO: what is the right way to do this??
-    def from_value(self, value):
+    @classmethod
+    def from_value(cls, value: str):
         """Construct a Hash object from a hex string."""
-        return super().from_parser(BinaryParser(value), self.width)
+        return cls.from_parser(BinaryParser(value), cls.width)
 
-    # TODO: same here??  what does this look like in python?
-    def from_parser(self, parser, length_hint: int = None):
+    @classmethod
+    def from_parser(cls, parser, length_hint: int = None):
         """Construct a Hash object from an existing BinaryParser."""
-        return self.__init__(parser.read())
+        num_bytes = length_hint if length_hint is not None else cls.width
+        return cls.__init__(parser.read(num_bytes))
