43168cbeb56918f24ab35fa470e846a25c06778c
Mayukha Vadari <mvadari@ripple.com>
2021-01-21 12:51:27 -0500

    add docstrings

--- xrpl/binary_codec/types/uint8.py
+++ xrpl/binary_codec/types/uint8.py
@@ -9,18 +9,22 @@ class UInt8(UInt):
     """Derived UInt class for serializing/deserializing 8 bit UInt."""
 
     def __init__(self, uint_bytes):
+        """Construct a new UInt8 type from a `bytes` value."""
         super().__init__(uint_bytes)
 
     @property
     def value(self):
+        """Get the value of the UInt8 represented by `this.bytes`."""
         pass
 
-    @staticmethod
+    @classmethod
     def from_parser(parser):
+        """Construct a new UInt8 type from a binary parser."""
         return UInt8(parser.read(_width))
 
-    @staticmethod
+    @classmethod
     def from_value(value):
+        """Construct a new UInt8 type from a number."""
         if isinstance(value, UInt8):
             return value
 
