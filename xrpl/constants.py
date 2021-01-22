c98150500c1c4d09571e4bd92a5b3d5175aa42b7
Greg Weisbrod <greg.weisbrod@gmail.com>
2021-01-22 01:22:58 -0500

    clean up enum syntax

--- xrpl/constants.py
+++ xrpl/constants.py
@@ -1,9 +1,4 @@
 """Collection of public constants for XRPL."""
 from enum import Enum
 
-
-class CryptoAlgorithm(Enum):
-    """Represents the supported cryptography algorithms."""
-
-    ED25519 = 0
-    SECP256K1 = 1
+CryptoAlgorithm = Enum("CryptoAlgorithm", "ED25519 SECP256K1")
