f7fc33b4abc4aa28787bbf368235b6b8d2f86dfe
Amie Corso <acorso@ripple.com>
2021-01-21 19:25:59 -0800

    Hash128 class

---
+++ xrpl/binary_codec/types/hash128.py
@@ -0,0 +1,34 @@
+"""A hash field with a width of 128 bits (16 bytes).
+`See Hash Fields <https://xrpl.org/serialization.html#hash-fields>`_
+"""
+from xrpl.binary_codec.binary_wrappers.binary_parser import BinaryParser
+from xrpl.binary_codec.types.hash import Hash
+
+
+class Hash128(Hash):
+    """
+    A hash field with a width of 128 bits (16 bytes).
+    `See Hash Fields <https://xrpl.org/serialization.html#hash-fields>`_
+
+
+    Attributes:
+        width: The length of this hash in bytes.
+    """
+
+    width = 16
+
+    def __init__(self, buffer: bytes = None):
+        """Construct a Hash128."""
+        buffer = buffer if buffer is not None else bytes(20)
+        super().__init__(buffer)
+
+    @classmethod
+    def from_value(cls, value: str):
+        """Construct a Hash128 object from a hex string."""
+        return cls(bytes.fromhex(value))
+
+    @classmethod
+    def from_parser(cls, parser: BinaryParser, length_hint: int = None):
+        """Construct a Hash128 object from an existing BinaryParser."""
+        num_bytes = length_hint if length_hint is not None else cls.width
+        return cls(parser.read(num_bytes))
