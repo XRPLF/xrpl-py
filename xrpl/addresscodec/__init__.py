b247e370f72d26b4e63fb24cb1aa384402ff0450
Ted Kalaw <ted.kalaw@gmail.com>
2021-01-22 13:28:50 -0600

    Re-orders all

--- xrpl/addresscodec/__init__.py
+++ xrpl/addresscodec/__init__.py
@@ -15,13 +15,13 @@ from .main import *  # noqa: F401 F403
 from .utils import *  # noqa: F401 F403
 
 __all__ = [
-    "encode_seed",
-    "decode_seed",
-    "encode_classic_address",
-    "decode_classic_address",
-    "encode_node_public_key",
-    "decode_node_public_key",
-    "encode_account_public_key",
     "decode_account_public_key",
+    "decode_classic_address",
+    "decode_node_public_key",
+    "decode_seed",
+    "encode_seed",
+    "encode_account_public_key",
+    "encode_classic_address",
+    "encode_node_public_key",
     "is_valid_classic_address",
 ]
