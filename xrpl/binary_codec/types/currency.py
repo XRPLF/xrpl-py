c5448b90230194165925bccdb2e135f866588e77
Amie Corso <acorso@ripple.com>
2021-01-23 13:44:19 -0800

    complete initial impl Currency + tests

--- xrpl/binary_codec/types/currency.py
+++ xrpl/binary_codec/types/currency.py
@@ -10,25 +10,28 @@ _ISO_REGEX = "^[A-Z0-9]{3}$"
 _HEX_REGEX = "^[A-F0-9]{40}$"
 
 
-def is_iso_code(value: str) -> bool:
+def _is_iso_code(value: str) -> bool:
     """Tests if value is a valid 3-char iso code."""
     pattern = re.compile(_ISO_REGEX)
-    return pattern.match(value)
+    return bool(pattern.fullmatch(value))
 
 
-def is_hex(value: str) -> bool:
+def _is_hex(value: str) -> bool:
     """Tests if value is a valid 40-char hex string."""
     pattern = re.compile(_HEX_REGEX)
-    return pattern.match(value)
+    return bool(pattern.fullmatch(value))
 
 
-def iso_to_bytes(iso: str) -> bytes:
+def _iso_to_bytes(iso: str) -> bytes:
     """
     Convert an ISO code to a 160-bit (20 byte) encoded representation.
 
     See "Currency codes" subheading in
     `Amount Fields <https://xrpl.org/serialization.html#amount-fields>`_
     """
+    if not _is_iso_code(iso):
+        raise XRPLBinaryCodecException("Invalid ISO code: {}".format(iso))
+
     if iso == "XRP":
         # This code (160 bit all zeroes) is used to indicate XRP in
         # rare cases where a field must specify a currency code for XRP.
@@ -45,91 +48,58 @@ def iso_to_bytes(iso: str) -> bytes:
     return bytes(12) + iso_bytes + bytes(5)
 
 
-def is_string_representation(value: str) -> bool:
-    """Tests if value is a valid string representation of a currency."""
-    return is_iso_code(value) or is_hex(value)
-
-
-def is_bytes_array(buffer: bytes) -> bool:
-    """Tests if a bytes object is a valid representation of a currency."""
-    return len(buffer) == 20
-
-
-# TODO: the type of param `value` is (bytes | str)... how to represent this?
-def is_valid_representation(value) -> bool:
-    """Ensures that a value is a valid representation of a currency."""
-    if type(value) == "bytes":
-        is_bytes_array(value)
-    else:
-        is_string_representation(value)
-
-
-def bytes_from_representation(value: str) -> bytes:
-    """Generate bytes from a string or buffer representation of a currency."""
-    if not is_valid_representation(value):
-        raise XRPLBinaryCodecException(
-            "Unsupported Currency representation: {}".format(value)
-        )
-    if len(value) == 3:
-        return iso_to_bytes(value)
-    return bytes.fromhex(value)
-
-
 class Currency(Hash160):
     """
     Defines how to encode and decode currency codes in issued currency amounts.
     `Amount fields <https://xrpl.org/serialization.html#amount-fields>`_
-    """
 
-    # static readonly XRP = new Currency(Buffer.alloc(20));
-    # private readonly _iso?: string;
-    # private readonly _isNative: boolean;
+    Attributes:
+        buffer: The byte encoding of this currency.
+        _iso: The three-character ISO currency code if standard format, else None.
+        _is_native: True if the currency code is "XRP"
+    """
 
     def __init__(self, buffer: bytes = None) -> None:
         """Construct a Currency."""
         if buffer is not None:
-            self.buffer = buffer
+            super().__init__(buffer)
         else:
-            self.buffer = bytes(20)
+            super().__init__(bytes(20))
 
-        #        let onlyISO = true;
-        #
-        # const bytes = this.bytes;
-        # const code = this.bytes.slice(12, 15);
-        # const iso = code.toString();
-        #
-        # for (let i = bytes.length - 1; i >= 0; i--) {
-        #   if (bytes[i] !== 0 && !(i === 12 || i === 13 || i === 14)) {
-        #     onlyISO = false;
-        #     break;
-        #   }
-        # }
-        #
-        # const lossLessISO = onlyISO && iso !== "XRP" && ISO_REGEX.test(iso);
-        # this._isNative = onlyISO && code.toString("hex") === "000000";
-        # this._iso = this._isNative ? "XRP" : lossLessISO ? iso : undefined;
+        # Determine whether this currency code is in standard or nonstandard format:
+        # https://xrpl.org/currency-formats.html#nonstandard-currency-codes
+        is_standard_code = True
+        first_12_bytes = self.buffer[:12]
+        code_bytes = self.buffer[12:15]
+        last_5_bytes = self.buffer[15:]
+        iso = code_bytes.decode("ascii")
 
-    def from_parser(self, parser, length_hint: int = None) -> Currency:
-        """Construct a Currency from an existing BinaryParser."""
-        pass
+        if not (first_12_bytes == bytes(12)) or not (last_5_bytes == bytes(5)):
+            is_standard_code = False
 
-    def from_value(self, value: str) -> Currency:
+        lossless_iso = is_standard_code and iso != "XRP" and _is_iso_code(iso)
+        self._is_native = is_standard_code and code_bytes.hex() == "000000"
+        if self._is_native:
+            self._iso = "XRP"
+        else:
+            if lossless_iso:
+                self._iso = iso
+            else:
+                self._iso = None
+
+    @classmethod
+    def from_value(cls, value: str) -> Currency:
         """Construct a Currency object from a string representation of a currency."""
-        # TODO: error/format checking?
-        return Currency(bytes_from_representation(value))
-
-    def is_native(self) -> bool:
-        """Tells if this currency is native."""
-        return self._is_native
-
-    # TODO: this method actually returns str or None... how to rep this?
-    def iso(self) -> str:
-        """Return the ISO code of this currency if it exists, else None."""
-        return self._iso
+        if _is_iso_code(value):
+            return Currency(_iso_to_bytes(value))
+        if _is_hex(value):
+            return cls(bytes.fromhex(value))
+        raise XRPLBinaryCodecException(
+            "Unsupported Currency representation: {}".format(value)
+        )
 
     def to_json(self) -> str:
         """Returns the JSON representation of a currency."""
-        iso = self.iso()
-        if iso is not None:
-            return iso
+        if self._iso is not None:
+            return self._iso
         return self.buffer.hex().upper()
