492c72ec98b0fa8f86da9ea4aeff121534e7b445
Mayukha Vadari <mvadari@ripple.com>
2021-01-25 12:27:13 -0500

    fix uint8

--- xrpl/binary_codec/types/uint8.py
+++ xrpl/binary_codec/types/uint8.py
@@ -1,6 +1,9 @@
 """Derived UInt class for serializing/deserializing 8 bit UInt."""
+from __future__ import annotations
+
+from xrpl.binary_codec.binary_wrappers.binary_parser import BinaryParser
 from xrpl.binary_codec.exceptions import XRPLBinaryCodecException
-from xrpl.binary_codec.types import UInt
+from xrpl.binary_codec.types.uint import UInt
 
 _WIDTH = 1  # 8 / 8
 
@@ -8,7 +11,7 @@ _WIDTH = 1  # 8 / 8
 class UInt8(UInt):
     """Derived UInt class for serializing/deserializing 8 bit UInt."""
 
-    def __init__(self, buffer):
+    def __init__(self, buffer: bytes):
         """Construct a new UInt8 type from a `bytes` value."""
         if buffer is None:
             super().__init__(DEFAULT_UINT8.buffer)
@@ -16,12 +19,12 @@ class UInt8(UInt):
             super().__init__(buffer)
 
     @classmethod
-    def from_parser(cls, parser):
+    def from_parser(cls, parser: BinaryParser) -> UInt8:
         """Construct a new UInt8 type from a BinaryParser."""
         return cls(parser.read(_WIDTH))
 
     @classmethod
-    def from_value(cls, value):
+    def from_value(cls, value: int) -> UInt8:
         """Construct a new UInt8 type from a number."""
         if isinstance(value, int):
             value_bytes = (value).to_bytes(_WIDTH, byteorder="big")
