0b221d80e5bef4f2585efccf24ae32c10b5a7ddb
Ted Kalaw <ted.kalaw@gmail.com>
2021-01-25 14:40:44 -0600

    Adds instructions

--- noxfile.py
+++ noxfile.py
@@ -4,7 +4,11 @@ import nox
 
 @nox.session(python=["3.9"])
 def tests(session):
-    """Runs the test suite in xrpl-py."""
+    """
+    Runs the test suite in xrpl-py.
+
+    You can run individual tests with `nox -rs tests -- <directory or module>`.
+    """
     args = session.posargs or ["discover", "tests"]
     session.run("poetry", "install", external=True)
     session.run("python", "-m", "unittest", *args)
