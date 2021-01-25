03e8b2b9fa689535350e6e6f4797c4d88e43ed4a
Mayukha Vadari <mvadari@ripple.com>
2021-01-25 12:29:35 -0500

    fix rest of uints

--- xrpl/binary_codec/types/uint32.py
+++ xrpl/binary_codec/types/uint32.py
@@ -1,6 +1,9 @@
 """Derived UInt class for serializing/deserializing 32 bit UInt."""
+from __future__ import annotations
+
+from xrpl.binary_codec.binary_wrappers.binary_parser import BinaryParser
 from xrpl.binary_codec.exceptions import XRPLBinaryCodecException
-from xrpl.binary_codec.types import UInt
+from xrpl.binary_codec.types.uint import UInt
 
 _WIDTH = 4  # 32 / 8
 
@@ -8,7 +11,7 @@ _WIDTH = 4  # 32 / 8
 class UInt32(UInt):
     """Derived UInt class for serializing/deserializing 32 bit UInt."""
 
-    def __init__(self, buffer):
+    def __init__(self, buffer: bytes):
         """Construct a new UInt32 type from a `bytes` value."""
         if buffer is None:
             super().__init__(DEFAULT_UINT32.buffer)
@@ -16,12 +19,12 @@ class UInt32(UInt):
             super().__init__(buffer)
 
     @classmethod
-    def from_parser(cls, parser):
+    def from_parser(cls, parser: BinaryParser) -> UInt32:
         """Construct a new UInt32 type from a BinaryParser."""
         return cls(parser.read(_WIDTH))
 
     @classmethod
-    def from_value(cls, value):
+    def from_value(cls, value: int) -> UInt32:
         """Construct a new UInt32 type from a number."""
         if isinstance(value, int):
             value_bytes = (value).to_bytes(_WIDTH, byteorder="big")
--- xrpl/binary_codec/types/uint64.py
+++ xrpl/binary_codec/types/uint64.py
@@ -1,6 +1,9 @@
 """Derived UInt class for serializing/deserializing 64 bit UInt."""
+from __future__ import annotations
+
+from xrpl.binary_codec.binary_wrappers.binary_parser import BinaryParser
 from xrpl.binary_codec.exceptions import XRPLBinaryCodecException
-from xrpl.binary_codec.types import UInt
+from xrpl.binary_codec.types.uint import UInt
 
 _WIDTH = 8  # 64 / 8
 
@@ -8,7 +11,7 @@ _WIDTH = 8  # 64 / 8
 class UInt64(UInt):
     """Derived UInt class for serializing/deserializing 64 bit UInt."""
 
-    def __init__(self, buffer):
+    def __init__(self, buffer: bytes):
         """Construct a new UInt64 type from a `bytes` value."""
         if buffer is None:
             super().__init__(DEFAULT_UINT64.buffer)
@@ -16,12 +19,12 @@ class UInt64(UInt):
             super().__init__(buffer)
 
     @classmethod
-    def from_parser(cls, parser):
+    def from_parser(cls, parser: BinaryParser) -> UInt64:
         """Construct a new UInt64 type from a BinaryParser."""
         return cls(parser.read(_WIDTH))
 
     @classmethod
-    def from_value(cls, value):
+    def from_value(cls, value: int) -> UInt64:
         """Construct a new UInt64 type from a number."""
         if isinstance(value, int):
             value_bytes = (value).to_bytes(_WIDTH, byteorder="big")
