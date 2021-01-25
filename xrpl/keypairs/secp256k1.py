41a9a41e6fe7e1dda061546ac5236027aa95e71f
Ted Kalaw <ted.kalaw@gmail.com>
2021-01-25 16:11:53 -0600

    Fixes keypairs

--- xrpl/keypairs/secp256k1.py
+++ xrpl/keypairs/secp256k1.py
@@ -1,19 +1,21 @@
 """Methods for deriving keypairs given an SECP256k1-encoded seed."""
+from typing import Any
+
 from ecpy.curves import Curve  # type: ignore
 
 _CURVE = Curve.get_curve("secp256k1")
 
 
-def derive_keypair(entropy):
+def derive_keypair(entropy: Any) -> None:
     """TODO: placeholder."""
     pass
 
 
-def sign(message, private_key):
+def sign(message: Any, private_key: Any) -> None:
     """TODO: placeholder."""
     pass
 
 
-def is_message_valid(message, signature, public_key):
+def is_message_valid(message: Any, signature: Any, public_key: Any) -> None:
     """TODO: placeholder."""
     pass
