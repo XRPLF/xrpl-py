6c534dbfbb8c104e3e32fa13e5cb776ef53d1c71
Greg Weisbrod <greg.weisbrod@gmail.com>
2021-01-26 18:02:29 -0500

    replace randbytes with token_bytes

--- tests/keypairs/test_main.py
+++ tests/keypairs/test_main.py
@@ -8,8 +8,8 @@ DUMMY_BYTES = b"\x01\x02\x03\x04\x05\x06\x07\x08\t\n\x0b\x0c\r\x0e\x0f\x10"
 
 class TestMain(TestCase):
     # unfortunately, this patching is very brittle; it depends on the syntax
-    # used to import random within the calling module.
-    @patch("xrpl.keypairs.main.randbytes", autospec=True, return_value=DUMMY_BYTES)
+    # used to import secrets within the calling module.
+    @patch("xrpl.keypairs.main.token_bytes", autospec=True, return_value=DUMMY_BYTES)
     def test_generate_seed_no_params(self, _randbytes):
         output = keypairs.generate_seed()
         self.assertEqual(output, "sEdSKaCy2JT7JaM7v95H9SxkhP9wS2r")
