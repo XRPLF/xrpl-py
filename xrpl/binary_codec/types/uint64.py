0ddc37c46ca4743b4e5ddbf829e9e9d82daa6636
Mayukha Vadari <mvadari@ripple.com>
2021-01-28 07:23:33 -0800

    Implements `UInt16`, `UInt32`, and `UInt64` classes in `binarycodec` types (#60)
    * first round of implementation
    
    * docstrings
    
    * uint8 implementation
    
    * add docstrings
    
    * fix class method
    
    * add tests
    
    * bytes -> buffer renaming
    
    * more of the same
    
    * respond to comments
    
    * fix logic issue
    
    * implement uint16
    
    * fix logic errors
    
    * implement 32 and 64
    
    * fix tests
    
    * code cleanup
    
    * respond to comments
    
    * respond to comments

--- xrpl/binary_codec/types/uint64.py
+++ xrpl/binary_codec/types/uint64.py
@@ -1,27 +1,27 @@
-"""Derived UInt class for serializing/deserializing 8 bit UInt."""
+"""Derived UInt class for serializing/deserializing 64 bit UInt."""
 from xrpl.binary_codec.exceptions import XRPLBinaryCodecException
 from xrpl.binary_codec.types import UInt
 
-_WIDTH = 1  # 8 / 8
+_WIDTH = 8  # 64 / 8
 
 
-class UInt8(UInt):
-    """Derived UInt class for serializing/deserializing 8 bit UInt."""
+class UInt64(UInt):
+    """Derived UInt class for serializing/deserializing 64 bit UInt."""
 
     def __init__(self, buffer=bytes(_WIDTH)):
-        """Construct a new UInt8 type from a `bytes` value."""
+        """Construct a new UInt64 type from a `bytes` value."""
         super().__init__(buffer)
 
     @classmethod
     def from_parser(cls, parser):
-        """Construct a new UInt8 type from a BinaryParser."""
+        """Construct a new UInt64 type from a BinaryParser."""
         return cls(parser.read(_WIDTH))
 
     @classmethod
     def from_value(cls, value):
-        """Construct a new UInt8 type from a number."""
+        """Construct a new UInt64 type from a number."""
         if isinstance(value, int):
             value_bytes = (value).to_bytes(_WIDTH, byteorder="big", signed=False)
             return cls(value_bytes)
 
-        raise XRPLBinaryCodecException("Cannot construct UInt8 from given value")
+        raise XRPLBinaryCodecException("Cannot construct UInt64 from given value")
