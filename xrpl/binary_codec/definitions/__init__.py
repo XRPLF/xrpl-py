7172dbb39ad5a2c8b1eaf1a3931e6e36fc01b4ef
Ted Kalaw <ted.kalaw@gmail.com>
2021-02-03 11:49:16 -0600

    Adds __init__ to definitions

--- xrpl/binary_codec/definitions/__init__.py
+++ xrpl/binary_codec/definitions/__init__.py
@@ -1,2 +1,6 @@
 """TODO: D104 Missing docstring in public package."""
-from xrpl.binary_codec.definitions.definitions import *  # noqa: F401 F403
+from xrpl.binary_codec.definitions.field_header import FieldHeader
+from xrpl.binary_codec.definitions.field_info import FieldInfo
+from xrpl.binary_codec.definitions.field_instance import FieldInstance
+
+__all__ = ["FieldHeader", "FieldInfo", "FieldInstance"]
