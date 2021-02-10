2d987b7e2c92d2b4e587acdc6894ce708f20c5b4
Ted Kalaw <ted.kalaw@gmail.com>
2021-02-10 17:06:46 -0600

    Adds from_dict test

--- tests/models/test_ledger_obj.py
+++ tests/models/test_ledger_obj.py
@@ -1,9 +1,33 @@
 from unittest import TestCase
 
 from xrpl.models.ledger.account_root_object import AccountRootObject
+from xrpl.models.ledger.ledger_object import LedgerObjectType
 
 
 class TestLedgerObj(TestCase):
-    def test_compile(self):
-        obj = AccountRootObject("address", "balance", 0, "index", 0, "prv_id", 0, 0)
-        assert obj.type == "AccountRoot"
+    def test_from_dict(self):
+        account = "r9LqNeG6qHxjeUocjvVki2XR35weJ9mZgQ"
+        balance = "0.0001"
+        flags = 1
+        index = "ahash256"
+        owner_count = 1
+        previous_transaction_id = "3"
+        previous_transaction_sequence = 5
+        sequence = 1
+
+        account_root_dict = {
+            "account": account,
+            "balance": balance,
+            "flags": flags,
+            "index": index,
+            "owner_count": owner_count,
+            "previous_transaction_id": previous_transaction_id,
+            "previous_transaction_ledger_sequence": previous_transaction_sequence,
+            "sequence": sequence,
+        }
+
+        obj = AccountRootObject.from_dict(account_root_dict)
+        expected_dict = {**account_root_dict, "type": LedgerObjectType.AccountRoot}
+        self.assertEqual(obj.owner_count, expected_dict["owner_count"])
+        # obj = AccountRootObject("address", "balance", 0, "index", 0, "prv_id", 0, 0)
+        # assert obj.type == "AccountRoot"
--- xrpl/models/ledger/ledger_object.py
+++ xrpl/models/ledger/ledger_object.py
@@ -1,7 +1,8 @@
 """Base object for ledger objects on XRPL"""
-from abc import ABC
 from dataclasses import dataclass, field
 from enum import Enum
+
+from xrpl.models.base_model import BaseModel
 
 
 class LedgerObjectType(str, Enum):
@@ -11,7 +12,7 @@ class LedgerObjectType(str, Enum):
 
 
 @dataclass(frozen=True)
-class LedgerObject(ABC):
+class LedgerObject(BaseModel):
     """
     Base object for ledger objects on XRPL.
 
