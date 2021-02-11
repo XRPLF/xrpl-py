c7d9d6f13c88a4bc3e9f0c4f6e4d1010acf73b7e
Ted Kalaw <ted.kalaw@gmail.com>
2021-02-11 15:51:56 -0600

    Makes Wrapper an ABC+dataclass

--- xrpl/models/transactions/wrapper.py
+++ xrpl/models/transactions/wrapper.py
@@ -1,10 +1,13 @@
 """DOCSTRING"""
+from abc import ABC
+from dataclasses import dataclass
 from typing import Generic, TypeVar
 
 T = TypeVar("T")
 
 
-class Wrapper(Generic[T]):
+@dataclass(frozen=True)
+class Wrapper(ABC, Generic[T]):
     """A typed wrapper for single values."""
 
     value: T
