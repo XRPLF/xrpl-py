5cf3a34ba6a41441432f7147c72d897ea7e85aed
Greg Weisbrod <greg.weisbrod@gmail.com>
2021-02-22 14:47:26 -0500

    add account_delete

---
+++ xrpl/models/transactions/account_delete.py
@@ -0,0 +1,34 @@
+"""
+Represents an AccountDelete transaction on the XRP Ledger.
+
+An AccountDelete transaction deletes an account and any objects it owns in the XRP
+Ledger, if possible, sending the account's remaining XRP to a specified destination
+account. See Deletion of Accounts for the requirements to delete an account.
+
+`See AccountDelete <https://xrpl.org/accountdelete.html>`_
+`See Deletion of Accounts <https://xrpl.org/accounts.html#deletion-of-accounts>`_
+"""
+from __future__ import annotations  # Requires Python 3.7+
+
+from dataclasses import dataclass
+from typing import Optional
+
+from xrpl.models.transactions.transaction import REQUIRED, Transaction, TransactionType
+
+
+@dataclass(frozen=True)
+class AccountDelete(Transaction):
+    """
+    Represents an AccountDelete transaction on the XRP Ledger.
+
+    An AccountDelete transaction deletes an account and any objects it owns in the XRP
+    Ledger, if possible, sending the account's remaining XRP to a specified destination
+    account. See Deletion of Accounts for the requirements to delete an account.
+
+    `See AccountDelete <https://xrpl.org/accountdelete.html>`_
+    `See Deletion of Accounts <https://xrpl.org/accounts.html#deletion-of-accounts>`_
+    """
+
+    destination: str = REQUIRED
+    destination_tag: Optional[int] = None
+    transaction_type: str = TransactionType.AccountDelete
