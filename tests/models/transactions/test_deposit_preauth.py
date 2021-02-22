378cfbc3dc7e9150dd3df652039a99f40b19232a
Ted Kalaw <ted.kalaw@gmail.com>
2021-02-22 17:05:24 -0600

    Removes unused vars

--- tests/models/transactions/test_deposit_preauth.py
+++ tests/models/transactions/test_deposit_preauth.py
@@ -3,10 +3,6 @@ from unittest import TestCase
 from xrpl.models.exceptions import XRPLModelValidationException
 from xrpl.models.transactions import DepositPreauth
 
-_ACCOUNT = "r9LqNeG6qHxjeUocjvVki2XR35weJ9mZgQ"
-_FEE = "0.00001"
-_SEQUENCE = 19048
-
 
 class TestDepositPreauth(TestCase):
     def test_authorize_deauthorize_both_set(self):
