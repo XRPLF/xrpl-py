884c45694b52183e41ea4abd1f20f2d06be690f9
Amie <acorso@ripple.com>
2021-02-23 13:36:55 -0800

    Implements PaymentChannel(Claim/Create/Fund) Transaction Models (#116)
    * paymentchannel transaction types
    
    * Update xrpl/models/transactions/payment_channel_create.py
    
    Co-authored-by: Mayukha Vadari <mvadari@ripple.com>
    
    * Update xrpl/models/transactions/payment_channel_fund.py
    
    Co-authored-by: Mayukha Vadari <mvadari@ripple.com>
    
    * Update xrpl/models/transactions/payment_channel_claim.py
    
    Co-authored-by: Mayukha Vadari <mvadari@ripple.com>
    
    Co-authored-by: Mayukha Vadari <mvadari@ripple.com>

---
+++ xrpl/models/transactions/payment_channel_fund.py
@@ -0,0 +1,29 @@
+"""
+Represents a PaymentChannelFund transaction on the XRP Ledger.
+A PaymentChannelFund transaction adds additional XRP to an open payment channel,
+and optionally updates the expiration time of the channel. Only the source address
+of the channel can use this transaction.
+
+`See PaymentChannelFund <https://xrpl.org/paymentchannelfund.html>`_
+"""
+from dataclasses import dataclass
+from typing import Optional
+
+from xrpl.models.transactions.transaction import REQUIRED, Transaction, TransactionType
+
+
+@dataclass(frozen=True)
+class PaymentChannelFund(Transaction):
+    """
+    Represents a PaymentChannelFund transaction on the XRP Ledger.
+    A PaymentChannelFund transaction adds additional XRP to an open payment channel,
+    and optionally updates the expiration time of the channel. Only the source address
+    of the channel can use this transaction.
+
+    `See PaymentChannelFund <https://xrpl.org/paymentchannelfund.html>`_
+    """
+
+    channel: str = REQUIRED
+    amount: str = REQUIRED
+    expiration: Optional[int] = None
+    transaction_type: TransactionType = TransactionType.PaymentChannelFund
