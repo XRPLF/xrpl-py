45e6d05ca5828b63efd0276018f4ac07965b438f
Mayukha Vadari <mvadari@ripple.com>
2021-02-24 12:38:13 -0500

    add tests

--- xrpl/models/transactions/__init__.py
+++ xrpl/models/transactions/__init__.py
@@ -4,12 +4,20 @@ from xrpl.models.transactions.account_delete import AccountDelete
 from xrpl.models.transactions.account_set import AccountSet
 from xrpl.models.transactions.offer_cancel import OfferCancel
 from xrpl.models.transactions.offer_create import OfferCreate
+from xrpl.models.transactions.payment_channel_claim import PaymentChannelClaim
+from xrpl.models.transactions.payment_channel_create import PaymentChannelCreate
+from xrpl.models.transactions.payment_channel_fund import PaymentChannelFund
 from xrpl.models.transactions.set_regular_key import SetRegularKey
+from xrpl.models.transactions.signer_list_set import SignerListSet
 
 __all__ = [
     "AccountDelete",
     "AccountSet",
     "OfferCancel",
     "OfferCreate",
+    "PaymentChannelClaim",
+    "PaymentChannelCreate",
+    "PaymentChannelFund",
     "SetRegularKey",
+    "SignerListSet",
 ]
