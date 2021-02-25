98f7ace037b6a0fd493193a1a1f3ff1c8a0948df
Ted Kalaw <ted.kalaw@gmail.com>
2021-02-25 15:20:07 -0600

    Reorders vars

--- tests/models/transactions/test_escrow_create.py
+++ tests/models/transactions/test_escrow_create.py
@@ -7,20 +7,20 @@ from xrpl.models.transactions import EscrowCreate
 class TestEscrowCreate(TestCase):
     def test_final_after_less_than_cancel_after(self):
         account = "r9LqNeG6qHxjeUocjvVki2XR35weJ9mZgQ"
-        fee = "0.00001"
-        sequence = 19048
         amount = "amount"
-        destination = "destination"
         cancel_after = 2
         finish_after = 1
+        destination = "destination"
+        fee = "0.00001"
+        sequence = 19048
 
         with self.assertRaises(XRPLModelValidationException):
             EscrowCreate(
                 account=account,
-                fee=fee,
-                sequence=sequence,
                 amount=amount,
-                destination=destination,
                 cancel_after=cancel_after,
+                destination=destination,
+                fee=fee,
                 finish_after=finish_after,
+                sequence=sequence,
             )
