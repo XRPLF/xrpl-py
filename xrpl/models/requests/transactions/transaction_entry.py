2c91525d9b41b01c39529839d1da41ecbc40b7b4
Mayukha Vadari <mvadari@ripple.com>
2021-03-05 15:23:31 -0500

    transaction_entry

---
+++ xrpl/models/requests/transactions/transaction_entry.py
@@ -0,0 +1,33 @@
+"""
+The transaction_entry method retrieves information on a single transaction from a
+specific ledger version. (The tx method, by contrast, searches all ledgers for the
+specified transaction. We recommend using that method instead.)
+
+`See transaction_entry <https://xrpl.org/transaction_entry.html>`_
+"""
+
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from typing import Optional, Union
+
+from xrpl.models.base_model import REQUIRED
+from xrpl.models.requests.request import Request, RequestMethod
+
+
+@dataclass(frozen=True)
+class TransactionEntry(Request):
+    """
+    The transaction_entry method retrieves information on a single transaction from a
+    specific ledger version. (The tx method, by contrast, searches all ledgers for the
+    specified transaction. We recommend using that method instead.)
+
+    `See transaction_entry <https://xrpl.org/transaction_entry.html>`_
+    """
+
+    method: RequestMethod = field(
+        default_factory=lambda: RequestMethod.TRANSACTION_ENTRY, init=False
+    )
+    ledger_hash: Optional[str] = None
+    ledger_index: [Optional[Union[str, int]]] = None
+    tx_hash: str = REQUIRED
