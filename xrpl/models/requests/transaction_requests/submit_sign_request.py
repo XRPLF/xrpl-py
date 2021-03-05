b8edfef71624b83f79ddeec88788f89a14a905eb
Mayukha Vadari <mvadari@ripple.com>
2021-03-05 12:59:03 -0500

    add submit requests

---
+++ xrpl/models/requests/transaction_requests/submit_sign_request.py
@@ -0,0 +1,108 @@
+"""
+The submit method applies a transaction and sends it to the network to be confirmed and
+included in future ledgers.
+
+This command has two modes:
+* Submit-only mode takes a signed, serialized transaction as a binary blob, and submits
+it to the network as-is. Since signed transaction objects are immutable, no part of the
+transaction can be modified or automatically filled in after submission.
+* Sign-and-submit mode takes a JSON-formatted Transaction object, completes and signs
+the transaction in the same manner as the sign method, and then submits the signed
+transaction. We recommend only using this mode for testing and development.
+
+To send a transaction as robustly as possible, you should construct and sign it in
+advance, persist it somewhere that you can access even after a power outage, then
+submit it as a tx_blob. After submission, monitor the network with the tx method
+command to see if the transaction was successfully applied; if a restart or other
+problem occurs, you can safely re-submit the tx_blob transaction: it won't be applied
+twice since it has the same sequence number as the old transaction.
+
+`See submit <https://xrpl.org/submit.html>`_
+"""
+
+from __future__ import annotations
+
+from dataclasses import dataclass, field
+from typing import Any, Dict, Optional
+
+from xrpl.models.requests.request import Request, RequestMethod
+from xrpl.models.transactions.transaction import REQUIRED, Transaction
+
+
+@dataclass(frozen=True)
+class SignRequest(Request):
+    """
+    The submit method applies a transaction and sends it to the network to be confirmed
+    and included in future ledgers.
+
+    This command has two modes:
+    * Submit-only mode takes a signed, serialized transaction as a binary blob, and
+    submits it to the network as-is. Since signed transaction objects are immutable, no
+    part of the transaction can be modified or automatically filled in after submission.
+    * Sign-and-submit mode takes a JSON-formatted Transaction object, completes and
+    signs the transaction in the same manner as the sign method, and then submits the
+    signed transaction. We recommend only using this mode for testing and development.
+
+    To send a transaction as robustly as possible, you should construct and sign it in
+    advance, persist it somewhere that you can access even after a power outage, then
+    submit it as a tx_blob. After submission, monitor the network with the tx method
+    command to see if the transaction was successfully applied; if a restart or other
+    problem occurs, you can safely re-submit the tx_blob transaction: it won't be
+    applied twice since it has the same sequence number as the old transaction.
+
+    `See submit <https://xrpl.org/submit.html>`_
+    """
+
+    method: RequestMethod = field(
+        default_factory=lambda: RequestMethod.SUBMIT, init=False
+    )
+    transaction: Transaction = REQUIRED
+    secret: Optional[str] = None
+    seed: Optional[str] = None
+    seed_hex: Optional[str] = None
+    passphrase: Optional[str] = None
+    key_type: Optional[str] = None
+    offline: bool = False
+    build_path: Optional[bool] = None
+    fee_mult_max: int = 10
+    fee_div_max: int = 1
+
+    def to_dict(self: SignRequest) -> Dict[str, Any]:
+        """
+        Returns the dictionary representation of a SignRequest.
+
+        Returns:
+            The dictionary representation of a SignRequest.
+        """
+        return_dict = super().to_dict()
+        del return_dict["transaction"]
+        return_dict["tx_json"] = self.transaction.to_dict()
+        return return_dict
+
+    def _get_errors(self: SignRequest) -> Dict[str, str]:
+        errors = super()._get_errors()
+        if self.secret is not None and (
+            self.key_type is not None
+            or self.seed is not None
+            or self.seed_hex is not None
+            or self.passphrase is not None
+        ):
+            errors["SignRequest"] = (
+                "`secret` cannot be used with `key_type`, `seed`, `seed_hex`, or "
+                "`passphrase`."
+            )
+        elif self.seed is not None and (
+            self.seed_hex is not None or self.passphrase is not None
+        ):
+            errors[
+                "SignRequest"
+            ] = "`seed` cannot be used with `seed_hex` or `passphrase`."
+        elif self.seed_hex is not None and self.passphrase is not None:
+            errors["SignRequest"] = "`seed` cannot be used with `passphrase`."
+        elif self.key_type is None and (
+            self.seed is not None
+            or self.seed_hex is not None
+            or self.passphrase is not None
+        ):
+            self.key_type = "secp256k1"  # default
+        return errors
