8de3f0d2e69aef9650b768008753f885f832b64d
Mayukha Vadari <mvadari@ripple.com>
2021-03-09 15:37:49 -0500

    clean up test

--- tests/models/test_utils.py
+++ tests/models/test_utils.py
@@ -4,13 +4,7 @@ from xrpl.models.currencies import IssuedCurrency
 from xrpl.models.exceptions import XRPLModelException
 
 currency = "BTC"
-value = "100"
 issuer = "r9LqNeG6qHxjeUocjvVki2XR35weJ9mZgQ"
-currency_dict = {
-    "currency": currency,
-    "issuer": issuer,
-    "value": value,
-}
 
 
 class TestUtils(unittest.TestCase):
