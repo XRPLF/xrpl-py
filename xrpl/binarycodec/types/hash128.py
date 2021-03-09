87a785372a805a3cfaaa0e2642ddac3d838f1176
Ted Kalaw <ted.kalaw@gmail.com>
2021-03-09 16:59:27 -0600

    Improves typing in Hash classes
    TSIA

--- xrpl/binarycodec/types/hash128.py
+++ xrpl/binarycodec/types/hash128.py
@@ -5,10 +5,8 @@ of 128 bits (16 bytes).
 """
 from __future__ import annotations
 
-from typing import Optional
+from typing import Type
 
-from xrpl.binarycodec import XRPLBinaryCodecException
-from xrpl.binarycodec.binary_wrappers.binary_parser import BinaryParser
 from xrpl.binarycodec.types.hash import Hash
 
 
@@ -17,54 +15,8 @@ class Hash128(Hash):
     Codec for serializing and deserializing a hash field with a width
     of 128 bits (16 bytes).
     `See Hash Fields <https://xrpl.org/serialization.html#hash-fields>`_
-
-
-    Attributes:
-        _LENGTH: The length of this hash in bytes.
     """
 
-    _LENGTH = 16
-
-    def __init__(self: Hash128, buffer: bytes = None) -> None:
-        """Construct a Hash128."""
-        buffer = buffer if buffer is not None else bytes(self._LENGTH)
-        super().__init__(buffer)
-
     @classmethod
-    def from_value(cls: Hash128, value: str) -> Hash128:
-        """
-        Construct a Hash128 object from a hex string.
-
-        Args:
-            value: The value to construct a Hash128 from.
-
-        Returns:
-            The Hash128 object constructed from value.
-
-        Raises:
-            XRPLBinaryCodecException: If the supplied value is of the wrong type.
-        """
-        if not isinstance(value, str):
-            raise XRPLBinaryCodecException(
-                "Invalid type to construct a Hash128: expected str,"
-                " received {}.".format(value.__class__.__name__)
-            )
-
-        return cls(bytes.fromhex(value))
-
-    @classmethod
-    def from_parser(
-        cls: Hash128, parser: BinaryParser, length_hint: Optional[int] = None
-    ) -> Hash128:
-        """
-        Construct a Hash128 object from an existing BinaryParser.
-
-        Args:
-            parser: The parser to construct the Hash128 object from.
-            length_hint: The number of bytes to consume from the parser.
-
-        Returns:
-            The Hash128 object constructed from a parser.
-        """
-        num_bytes = length_hint if length_hint is not None else cls._LENGTH
-        return cls(parser.read(num_bytes))
+    def _get_length(cls: Type[Hash128]) -> int:
+        return 16
