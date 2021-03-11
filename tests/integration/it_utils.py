f513aadaa30bc4185eb2dd47807dd22403a89546
Amie Corso <acorso@ripple.com>
2021-03-10 18:04:01 -0800

    test_deposit_preauth

--- tests/integration/it_utils.py
+++ tests/integration/it_utils.py
@@ -1,6 +1,5 @@
 from time import sleep
 
-from https import JsonRpcClient, json_to_response
 from requests import post
 
 from xrpl.binarycodec.main import encode, encode_for_signing
@@ -15,6 +14,7 @@ from xrpl.models.requests.accounts.account_info import AccountInfo
 from xrpl.models.requests.fee import Fee
 from xrpl.models.response import Response
 from xrpl.models.transactions.transaction import Transaction
+from xrpl.network_clients.json_rpc_client import JsonRpcClient, json_to_response
 
 JSON_RPC_URL = "http://test.xrp.xpring.io:51234"
 JSON_RPC_CLIENT = JsonRpcClient(JSON_RPC_URL)
