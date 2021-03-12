48e376ace481ad0de84cc271dce562b641a3865c
Amie <acorso@ripple.com>
2021-03-12 10:07:21 -0800

    Integration Tests for `EscrowCancel`, `EscrowCreate`, `EscrowFinish` Transactions (#162)
    * json_rpc_client.py
    
    * rippled_exception.py
    
    * __init__.py
    
    * it_utils.py
    
    * test_json_rpc_client.py
    
    * oops delete it_utils.py until next PR
    
    * it_utils.py
    
    * test_account_set.py
    
    * fix imports
    
    * test_account_set and test_account_delete
    
    * test_check_cancel, cash, create, reusables
    
    * delete accidental files added to this branch
    
    * update account_set + delete with reusables
    
    * update it_utils
    
    * test_deposit_preauth
    
    * fix import
    
    * fix types
    
    * escrow create cancel
    
    * review comments
    
    * Update tests/integration/it_utils.py
    
    Co-authored-by: Mayukha Vadari <mvadari@ripple.com>
    
    * review comments
    
    * add actual tests
    
    * incomplete escrow finish
    
    * reusable_values.py
    
    * fix escrow finish test
    
    * fix escrow create tests
    
    * fix escrow cancel test
    
    Co-authored-by: Mayukha Vadari <mvadari@ripple.com>

---
+++ tests/integration/transactions/test_escrow_finish.py
@@ -0,0 +1,39 @@
+from unittest import TestCase
+
+from tests.integration.it_utils import submit_transaction
+from tests.integration.transactions.reusable_values import WALLET
+from xrpl.models.response import ResponseStatus
+from xrpl.models.transactions import EscrowFinish
+from xrpl.network_clients import JsonRpcClient
+
+JSON_RPC_URL = "http://test.xrp.xpring.io:51234"
+JSON_RPC_CLIENT = JsonRpcClient(JSON_RPC_URL)
+
+# Special fee for EscrowFinish transactions that contain a fulfillment.
+# See note here: https://xrpl.org/escrowfinish.html
+FEE = "600000000"
+
+ACCOUNT = WALLET.classic_address
+OWNER = "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn"
+OFFER_SEQUENCE = 7
+CONDITION = (
+    "A0258020E3B0C44298FC1C149AFBF4C8996FB92427AE41E4649B934CA495991B7852B855810100"
+)
+FULFILLMENT = "A0028000"
+
+
+class TestEscrowFinish(TestCase):
+    def test_all_fields(self):
+        escrow_finish = EscrowFinish(
+            account=ACCOUNT,
+            fee=FEE,
+            sequence=WALLET.next_sequence_num,
+            owner=OWNER,
+            offer_sequence=OFFER_SEQUENCE,
+            condition=CONDITION,
+            fulfillment=FULFILLMENT,
+        )
+        response = submit_transaction(escrow_finish, WALLET)
+        # Actual engine_result will be 'tecNO_TARGET' since using non-extant
+        # account for OWNER
+        self.assertEqual(response.status, ResponseStatus.SUCCESS)
