0fb28947b5dbfca7d511be61f3b6cf8456d625da
Amie Corso <acorso@ripple.com>
2021-03-12 08:04:21 -0800

    fix escrow finish test

--- tests/integration/transactions/test_escrow_finish.py
+++ tests/integration/transactions/test_escrow_finish.py
@@ -1,7 +1,7 @@
 from unittest import TestCase
 
 from tests.integration.it_utils import submit_transaction
-from tests.integration.transactions.reusable_values import FEE, WALLET
+from tests.integration.transactions.reusable_values import WALLET
 from xrpl.models.response import ResponseStatus
 from xrpl.models.transactions import EscrowFinish
 from xrpl.network_clients import JsonRpcClient
@@ -9,19 +9,17 @@ from xrpl.network_clients import JsonRpcClient
 JSON_RPC_URL = "http://test.xrp.xpring.io:51234"
 JSON_RPC_CLIENT = JsonRpcClient(JSON_RPC_URL)
 
+# Special fee for EscrowFinish transactions that contain a fulfillment.
+# See note here: https://xrpl.org/escrowfinish.html
+FEE = "600000000"
+
 ACCOUNT = WALLET.classic_address
 OWNER = "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn"
 OFFER_SEQUENCE = 7
-
-AMOUNT = "10000"
-DESTINATION = "rsA2LpzuawewSBQXkiju3YQTMzW13pAAdW"
-CANCEL_AFTER = 533257958
-FINISH_AFTER = 533171558
 CONDITION = (
     "A0258020E3B0C44298FC1C149AFBF4C8996FB92427AE41E4649B934CA495991B7852B855810100"
 )
-DESTINATION_TAG = 23480
-SOURCE_TAG = 11747
+FULFILLMENT = "A0028000"
 
 
 class TestEscrowFinish(TestCase):
@@ -30,7 +28,12 @@ class TestEscrowFinish(TestCase):
             account=ACCOUNT,
             fee=FEE,
             sequence=WALLET.next_sequence_num,
+            owner=OWNER,
+            offer_sequence=OFFER_SEQUENCE,
+            condition=CONDITION,
+            fulfillment=FULFILLMENT,
         )
         response = submit_transaction(escrow_finish, WALLET)
-        print(response)
+        # Actual engine_result will be 'tecNO_TARGET' since using non-extant
+        # account for OWNER
         self.assertEqual(response.status, ResponseStatus.SUCCESS)
