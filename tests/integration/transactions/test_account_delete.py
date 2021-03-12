383977aec8f794770be287784dbdc91e6cdb9259
Amie <acorso@ripple.com>
2021-03-12 07:37:28 -0800

    Integration Tests for `AccountSet` and `AccountDelete` Transactions (#153)
    * json_rpc_client.py
    
    * rippled_exception.py
    
    * __init__.py
    
    * it_utils.py
    
    * test_json_rpc_client.py
    
    * oops delete it_utils.py until next PR
    
    * it_utils.py
    
    * test_account_set.py
    
    * fix imports
    
    * test_account_set and test_account_delete
    
    * test_check_cancel, cash, create, reusables
    
    * delete accidental files added to this branch
    
    * fix types
    
    * review comments
    
    * Update tests/integration/it_utils.py
    
    Co-authored-by: Mayukha Vadari <mvadari@ripple.com>
    
    * review comments
    
    Co-authored-by: Mayukha Vadari <mvadari@ripple.com>

---
+++ tests/integration/transactions/test_account_delete.py
@@ -0,0 +1,46 @@
+from unittest import TestCase
+
+from tests.integration.it_utils import generate_faucet_wallet, submit_transaction
+from xrpl.models.response import ResponseStatus
+from xrpl.models.transactions import AccountDelete
+from xrpl.network_clients import JsonRpcClient
+
+JSON_RPC_URL = "http://test.xrp.xpring.io:51234"
+JSON_RPC_CLIENT = JsonRpcClient(JSON_RPC_URL)
+
+WALLET = generate_faucet_wallet()
+DESTINATION_WALLET = generate_faucet_wallet()
+
+ACCOUNT = WALLET.classic_address
+
+# AccountDelete transactions have a special fee.
+# See https://xrpl.org/accountdelete.html#special-transaction-cost.
+FEE = "5000000"
+
+DESTINATION_TAG = 3
+
+# TODO: Figure out how to handle the `tecTOO_SOON` error code... Sleep??  It'd be long.
+# See https://xrpl.org/accountdelete.html#error-cases
+
+
+class TestAccountDelete(TestCase):
+    def test_all_fields(self):
+        account_delete = AccountDelete(
+            account=ACCOUNT,
+            fee=FEE,
+            sequence=WALLET.next_sequence_num,
+            destination=DESTINATION_WALLET.classic_address,
+            destination_tag=DESTINATION_TAG,
+        )
+        response = submit_transaction(account_delete, WALLET)
+        self.assertEqual(response.status, ResponseStatus.SUCCESS)
+
+        # Note, we can't test the `engine_result` without waiting a significant
+        # amount of time because accounts can't be deleted until some number of
+        # ledgers have closed since its creation.
+        #
+        # The documentation for `tecTOO_SOON` reads:
+        # "The AccountDelete transaction failed because the account to be deleted had a
+        # Sequence number that is too high. The current ledger index must be at least
+        # 256 higher than the account's sequence number."
+        # self.assertEqual(response.result['engine_result'], 'tesSUCCESS')
