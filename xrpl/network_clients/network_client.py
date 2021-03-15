5e52a591ce4425e3818c7c04cee34525d61d9f1d
Mayukha Vadari <mvadari@ripple.com>
2021-03-15 10:09:21 -0400

    add interface

---
+++ xrpl/network_clients/network_client.py
@@ -0,0 +1,34 @@
+"""Interface for all network clients to follow."""
+from __future__ import annotations
+
+from abc import ABC, abstractmethod
+
+from xrpl.models.requests.request import Request
+from xrpl.models.response import Response
+
+
+class NetworkClient(ABC):
+    """Interface for all network clients to follow."""
+
+    def __init__(self: NetworkClient, url: str) -> None:
+        """
+        Constructs a NetworkClient.
+
+        Arguments:
+            url: The URL of the rippled node to submit requests to.
+        """
+        self.url = url
+
+    @abstractmethod
+    def request(self: NetworkClient, request_object: Request) -> Response:
+        """
+        Submit the request represented by the request_object to the rippled node
+        specified by this client's URL.
+
+        Arguments:
+            request_object: An object representing information about a rippled request.
+
+        Raises:
+            NotImplementedError: Always.
+        """
+        raise NotImplementedError("{self.__class__.__name__}.request not implemented.")
