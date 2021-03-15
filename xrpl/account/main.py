d0cd08879ee815e3e6c30415fb1f3a7dd8dc574b
Mayukha Vadari <mvadari@ripple.com>
2021-03-15 14:19:55 -0400

    use cast instead of assert

--- xrpl/account/main.py
+++ xrpl/account/main.py
@@ -1,6 +1,6 @@
 """High-level methods to obtain information about accounts."""
 
-from typing import cast
+from typing import Any, Dict, cast
 
 from xrpl.models.requests.accounts.account_info import AccountInfo
 from xrpl.models.requests.fee import Fee
@@ -20,8 +20,8 @@ def get_fee(client: JsonRpcClient) -> str:
         The minimum fee for transactions.
     """
     response = client.request(Fee())
-    assert isinstance(response.result, dict)
-    return cast(str, response.result["drops"]["minimum_fee"])
+    result = cast(Dict[str, Any], response.result)
+    return cast(str, result["drops"]["minimum_fee"])
 
 
 def get_next_valid_seq_number(address: str, client: JsonRpcClient) -> int:
@@ -36,8 +36,8 @@ def get_next_valid_seq_number(address: str, client: JsonRpcClient) -> int:
         The next valid sequence number for the address.
     """
     account_info = get_account_info(address, client)
-    assert isinstance(account_info.result, dict)
-    return cast(int, account_info.result["account_data"]["Sequence"])
+    result = cast(Dict[str, Any], account_info.result)
+    return cast(int, result["account_data"]["Sequence"])
 
 
 def get_balance(address: str, client: JsonRpcClient) -> int:
@@ -52,8 +52,8 @@ def get_balance(address: str, client: JsonRpcClient) -> int:
         The balance of the address.
     """
     account_info = get_account_info(address, client)
-    assert isinstance(account_info.result, dict)
-    return cast(int, account_info.result["account_data"]["Balance"])
+    result = cast(Dict[str, Any], account_info.result)
+    return cast(int, result["account_data"]["Balance"])
 
 
 def get_account_info(address: str, client: JsonRpcClient) -> Response:
