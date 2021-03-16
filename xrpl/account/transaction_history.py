119144cd512f2bdb009729591c0af25b3cd66e8c
Mayukha Vadari <mvadari@ripple.com>
2021-03-16 16:03:10 -0400

    payment history

--- xrpl/account/transaction_history.py
+++ xrpl/account/transaction_history.py
@@ -5,6 +5,23 @@ from xrpl.clients import Client, XRPLTransactionFailureException
 from xrpl.models.requests import AccountTx
 
 
+def get_account_payment_transaactions(
+    address: str, client: Client
+) -> List[Dict[str, Any]]:
+    """
+    Query the ledger for a list of payment transactions that involved a given account.
+
+    Args:
+        address: the account to query.
+        client: the network client used to make network calls.
+
+    Returns:
+        The payment transaction history for the address.
+    """
+    all_transactions = get_account_transactions(address, client)
+    return [tx for tx in all_transactions if tx["tx"]["TransactionType"] == "Payment"]
+
+
 def get_account_transactions(address: str, client: Client) -> List[Dict[str, Any]]:
     """
     Query the ledger for a list of transactions that involved a given account.
