d29c3d0eb5ae6e45fdbb8c5f749d9d5f0891693f
Mayukha Vadari <mvadari@ripple.com>
2021-03-25 11:39:23 -0400

    change testnet url to https

--- tests/integration/it_utils.py
+++ tests/integration/it_utils.py
@@ -6,7 +6,7 @@ from xrpl.models.transactions.transaction import Transaction
 from xrpl.transaction import safe_sign_and_submit_transaction
 from xrpl.wallet import Wallet
 
-JSON_RPC_URL = "http://test.xrp.xpring.io:51234"
+JSON_RPC_URL = "https://s.altnet.rippletest.net:51234"
 JSON_RPC_CLIENT = JsonRpcClient(JSON_RPC_URL)
 
 
