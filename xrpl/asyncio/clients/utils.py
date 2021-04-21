0e6bea2cc4bf856810a179b9433a76628f1ab0df
Mayukha Vadari <mvadari@ripple.com>
2021-04-21 16:57:41 -0400

    add tests, export

--- xrpl/asyncio/clients/utils.py
+++ xrpl/asyncio/clients/utils.py
@@ -46,3 +46,45 @@ def json_to_response(json: Dict[str, Any]) -> Response:
     # TODO: should we pull fields "status" OUT of result dict?
     response_type = ResponseType.RESPONSE
     return Response(status=status, result=result, type=response_type)
+
+
+def request_to_websocket(request_object: Request) -> Dict[str, Any]:
+    """Converts a request model object to the appropriate format for
+    interacting with the rippled API via the WebSocket API.
+
+    Args:
+        request_object: A Request object representing the parameters of a
+                        request to the rippled JSON RPC.
+
+    Returns:
+        A dictionary containing the attributes of this Request object formatted
+        for submission to the rippled WebSocket API.
+    """
+    method = request_object.method.name.lower()
+    dictionary = request_object.to_dict()
+    del dictionary["method"]
+    dictionary["command"] = method
+    return dictionary
+
+
+def websocket_to_response(response_dict: Dict[str, Any]) -> Response:
+    """Converts a WebSocket API response from the rippled server into a Response object.
+
+    Args:
+        response_dict: A dictionary representing the contents of the WebSocket API
+                       response from the rippled server.
+
+    Returns:
+        A Response object containing the information in the rippled server's response.
+    """
+    result = response_dict["result"]
+    raw_status = response_dict["status"]
+    if raw_status == "success":
+        status = ResponseStatus.SUCCESS
+    else:
+        status = ResponseStatus.ERROR
+    # TODO: response_type changes based on what we're getting back... where/how do we
+    #  differentiate based on that?
+    # TODO: should we pull fields "status" OUT of result dict?
+    response_type = ResponseType.RESPONSE
+    return Response(status=status, result=result, type=response_type)
--- xrpl/asyncio/clients/websocket_client.py
+++ xrpl/asyncio/clients/websocket_client.py
@@ -2,13 +2,14 @@
 from __future__ import annotations
 
 import asyncio
-from typing import Union
+import json
 
 import websockets
 
 from xrpl.clients.client import Client
-from xrpl.clients.utils import request_to_json_rpc
+from xrpl.clients.utils import request_to_websocket, websocket_to_response
 from xrpl.models.requests.request import Request
+from xrpl.models.response import Response
 
 # from xrpl.models.response import Response
 
@@ -25,9 +26,7 @@ class WebsocketClient(Client):
         """
         self.url = url
 
-    async def request_async(
-        self: WebsocketClient, request_object: Request
-    ) -> Union[str, bytes]:
+    async def request_async(self: WebsocketClient, request_object: Request) -> Response:
         """
         Submit the request represented by the request_object to the rippled node
         specified by this client's URL.
@@ -38,17 +37,17 @@ class WebsocketClient(Client):
         Returns:
             The response from the server, as a Response object.
         """
-        formatted_request = request_to_json_rpc(request_object)
-        print(formatted_request)
+        formatted_request = request_to_websocket(request_object)
+        if "id" not in formatted_request:
+            formatted_request["id"] = "request_{}".format(formatted_request["command"])
         async with websockets.connect(self.url) as websocket:
-            await websocket.send(formatted_request)
+            await websocket.send(json.dumps(formatted_request))
             response = await websocket.recv()
-            print(response)
-            return response
+            response_dict = json.loads(response)
+            assert response_dict["id"] == formatted_request["id"]
+            return websocket_to_response(response_dict)
 
-    def request_test(
-        self: WebsocketClient, request_object: Request
-    ) -> Union[str, bytes]:
+    def request(self: WebsocketClient, request_object: Request) -> Response:
         """
         Submit the request represented by the request_object to the rippled node
         specified by this client's URL.
