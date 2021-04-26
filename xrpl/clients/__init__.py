db031b6cd8363dab8b97b33e9e385371bd45ab4a
Mayukha Vadari <mvadari@ripple.com>
2021-04-26 14:58:08 -0400

    export websocket util methods

--- xrpl/clients/__init__.py
+++ xrpl/clients/__init__.py
@@ -1,13 +1,20 @@
 """Public interface for network clients for interacting with the XRPL."""
 from xrpl.asyncio.clients.client import Client
 from xrpl.asyncio.clients.exceptions import XRPLRequestFailureException
-from xrpl.asyncio.clients.utils import json_to_response, request_to_json_rpc
+from xrpl.asyncio.clients.utils import (
+    json_to_response,
+    request_to_json_rpc,
+    request_to_websocket,
+    websocket_to_response,
+)
 from xrpl.clients.json_rpc_client import JsonRpcClient
 
 __all__ = [
-    "JsonRpcClient",
-    "json_to_response",
     "Client",
+    "JsonRpcClient",
     "request_to_json_rpc",
+    "json_to_response",
+    "request_to_websocket",
     "XRPLRequestFailureException",
+    "websocket_to_response",
 ]
