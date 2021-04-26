f7661135f320efa72d4fbfcedade69de97b64846
Mayukha Vadari <mvadari@ripple.com>
2021-04-26 18:07:14 -0400

    handle long connections

--- tests/integration/test_websocket.py
+++ tests/integration/test_websocket.py
@@ -1,9 +1,28 @@
+import asyncio
 from unittest import TestCase
 
-from tests.integrations.it_utils import WEBSOCKET_CLIENT
+from tests.integration.it_utils import WEBSOCKET_CLIENT
 from xrpl.ledger import get_fee
 
 
 class TestWebsocket(TestCase):
     def test_get_fee(self):
         print(get_fee(WEBSOCKET_CLIENT))
+
+    def test_connection(self):
+        async def test():
+            def handler(message: str) -> None:
+                print(type(message))
+                print(message)
+
+            handler = asyncio.create_task(WEBSOCKET_CLIENT.handle_async(handler))
+            send = asyncio.create_task(WEBSOCKET_CLIENT.send("message"))
+
+            print("starting")
+            await handler
+            print("set up")
+            await send
+            print("sent?")
+            WEBSOCKET_CLIENT.close()
+
+        asyncio.get_event_loop().run_until_complete(test())
