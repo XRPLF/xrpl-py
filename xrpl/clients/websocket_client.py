523e193c2a3936af21ab9c8b45de1c8882a2a362
Mayukha Vadari <mvadari@ripple.com>
2021-04-26 18:07:14 -0400

    handle long connections

--- xrpl/clients/websocket_client.py
+++ xrpl/clients/websocket_client.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 
 import asyncio
 import json
+from typing import Callable
 
 import websockets
 
@@ -25,11 +26,12 @@ class WebsocketClient(Client):
             url: The URL of the rippled node to submit requests to.
         """
         self.url = url
+        self.websocket = None
 
     async def request_async(self: WebsocketClient, request_object: Request) -> Response:
         """
-        Submit the request represented by the request_object to the rippled node
-        specified by this client's URL.
+        Asynchronously submit the request represented by the request_object to the
+        rippled node specified by this client's URL.
 
         Arguments:
             request_object: An object representing information about a rippled request.
@@ -49,8 +51,8 @@ class WebsocketClient(Client):
 
     def request(self: WebsocketClient, request_object: Request) -> Response:
         """
-        Submit the request represented by the request_object to the rippled node
-        specified by this client's URL.
+        Synchronously submit the request represented by the request_object to the
+        rippled node specified by this client's URL.
 
         Arguments:
             request_object: An object representing information about a rippled request.
@@ -61,3 +63,58 @@ class WebsocketClient(Client):
         return asyncio.get_event_loop().run_until_complete(
             self.request_async(request_object)
         )
+
+    async def listen_async(
+        self: WebsocketClient,
+        request_object: Request,
+        handler: Callable[[Response], None],
+    ) -> None:
+        """
+        Asynchronously submits a request represented by the request_object to the
+        rippled node specified by this client's URL, and listens to (and processes) all
+        responses from the server.
+
+        Arguments:
+            request_object: An object representing information about a rippled request.
+            handler: The method that handles the Response objects from the server.
+                Takes a Response object as a parameter and returns None.
+        """
+        formatted_request = request_to_websocket(request_object)
+        if "id" not in formatted_request:
+            formatted_request["id"] = "request_{}".format(formatted_request["command"])
+
+        async with websockets.connect(self.url) as websocket:
+            await websocket.send(json.dumps(formatted_request))
+
+            async for message in websocket:
+                await websocket.send(json.dumps(formatted_request))
+                response = await websocket.recv()
+                response_dict = json.loads(response)
+                assert response_dict["id"] == formatted_request["id"]
+                handler(websocket_to_response(response_dict))
+
+    def listen(
+        self: WebsocketClient,
+        request_object: Request,
+        handler: Callable[[Response], None],
+    ) -> None:
+        """
+        Synchronously submits a request represented by the request_object to the
+        rippled node specified by this client's URL, and listens to (and processes) all
+        responses from the server.
+
+        Arguments:
+            request_object: An object representing information about a rippled request.
+            handler: The method that handles the Response objects from the server.
+                Takes a Response object as a parameter and returns None.
+        """
+        asyncio.get_event_loop().run_until_complete(
+            self.listen_async(request_object, handler)
+        )
+        asyncio.get_event_loop().run_forever()
+
+    def close(self: WebsocketClient) -> None:
+        """Closes any open WebSocket connections."""
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(loop.shutdown_asyncgens())
+        loop.close()
