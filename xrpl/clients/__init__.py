c2f324dcf764953073328b870c5052c1748b7099
Mayukha Vadari <mvadari@ripple.com>
2021-04-26 14:58:08 -0400

    export websocket util methods

--- xrpl/clients/__init__.py
+++ xrpl/clients/__init__.py
@@ -2,14 +2,21 @@
 from xrpl.clients.client import Client
 from xrpl.clients.exceptions import XRPLRequestFailureException
 from xrpl.clients.json_rpc_client import JsonRpcClient
-from xrpl.clients.utils import json_to_response, request_to_json_rpc
+from xrpl.clients.utils import (
+    json_to_response,
+    request_to_json_rpc,
+    request_to_websocket,
+    websocket_to_response,
+)
 from xrpl.clients.websocket_client import WebsocketClient
 
 __all__ = [
-    "JsonRpcClient",
-    "json_to_response",
     "Client",
-    "request_to_json_rpc",
+    "JsonRpcClient",
     "WebsocketClient",
+    "request_to_json_rpc",
+    "json_to_response",
+    "request_to_websocket",
+    "websocket_to_response",
     "XRPLRequestFailureException",
 ]
