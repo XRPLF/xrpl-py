2e1c028ed42a4bd87025c3510af3787cb93d7979
Mayukha Vadari <mvadari@ripple.com>
2021-05-04 11:43:44 -0400

    fix closing error

--- tests/integration/test_websocket.py
+++ tests/integration/test_websocket.py
@@ -1,31 +1,30 @@
 from asyncio import sleep
 from unittest import IsolatedAsyncioTestCase
 
-from tests.integration.it_utils import JSON_RPC_CLIENT, WEBSOCKET_URL
+from tests.integration.it_utils import JSON_RPC_CLIENT, WEBSOCKET_URL, WEBSOCKET_CLIENT
 from xrpl.clients import WebsocketClient
 from xrpl.ledger import get_fee
 from xrpl.models.requests import StreamParameter, Subscribe
 
-websocket = WebsocketClient(WEBSOCKET_URL)
-
 
 class TestWebsocket(IsolatedAsyncioTestCase):
     async def asyncSetUp(self):
-        await websocket.open_async()
+        await WEBSOCKET_CLIENT.open_async()
 
     async def asyncTearDown(self):
-        await websocket.close_async()
+        print("tearing downnnnnnnnnn")
+        await WEBSOCKET_CLIENT.close_async()
 
     def test_get_fee(self):
-        self.assertEqual(get_fee(websocket), get_fee(JSON_RPC_CLIENT))
+        self.assertEqual(get_fee(WEBSOCKET_CLIENT), get_fee(JSON_RPC_CLIENT))
 
     async def test_connection(self):
         print("running")
         id_val = "stream_test"
         subscribe = Subscribe(id=id_val, streams=[StreamParameter.LEDGER])
-        await websocket.send(subscribe)
+        await WEBSOCKET_CLIENT.send(subscribe)
         subscribe2 = Subscribe(
             id="test2", accounts=["rJHjA2WqqYWSh4ttCPW1b9aSyFkisfz93j"]
         )
-        await websocket.send(subscribe2)
+        await WEBSOCKET_CLIENT.send(subscribe2)
         await sleep(4)
