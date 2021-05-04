3f2a45d4be60b40c60da33668e1cdc13bb7a01ff
Mayukha Vadari <mvadari@ripple.com>
2021-05-04 12:59:50 -0400

    add docstrings, cleanup

--- tests/integration/test_websocket.py
+++ tests/integration/test_websocket.py
@@ -1,8 +1,7 @@
 from asyncio import sleep
 from unittest import IsolatedAsyncioTestCase
 
-from tests.integration.it_utils import JSON_RPC_CLIENT, WEBSOCKET_URL, WEBSOCKET_CLIENT
-from xrpl.clients import WebsocketClient
+from tests.integration.it_utils import JSON_RPC_CLIENT, WEBSOCKET_CLIENT
 from xrpl.ledger import get_fee
 from xrpl.models.requests import StreamParameter, Subscribe
 
@@ -12,14 +11,12 @@ class TestWebsocket(IsolatedAsyncioTestCase):
         await WEBSOCKET_CLIENT.open_async()
 
     async def asyncTearDown(self):
-        print("tearing downnnnnnnnnn")
         await WEBSOCKET_CLIENT.close_async()
 
     def test_get_fee(self):
         self.assertEqual(get_fee(WEBSOCKET_CLIENT), get_fee(JSON_RPC_CLIENT))
 
     async def test_connection(self):
-        print("running")
         id_val = "stream_test"
         subscribe = Subscribe(id=id_val, streams=[StreamParameter.LEDGER])
         await WEBSOCKET_CLIENT.send(subscribe)
