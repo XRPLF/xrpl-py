95d706d39244df6a75e2527aae57e328f124d200
Greg Weisbrod <greg.weisbrod@gmail.com>
2021-05-06 00:31:34 -0400

    use futures instead of sleeping for the request API, and a few other simplifications and performance gains

--- xrpl/asyncio/clients/websocket_client.py
+++ xrpl/asyncio/clients/websocket_client.py
@@ -1,10 +1,12 @@
 """A client for interacting with the rippled JSON RPC."""
 from __future__ import annotations
 
-import asyncio
 import json
+from asyncio import Future, Task, create_task, get_event_loop, get_running_loop
+from random import randrange
 from typing import Any, Callable, Dict, Optional, Union, cast
 
+from typing_extensions import Final
 from websockets.legacy.client import connect
 
 from xrpl.clients.client import Client
@@ -12,6 +14,16 @@ from xrpl.clients.exceptions import XRPLWebsocketException
 from xrpl.clients.utils import request_to_websocket, websocket_to_response
 from xrpl.models.requests.request import Request
 from xrpl.models.response import Response
+
+_REQ_ID_MAX: Final[int] = 1_000_000
+
+
+def _inject_request_id(request: Request) -> Request:
+    if request.id is not None:
+        return request
+    request_dict = request.to_dict()
+    request_dict["id"] = f"{request.method}_{randrange(_REQ_ID_MAX)}"
+    return cast(Request, Request.from_dict(request_dict))
 
 
 class WebsocketClient(Client):
@@ -33,23 +45,8 @@ class WebsocketClient(Client):
         """
         self.url = url
         self.response_handler = response_handler
-
-        self._open_requests: Dict[Union[str, int], Optional[Dict[str, Any]]] = {}
-        self._next_response_id = 0
-        self._open = False
-
-    @property
-    def next_response_id(self: WebsocketClient) -> int:
-        """
-        The next response ID number. Incremented under the hood so that each ID can be
-        unique without the user needing to do that by hand.
-
-        Returns:
-            The next valid response ID number.
-        """
-        ret = self._next_response_id
-        self._next_response_id += 1
-        return ret
+        self._handler_task: Optional[Task[None]] = None
+        self._open_requests: Dict[str, Future[Dict[str, Any]]] = {}
 
     def is_open(self: WebsocketClient) -> bool:
         """
@@ -58,26 +55,39 @@ class WebsocketClient(Client):
         Returns:
             Whether the Websocket client is currently open.
         """
-        return self._open
+        return self._handler_task is not None
 
     async def open_async(self: WebsocketClient) -> None:
-        """Asynchronously connects the client to the Web Socket API at the given URL."""
+        """
+        Asynchronously connects the client to the Web Socket API at the given URL.
+
+        Raises:
+            XRPLWebsocketException: If the Websocket is already open.
+        """
+        if self.is_open():
+            raise XRPLWebsocketException("Already open")
         self.websocket = await connect(self.url)
-        self._handler_task = asyncio.get_event_loop().create_task(self._handler())
-        self._open = True
+        self._handler_task = create_task(self._handler())
 
     def open(self: WebsocketClient) -> None:
         """Synchronously connects the client to the Web Socket API at the given URL."""
-        asyncio.get_event_loop().run_until_complete(self.open_async())
+        get_event_loop().run_until_complete(self.open_async())
 
     async def _handler(self: WebsocketClient) -> None:
         async for response in self.websocket:
             response_dict = json.loads(response)
             if "id" in response_dict and response_dict["id"] in self._open_requests:
-                self._open_requests[response_dict["id"]] = response_dict
+                self._open_requests[response_dict["id"]].set_result(response_dict)
 
             if self.response_handler is not None:
                 self.response_handler(response_dict)
+
+    def _ensure_future(self: WebsocketClient, request_id: Union[str, int]) -> None:
+        str_id = str(request_id)
+        if str_id not in self._open_requests:
+            self._open_requests[str_id] = get_running_loop().create_future()
+        elif self._open_requests[str_id].done():
+            raise XRPLWebsocketException(f"Request {str_id} is already completed")
 
     async def send(self: WebsocketClient, request_object: Request) -> None:
         """
@@ -88,12 +98,18 @@ class WebsocketClient(Client):
             request_object: An object representing information about a rippled request.
 
         Raises:
-            XRPLWebsocketException: If the Websocket is not open.
+            XRPLWebsocketException: If the Websocket is not open or the there is
+                already an open request by the request_object's ID.
         """
-        if not self._open:
+        if not self.is_open():
             raise XRPLWebsocketException("Websocket is not open")
-        formatted_request = request_to_websocket(request_object)
-        await self.websocket.send(json.dumps(formatted_request))
+
+        request_with_id = _inject_request_id(request_object)
+        assert request_with_id.id is not None  # mypy
+        self._ensure_future(request_with_id.id)
+
+        formatted_request = request_to_websocket(request_with_id)
+        self.websocket.send(json.dumps(formatted_request))
 
     async def request_async(self: WebsocketClient, request_object: Request) -> Response:
         """
@@ -115,25 +131,13 @@ class WebsocketClient(Client):
             XRPLWebsocketException: If there is already an open request by the
                 request_object's ID.
         """
-        if request_object.id is None:
-            request_dict = request_object.to_dict()
-            request_dict[
-                "id"
-            ] = f"request_{request_object.method}_{self.next_response_id}"
-            request_object = cast(Request, Request.from_dict(request_dict))
+        request_with_id = _inject_request_id(request_object)
+        assert request_with_id.id is not None  # mypy
+        self._ensure_future(request_with_id.id)
 
-        if request_object.id in self._open_requests:
-            raise XRPLWebsocketException("Already have an open request by that ID")
-
-        assert request_object.id is not None  # for mypy
-        self._open_requests[request_object.id] = None
-
-        await self.send(request_object)
-        while self._open_requests[request_object.id] is None:
-            await asyncio.sleep(1)  # TODO: make this smaller
-
-        response_dict = self._open_requests[request_object.id]
-        return websocket_to_response(cast(Dict[str, Any], response_dict))
+        create_task(self.send(request_with_id))
+        raw_response = await self._open_requests[str(request_with_id.id)]
+        return websocket_to_response(raw_response)
 
     def request(self: WebsocketClient, request_object: Request) -> Response:
         """
@@ -150,17 +154,32 @@ class WebsocketClient(Client):
 
         Returns:
             The response from the server, as a Response object.
+
+        Raises:
+            XRPLWebsocketException: If there is already an open request by the
+                request_object's ID.
         """
-        return asyncio.get_event_loop().run_until_complete(
-            self.request_async(request_object)
-        )
+        return get_event_loop().run_until_complete(self.request_async(request_object))
 
     async def close_async(self: WebsocketClient) -> None:
-        """Asynchronously closes any open WebSocket connections."""
+        """
+        Asynchronously closes the connection.
+
+        Raises:
+            XRPLWebsocketException: If the Websocket is already closed.
+        """
+        if not self.is_open():
+            raise XRPLWebsocketException("Websocket is not open")
         await self.websocket.close()
+        assert self._handler_task is not None  # mypy
         self._handler_task.cancel()
-        self._open = False
+        self._handler_task = None
 
     def close(self: WebsocketClient) -> None:
-        """Synchronously closes any open WebSocket connections."""
-        asyncio.get_event_loop().create_task(self.close_async())
+        """
+        Synchronously closes the connection.
+
+        Raises:
+            XRPLWebsocketException: If the Websocket is already closed.
+        """
+        get_event_loop().run_until_complete(self.close_async())
