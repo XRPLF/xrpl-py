6b422b0afce21e0d7b8d97c488824234b8f4bce0
Mayukha Vadari <mvadari@ripple.com>
2021-05-24 16:31:19 -0400

    account sugar

---
+++ tests/integration/sugar/test_asyncio_account.py
@@ -0,0 +1,100 @@
+from unittest import IsolatedAsyncioTestCase
+
+from tests.integration.it_utils import (
+    ASYNC_JSON_RPC_CLIENT,
+    JSON_RPC_CLIENT,
+    sign_and_reliable_submission_async,
+)
+from tests.integration.reusable_values import DESTINATION, WALLET
+from xrpl.asyncio.account import (
+    does_account_exist,
+    get_account_info,
+    get_account_transactions,
+    get_balance,
+    get_latest_transaction,
+)
+from xrpl.core.addresscodec import classic_address_to_xaddress
+from xrpl.models.transactions import Payment
+from xrpl.wallet import Wallet, generate_faucet_wallet
+
+NEW_WALLET = generate_faucet_wallet(JSON_RPC_CLIENT)
+EMPTY_WALLET = Wallet.create()
+
+
+class TestAccount(IsolatedAsyncioTestCase):
+    async def test_does_account_exist_true(self):
+        self.assertTrue(
+            await does_account_exist(WALLET.classic_address, ASYNC_JSON_RPC_CLIENT)
+        )
+
+    async def test_does_account_exist_false(self):
+        address = "rG1QQv2nh2gr7RCZ1P8YYcBUcCCN633jCn"
+        self.assertFalse(await does_account_exist(address, ASYNC_JSON_RPC_CLIENT))
+
+    async def test_does_account_exist_xaddress(self):
+        xaddress = classic_address_to_xaddress(WALLET.classic_address, None, True)
+        self.assertTrue(await does_account_exist(xaddress, ASYNC_JSON_RPC_CLIENT))
+
+    async def test_get_balance(self):
+        self.assertEqual(
+            await get_balance(NEW_WALLET.classic_address, ASYNC_JSON_RPC_CLIENT),
+            1000000000,
+        )
+
+    async def test_get_account_transactions(self):
+        transactions = await get_account_transactions(
+            NEW_WALLET.classic_address, ASYNC_JSON_RPC_CLIENT
+        )
+        self.assertEqual(len(transactions), 1)
+        self.assertEqual(transactions[0]["tx"]["TransactionType"], "Payment")
+        self.assertEqual(transactions[0]["tx"]["Amount"], "1000000000")
+
+    async def test_get_account_transactions_empty(self):
+        transactions = await get_account_transactions(
+            EMPTY_WALLET.classic_address, ASYNC_JSON_RPC_CLIENT
+        )
+        self.assertEqual(len(transactions), 0)
+
+    async def test_payment_transactions(self):
+        transactions = await get_account_transactions(
+            NEW_WALLET.classic_address, ASYNC_JSON_RPC_CLIENT
+        )
+        self.assertEqual(len(transactions), 1)
+        self.assertEqual(transactions[0]["tx"]["TransactionType"], "Payment")
+        self.assertEqual(transactions[0]["tx"]["Amount"], "1000000000")
+
+    async def test_payment_transactions_xaddress(self):
+        xaddress = classic_address_to_xaddress(NEW_WALLET.classic_address, None, True)
+        transactions = await get_account_transactions(xaddress, ASYNC_JSON_RPC_CLIENT)
+        self.assertEqual(len(transactions), 1)
+        self.assertEqual(transactions[0]["tx"]["TransactionType"], "Payment")
+        self.assertEqual(transactions[0]["tx"]["Amount"], "1000000000")
+
+    async def test_get_latest_transaction(self):
+        # NOTE: this test may take a long time to run
+        amount = "21000000"
+        payment = Payment(
+            account=WALLET.classic_address,
+            destination=DESTINATION.classic_address,
+            amount=amount,
+            last_ledger_sequence=WALLET.sequence + 20,
+        )
+        await sign_and_reliable_submission_async(payment, WALLET)
+        WALLET.sequence += 1
+
+        response = await get_latest_transaction(
+            WALLET.classic_address, ASYNC_JSON_RPC_CLIENT
+        )
+        self.assertEqual(len(response.result["transactions"]), 1)
+        transaction = response.result["transactions"][0]["tx"]
+        self.assertEqual(transaction["TransactionType"], "Payment")
+        self.assertEqual(transaction["Amount"], amount)
+        self.assertEqual(transaction["Account"], WALLET.classic_address)
+
+    async def test_get_account_info(self):
+        response = await get_account_info(WALLET.classic_address, ASYNC_JSON_RPC_CLIENT)
+        self.assertTrue(response.is_successful())
+        self.assertEqual(
+            response.result["account_data"]["Account"],
+            WALLET.classic_address,
+        )
