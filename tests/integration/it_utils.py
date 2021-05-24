6167fe023fef9c7339ffba197bf1c4855c3f1f03
Mayukha Vadari <mvadari@ripple.com>
2021-05-24 13:40:23 -0400

    first set of tests

--- tests/integration/it_utils.py
+++ tests/integration/it_utils.py
@@ -1,6 +1,15 @@
 """Utility functions and variables for integration tests."""
 
 from xrpl.asyncio.clients import AsyncJsonRpcClient, AsyncWebsocketClient
+from xrpl.asyncio.transaction import (
+    safe_sign_and_autofill_transaction as sign_and_autofill_async,
+)
+from xrpl.asyncio.transaction import (
+    safe_sign_and_submit_transaction as sign_and_submit_async,
+)
+from xrpl.asyncio.transaction import (
+    send_reliable_submission as send_reliable_submission_async,
+)
 from xrpl.clients import Client, JsonRpcClient, WebsocketClient
 from xrpl.models.response import Response
 from xrpl.models.transactions.transaction import Transaction
@@ -38,6 +47,15 @@ def submit_transaction(
     )
 
 
+async def submit_transaction_async(
+    transaction: Transaction,
+    wallet: Wallet,
+    client: Client = ASYNC_JSON_RPC_CLIENT,
+    check_fee: bool = True,
+) -> Response:
+    return await sign_and_submit_async(transaction, wallet, client, check_fee=check_fee)
+
+
 def sign_and_reliable_submission(
     transaction: Transaction, wallet: Wallet, use_json_client: bool = True
 ) -> Response:
@@ -46,8 +64,23 @@ def sign_and_reliable_submission(
     return send_reliable_submission(signed_tx, client)
 
 
+async def sign_and_reliable_submission_async(
+    transaction: Transaction, wallet: Wallet, use_json_client: bool = True
+) -> Response:
+    client = _choose_client_async(use_json_client)
+    signed_tx = await sign_and_autofill_async(transaction, wallet, client)
+    return await send_reliable_submission_async(signed_tx, client)
+
+
 def _choose_client(use_json_client: bool) -> Client:
     if use_json_client:
         return JSON_RPC_CLIENT
     else:
         return WEBSOCKET_CLIENT
+
+
+def _choose_client_async(use_json_client: bool) -> Client:
+    if use_json_client:
+        return ASYNC_JSON_RPC_CLIENT
+    else:
+        return ASYNC_WEBSOCKET_CLIENT
--- tests/integration/transactions/test_account_set.py
+++ tests/integration/transactions/test_account_set.py
@@ -1,6 +1,6 @@
-from unittest import TestCase
+from unittest import IsolatedAsyncioTestCase, TestCase
 
-from tests.integration.it_utils import submit_transaction
+from tests.integration.it_utils import submit_transaction, submit_transaction_async
 from tests.integration.reusable_values import WALLET
 from xrpl.models.response import ResponseStatus
 from xrpl.models.transactions import AccountSet
@@ -16,7 +16,7 @@ TRANSFER_RATE = 0
 TICK_SIZE = 10
 
 
-class TestAccountSet(TestCase):
+class TestAccountSetSync(TestCase):
     def test_required_fields_and_set_flag(self):
         account_set = AccountSet(
             account=ACCOUNT,
@@ -40,6 +40,35 @@ class TestAccountSet(TestCase):
             tick_size=TICK_SIZE,
         )
         response = submit_transaction(account_set, WALLET)
+        self.assertEqual(response.status, ResponseStatus.SUCCESS)
+        self.assertEqual(response.result["engine_result"], "tesSUCCESS")
+        WALLET.sequence += 1
+
+
+class TestAccountSetAsync(IsolatedAsyncioTestCase):
+    async def test_required_fields_and_set_flag(self):
+        account_set = AccountSet(
+            account=ACCOUNT,
+            sequence=WALLET.sequence,
+            set_flag=SET_FLAG,
+        )
+        response = await submit_transaction_async(account_set, WALLET)
+        self.assertEqual(response.status, ResponseStatus.SUCCESS)
+        self.assertEqual(response.result["engine_result"], "tesSUCCESS")
+        WALLET.sequence += 1
+
+    async def test_all_fields_minus_set_flag(self):
+        account_set = AccountSet(
+            account=ACCOUNT,
+            sequence=WALLET.sequence,
+            clear_flag=CLEAR_FLAG,
+            domain=DOMAIN,
+            email_hash=EMAIL_HASH,
+            message_key=MESSAGE_KEY,
+            transfer_rate=TRANSFER_RATE,
+            tick_size=TICK_SIZE,
+        )
+        response = await submit_transaction_async(account_set, WALLET)
         self.assertEqual(response.status, ResponseStatus.SUCCESS)
         self.assertEqual(response.result["engine_result"], "tesSUCCESS")
         WALLET.sequence += 1
