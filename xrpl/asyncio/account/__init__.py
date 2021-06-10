c7fa2930fd164fcf0b24f4db0321289689d847ac
Mayukha Vadari <mvadari@ripple.com>
2021-06-10 15:34:52 -0400

    respond to comments

--- xrpl/asyncio/account/__init__.py
+++ xrpl/asyncio/account/__init__.py
@@ -1,4 +1,4 @@
-"""Useful async methods for interacting with XRPL accounts."""
+"""Async methods for interacting with XRPL accounts."""
 from xrpl.asyncio.account.main import (
     does_account_exist,
     get_account_info,
