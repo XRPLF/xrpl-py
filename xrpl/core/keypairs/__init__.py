578d574c6d26ecd1b9a21fb0be42293a5b009e01
Matt Hamilton <mhamilton@ripple.com>
2021-07-12 13:35:30 +0100

    Workaround for ripemd160 not being available on Google Colab

--- xrpl/core/keypairs/__init__.py
+++ xrpl/core/keypairs/__init__.py
@@ -2,7 +2,6 @@
 Low-level functions for creating and using cryptographic keys with the XRP
 Ledger.
 """
-from hashlib import algorithms_available
 
 from xrpl.core.keypairs.exceptions import XRPLKeypairsException
 from xrpl.core.keypairs.main import (
@@ -13,10 +12,6 @@ from xrpl.core.keypairs.main import (
     sign,
 )
 
-assert (
-    "ripemd160" in algorithms_available
-), """Your OpenSSL implementation does not include the RIPEMD160 algorithm,
-    which is required by XRPL"""
 
 __all__ = [
     "derive_classic_address",
--- xrpl/core/keypairs/helpers.py
+++ xrpl/core/keypairs/helpers.py
@@ -2,6 +2,18 @@
 import hashlib
 
 
+if "ripemd160" in hashlib.algorithms_available:
+    ripemd160 = lambda x: hashlib.new("ripemd160", x).digest()
+else:
+    try:
+        from Crypto.Hash import RIPEMD
+        ripemd160 = lambda x: RIPEMD.new(x).digest()
+    except ImportError:
+        raise ImportError("""Your OpenSSL implementation does not include the RIPEMD160 """
+                          """algorithm, which is required by XRPL, or pycrypto """
+                          """needs installing""")
+
+
 def sha512_first_half(message: bytes) -> bytes:
     """
     Returns the first 32 bytes of SHA-512 hash of message.
@@ -28,4 +40,4 @@ def get_account_id(public_key: bytes) -> bytes:
         The account ID for the given public key.
     """
     sha_hash = hashlib.sha256(public_key).digest()
-    return hashlib.new("ripemd160", sha_hash).digest()
+    return ripemd160(sha_hash)
