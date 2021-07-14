5102a744cc63e5e604712818f470b89adc6c2852
Greg Weisbrod <ledhed2222@users.noreply.github.com>
2021-07-14 17:41:51 -0400

    fixes no_ripple_check request (#274)

---
+++ tests/integration/reqs/test_no_ripple_check.py
@@ -0,0 +1,16 @@
+from tests.integration.integration_test_case import IntegrationTestCase
+from tests.integration.it_utils import test_async_and_sync
+from tests.integration.reusable_values import WALLET
+from xrpl.models.requests import NoRippleCheck, NoRippleCheckRole
+
+
+class TestNoRippleCheck(IntegrationTestCase):
+    @test_async_and_sync(globals())
+    async def test_basic_functionality(self, client):
+        response = await client.request(
+            NoRippleCheck(
+                account=WALLET.classic_address,
+                role=NoRippleCheckRole.USER,
+            )
+        )
+        self.assertTrue(response.is_successful())
