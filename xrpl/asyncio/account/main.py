99e11a3603356e47158e1fa3bae06ed5c445c40b
Mayukha Vadari <mvadari@ripple.com>
2021-10-25 16:16:12 -0400

    change sequence number method to "current" instead of "validated"

--- xrpl/asyncio/account/main.py
+++ xrpl/asyncio/account/main.py
@@ -32,18 +32,23 @@ async def does_account_exist(address: str, client: Client) -> bool:
         raise
 
 
-async def get_next_valid_seq_number(address: str, client: Client) -> int:
+async def get_next_valid_seq_number(
+    address: str, client: Client, ledger_index: Union[str, int] = "current"
+) -> int:
     """
     Query the ledger for the next available sequence number for an account.
 
     Args:
         address: the account to query.
         client: the network client used to make network calls.
+        ledger_index: the version of the ledger to query. Defaults to "current".
 
     Returns:
         The next valid sequence number for the address.
     """
-    return cast(int, (await get_account_root(address, client))["Sequence"])
+    return cast(
+        int, (await get_account_root(address, client, ledger_index))["Sequence"]
+    )
 
 
 async def get_balance(address: str, client: Client) -> int:
@@ -60,28 +65,34 @@ async def get_balance(address: str, client: Client) -> int:
     return int((await get_account_root(address, client))["Balance"])
 
 
-async def get_account_root(address: str, client: Client) -> Dict[str, Union[int, str]]:
+async def get_account_root(
+    address: str, client: Client, ledger_index: Union[str, int] = "validated"
+) -> Dict[str, Union[int, str]]:
     """
     Query the ledger for the AccountRoot object associated with a given address.
 
     Args:
         address: the account to query.
         client: the network client used to make network calls.
+        ledger_index: the version of the ledger to query. Defaults to "validated".
 
     Returns:
         The AccountRoot dictionary for the address.
     """
-    account_info = await get_account_info(address, client)
+    account_info = await get_account_info(address, client, ledger_index)
     return cast(Dict[str, Union[int, str]], account_info.result["account_data"])
 
 
-async def get_account_info(address: str, client: Client) -> Response:
+async def get_account_info(
+    address: str, client: Client, ledger_index: Union[str, int] = "validated"
+) -> Response:
     """
     Query the ledger for account info of given address.
 
     Args:
         address: the account to query.
         client: the network client used to make network calls.
+        ledger_index: the version of the ledger to query. Defaults to "validated".
 
     Returns:
         The account info for the address.
@@ -94,7 +105,7 @@ async def get_account_info(address: str, client: Client) -> Response:
     response = await client.request_impl(
         AccountInfo(
             account=address,
-            ledger_index="validated",
+            ledger_index=ledger_index,
         )
     )
     if response.is_successful():
