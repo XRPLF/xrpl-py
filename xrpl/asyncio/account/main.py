6df6e578124a6776a33e02481070f08c6f97923b
Mayukha Vadari <mvadari@ripple.com>
2021-11-16 10:21:10 -0500

    feat: add optional `ledger_index` param to account methods (#304)
    * change sequence number method to "current" instead of "validated"
    
    * add ledger_index to sync methods
    
    * add error if bad ledger_index
    
    * update changelog
    
    * edit docstrings to follow style guide

--- xrpl/asyncio/account/main.py
+++ xrpl/asyncio/account/main.py
@@ -3,18 +3,25 @@
 from typing import Dict, Union, cast
 
 from xrpl.asyncio.clients import Client, XRPLRequestFailureException
+from xrpl.constants import XRPLException
 from xrpl.core.addresscodec import is_valid_xaddress, xaddress_to_classic_address
 from xrpl.models.requests import AccountInfo
 from xrpl.models.response import Response
 
 
-async def does_account_exist(address: str, client: Client) -> bool:
+async def does_account_exist(
+    address: str, client: Client, ledger_index: Union[str, int] = "validated"
+) -> bool:
     """
     Query the ledger for whether the account exists.
 
     Args:
         address: the account to query.
         client: the network client used to make network calls.
+        ledger_index: The ledger index to use for the request. Must be an integer
+            ledger value or "current" (the current working version), "closed" (for the
+            closed-and-proposed version), or "validated" (the most recent version
+            validated by consensus). The default is "validated".
 
     Returns:
         Whether the account exists on the ledger.
@@ -32,27 +39,41 @@ async def does_account_exist(address: str, client: Client) -> bool:
         raise
 
 
-async def get_next_valid_seq_number(address: str, client: Client) -> int:
+async def get_next_valid_seq_number(
+    address: str, client: Client, ledger_index: Union[str, int] = "current"
+) -> int:
     """
     Query the ledger for the next available sequence number for an account.
 
     Args:
         address: the account to query.
         client: the network client used to make network calls.
+        ledger_index: The ledger index to use for the request. Must be an integer
+            ledger value or "current" (the current working version), "closed" (for the
+            closed-and-proposed version), or "validated" (the most recent version
+            validated by consensus). The default is "current".
 
     Returns:
         The next valid sequence number for the address.
     """
-    return cast(int, (await get_account_root(address, client))["Sequence"])
+    return cast(
+        int, (await get_account_root(address, client, ledger_index))["Sequence"]
+    )
 
 
-async def get_balance(address: str, client: Client) -> int:
+async def get_balance(
+    address: str, client: Client, ledger_index: Union[str, int] = "validated"
+) -> int:
     """
     Query the ledger for the balance of the given account.
 
     Args:
         address: the account to query.
         client: the network client used to make network calls.
+        ledger_index: The ledger index to use for the request. Must be an integer
+            ledger value or "current" (the current working version), "closed" (for the
+            closed-and-proposed version), or "validated" (the most recent version
+            validated by consensus). The default is "validated".
 
     Returns:
         The balance of the address.
@@ -60,41 +81,63 @@ async def get_balance(address: str, client: Client) -> int:
     return int((await get_account_root(address, client))["Balance"])
 
 
-async def get_account_root(address: str, client: Client) -> Dict[str, Union[int, str]]:
+async def get_account_root(
+    address: str, client: Client, ledger_index: Union[str, int] = "validated"
+) -> Dict[str, Union[int, str]]:
     """
     Query the ledger for the AccountRoot object associated with a given address.
 
     Args:
         address: the account to query.
         client: the network client used to make network calls.
+        ledger_index: The ledger index to use for the request. Must be an integer
+            ledger value or "current" (the current working version), "closed" (for the
+            closed-and-proposed version), or "validated" (the most recent version
+            validated by consensus). The default is "validated".
 
     Returns:
         The AccountRoot dictionary for the address.
     """
-    account_info = await get_account_info(address, client)
+    account_info = await get_account_info(address, client, ledger_index)
     return cast(Dict[str, Union[int, str]], account_info.result["account_data"])
 
 
-async def get_account_info(address: str, client: Client) -> Response:
+async def get_account_info(
+    address: str, client: Client, ledger_index: Union[str, int] = "validated"
+) -> Response:
     """
     Query the ledger for account info of given address.
 
     Args:
         address: the account to query.
         client: the network client used to make network calls.
+        ledger_index: The ledger index to use for the request. Must be an integer
+            ledger value or "current" (the current working version), "closed" (for the
+            closed-and-proposed version), or "validated" (the most recent version
+            validated by consensus). The default is "validated".
 
     Returns:
         The account info for the address.
 
     Raises:
+        XRPLException: If the ledger_index value is invalid.
         XRPLRequestFailureException: if the rippled API call fails.
     """
     if is_valid_xaddress(address):
         address, _, _ = xaddress_to_classic_address(address)
+    if isinstance(ledger_index, str) and ledger_index not in {
+        "validated",
+        "current",
+        "closed",
+    }:
+        raise XRPLException(
+            "`ledger_index` is not valid - must be an `int` or one of {'validated', "
+            "'current', 'closed'}."
+        )
     response = await client.request_impl(
         AccountInfo(
             account=address,
-            ledger_index="validated",
+            ledger_index=ledger_index,
         )
     )
     if response.is_successful():
