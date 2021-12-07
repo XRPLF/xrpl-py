300d930186cc042ba81e6a3c619d88fd322c24f7
Mayukha Vadari <mvadari@ripple.com>
2021-12-07 15:38:39 -0800

    fix tests

--- tests/unit/models/amounts/test_issued_currency_amount.py
+++ tests/unit/models/amounts/test_issued_currency_amount.py
@@ -1,32 +1,12 @@
 from unittest import TestCase
 
 from xrpl.models.amounts import IssuedCurrencyAmount
-from xrpl.models.currencies import XRP, IssuedCurrency
+from xrpl.models.currencies import IssuedCurrency
 
 _ISSUER = "r9LqNeG6qHxjeUocjvVki2XR35weJ9mZgQ"
 
 
 class TestIssuedCurrencyAmount(TestCase):
-    def test_from_issued_currency(self):
-        currency = "USD"
-        amount = "12"
-        issued_currency = IssuedCurrency(currency=currency, issuer=_ISSUER)
-        issued_currency_amount = IssuedCurrencyAmount.from_issued_currency(
-            issued_currency, amount
-        )
-
-        self.assertEqual(issued_currency_amount.currency, currency)
-        self.assertEqual(issued_currency_amount.issuer, _ISSUER)
-        self.assertEqual(issued_currency_amount.value, amount)
-
-    def test_from_issued_currency_xrp(self):
-        amount = "12"
-        issued_currency_amount = IssuedCurrencyAmount.from_issued_currency(
-            XRP(), amount
-        )
-
-        self.assertEqual(issued_currency_amount, amount)
-
     def test_to_currency(self):
         currency = "USD"
         amount = "12"
