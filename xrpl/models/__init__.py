ea63bdf9e8bcdf8eff78313906e90aadece49685
Mayukha Vadari <mvadari@ripple.com>
2021-12-10 16:41:46 -0800

    fix: export `Transaction`, `Response`, pseudotransactions at the `models` level (#327)
    * export transaction
    
    * export Response
    
    * export pseudo-transactions
    
    * edit changelog

--- xrpl/models/__init__.py
+++ xrpl/models/__init__.py
@@ -5,7 +5,9 @@ from xrpl.models.currencies import *  # noqa: F401, F403
 from xrpl.models.exceptions import XRPLModelException
 from xrpl.models.path import Path, PathStep
 from xrpl.models.requests import *  # noqa: F401, F403
+from xrpl.models.response import Response
 from xrpl.models.transactions import *  # noqa: F401, F403
+from xrpl.models.transactions.pseudo_transactions import *  # noqa: F401, F403
 
 __all__ = [
     "XRPLModelException",
@@ -17,6 +19,8 @@ __all__ = [
     *requests.__all__,
     "transactions",
     *transactions.__all__,
+    *transactions.pseudo_transactions.__all__,
     "Path",
     "PathStep",
+    "Response",
 ]
