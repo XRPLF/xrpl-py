1811e8c6c9c07ae8b05b8d61735a5a3708dff999
Mayukha Vadari <mvadari@ripple.com>
2021-12-16 12:13:10 -0800

    chore: update dev dependencies so tests pass (#334)
    * update deps
    
    * make mypy happy

--- xrpl/models/utils.py
+++ xrpl/models/utils.py
@@ -57,7 +57,7 @@ def require_kwargs_on_init(cls: Type[_T]) -> Type[_T]:
                 f"{type(self).__name__}.__init__ only allows keyword arguments. "
                 f"Found the following positional arguments: {args}"
             )
-        original_init(self, **kwargs)  # type: ignore
+        original_init(self, **kwargs)
 
     # noinspection PyTypeHints
     cls.__init__ = new_init  # type: ignore
