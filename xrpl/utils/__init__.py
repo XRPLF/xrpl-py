f971eaa14516cee552b243aa4d5f65e0f406c80a
LimpidCrypto <97235361+limpidcrypto@users.noreply.github.com>
2022-01-13 12:25:34 +0100

    Fixed Linter notes

--- xrpl/utils/__init__.py
+++ xrpl/utils/__init__.py
@@ -8,7 +8,11 @@ from xrpl.utils.time_conversions import (
     ripple_time_to_datetime,
     ripple_time_to_posix,
 )
-from xrpl.utils.tx_parser import parse_balance_changes, parse_final_balances
+from xrpl.utils.tx_parser import (
+    XRPLMetadataExcetion,
+    parse_balance_changes,
+    parse_final_balances
+)
 from xrpl.utils.xrp_conversions import XRPRangeException, drops_to_xrp, xrp_to_drops
 
 __all__ = [
@@ -22,6 +26,7 @@ __all__ = [
     "posix_to_ripple_time",
     "parse_balance_changes",
     "parse_final_balances",
+    "XRPLMetadataExcetion",
     "XRPRangeException",
     "XRPLTimeRangeException",
 ]
