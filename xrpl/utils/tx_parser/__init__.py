b336f00111459df3cc8829db59ce5d826e9737b1
LimpidCrypto <97235361+LimpidCrypto@users.noreply.github.com>
2022-03-29 21:28:52 +0200

    Update __init__.py

--- xrpl/utils/tx_parser/__init__.py
+++ xrpl/utils/tx_parser/__init__.py
@@ -1,17 +1,17 @@
 """Transaction parser."""
 
 from xrpl.utils.tx_parser.balance_changes import (
-    ParseBalanceChanges,
-    ParseFinalBalances,
-    ParsePreviousBalances,
+    parse_balance_changes,
+    parse_final_balances,
+    parse_previous_balances,
 )
-from xrpl.utils.tx_parser.orderbook_changes import ParseOrderBookChanges
+from xrpl.utils.tx_parser.orderbook_changes import parse_order_book_changes
 from xrpl.utils.tx_parser.utils import XRPLMetadataException
 
 __all__ = [
-    "ParseBalanceChanges",
-    "ParseFinalBalances",
-    "ParsePreviousBalances",
-    "ParseOrderBookChanges",
+    "parse_balance_changes",
+    "parse_final_balances",
+    "parse_previous_balances",
+    "parse_order_book_changes",
     "XRPLMetadataException",
 ]
