5a6b90ffd13dc9f196b0afe761536f6eebd7e2d0
LimpidCrypto <info@limpidcrypto.de>
2022-04-06 23:11:17 +0200

    add helper function; add tests

--- xrpl/asyncio/ledger/main.py
+++ xrpl/asyncio/ledger/main.py
@@ -5,6 +5,7 @@ from typing import Optional, cast
 from typing_extensions import Literal
 
 from xrpl.asyncio.clients import Client, XRPLRequestFailureException
+from xrpl.asyncio.ledger.utils import calculate_fee_dynamically
 from xrpl.constants import XRPLException
 from xrpl.models.requests import Fee, Ledger
 from xrpl.utils import xrp_to_drops
@@ -88,62 +89,17 @@ async def get_fee(
 
     result = response.result
     drops = result["drops"]
-    if fee_type:
-        if fee_type == "open":
-            fee = cast(str, drops["open_ledger_fee"])
-        elif fee_type == "minimum":
-            fee = cast(str, drops["minimum_fee"])
-        else:
-            raise XRPLException(
-                f'`fee_type` param must be "open" or "minimum". {fee_type} is not a '
-                "valid option."
-            )
-    else:  # use https://gist.github.com/WietseWind/3e9f9339f37a5881978a9661f49b0e52
-        current_queue_size = int(result["current_queue_size"])
-        max_queue_size = int(result["max_queue_size"])
-        queue_pct = current_queue_size / max_queue_size
-        minimum_fee = int(drops["minimum_fee"])
-        median_fee = int(drops["median_fee"])
-        open_ledger_fee = int(drops["open_ledger_fee"])
-
-        fee_low = round(
-            min(
-                max(minimum_fee * 1.5, round(max(median_fee, open_ledger_fee) / 500)),
-                1000,
-            ),
+    if fee_type == "open":
+        fee = cast(str, drops["open_ledger_fee"])
+    elif fee_type == "minimum":
+        fee = cast(str, drops["minimum_fee"])
+    elif fee_type is not None:
+        raise XRPLException(
+            f'`fee_type` param must be "open" or "minimum". {fee_type} is not a '
+            "valid option."
         )
-        if queue_pct > 0.1:  # if 'current_queue_size' is >10 % of 'max_queue_size'
-            possible_fee_medium = round(
-                (minimum_fee + median_fee + open_ledger_fee) / 3
-            )
-        elif queue_pct == 0:  # if 'current_queue_size' is 0
-            possible_fee_medium = max(
-                10 * minimum_fee, min(minimum_fee, open_ledger_fee)
-            )
-        else:
-            possible_fee_medium = max(
-                10 * minimum_fee, round((minimum_fee + median_fee) / 2)
-            )
-        fee_medium = round(
-            min(
-                possible_fee_medium,
-                fee_low * 15,
-                10000,
-            ),
-        )
-        fee_high = round(
-            min(
-                max(10 * minimum_fee, round(max(median_fee, open_ledger_fee) * 1.1)),
-                100000,
-            ),
-        )
-
-        if queue_pct == 0:  # if queue is empty
-            fee = str(fee_low)
-        elif queue_pct == 1:  # if queue is full
-            fee = str(fee_high)
-        else:
-            fee = str(fee_medium)
+    else:
+        fee = calculate_fee_dynamically(fee_data_set=result)
     if max_fee is not None:
         max_fee_drops = int(xrp_to_drops(max_fee))
         if max_fee_drops < int(fee):  # if 'fee' exceeds the 'max_fee' use 'max_fee'
