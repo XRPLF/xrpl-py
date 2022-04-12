1b322ec56fee0cad90ef21c029d06bb90590aeb2
Jackson Mills <jmills@ripple.com>
2022-04-12 15:57:44 -0700

    Add tests

---
+++ tests/unit/utils/test_parse_nftoken_id.py
@@ -0,0 +1,27 @@
+"""Test the parse_nftoken_id util."""
+from __future__ import annotations
+
+from unittest import TestCase
+
+from xrpl.utils.parse_nftoken_id import parse_nftoken_id
+
+
+class TestParseNFTokenID(TestCase):
+    """Test parse_nftoken_id."""
+
+    def _test_parse_nftoken_id_successful(self: TestParseNFTokenID) -> None:
+        nft_id = "000B0539C35B55AA096BA6D87A6E6C965A6534150DC56E5E12C5D09E0000000C"  # noqa:E501
+        result = parse_nftoken_id(nft_id)
+        expected = {
+            "token_id": nft_id,
+            "flags": 11,
+            "transfer_fee": 1337,
+            "issuer": "rJoxBSzpXhPtAuqFmqxQtGKjA13jUJWthE",
+            "taxon": 1337,
+            "sequence": 12,
+        }
+        self.assertEqual(result, expected)
+
+    def _test_parse_nftoken_id_raises(self: TestParseNFTokenID) -> None:
+        with self.assertRaises(RuntimeError):
+            parse_nftoken_id("ABCD")
