7c6370eba3964bc90b880266a4a6d2c5f2ad0ba1
Obiajulu <47371105+ObiajuluM@users.noreply.github.com>
2022-05-11 08:52:16 -0700

    Create escrow_utility.py

---
+++ xrpl/utils/escrow_utility.py
@@ -0,0 +1,12 @@
+"""
+Utilities for managing conditional escrows
+"""
+
+from cryptoconditions import PreimageSha256
+
+def genCondition_Fulfillment(secret: bytes) -> dict:
+    """Generate a condition and fulfillment for escrows"""
+    fufill = PreimageSha256(preimage=secret)
+    return {
+    "condition": str.upper(fufill.condition_binary.hex()),
+    "fulfillment": str.upper(fufill.serialize_binary().hex())}
