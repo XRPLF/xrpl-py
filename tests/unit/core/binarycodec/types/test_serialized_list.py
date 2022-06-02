38d7d7a4aa3aaa7200138c9989a3b04cef7f67d2
Mayukha Vadari <mvadari@ripple.com>
2022-06-02 13:29:34 -0400

    fix tests

--- tests/unit/core/binarycodec/types/test_serialized_list.py
+++ tests/unit/core/binarycodec/types/test_serialized_list.py
@@ -2,10 +2,7 @@ from unittest import TestCase
 
 from xrpl.core.binarycodec.binary_wrappers.binary_parser import BinaryParser
 from xrpl.core.binarycodec.exceptions import XRPLBinaryCodecException
-from xrpl.core.binarycodec.types.serialized_list import (
-    _ARRAY_END_MARKER,
-    SerializedList,
-)
+from xrpl.core.binarycodec.types.st_array import _ARRAY_END_MARKER, STArray
 
 MEMO = {
     "Memo": {
@@ -22,43 +19,43 @@ EXPECTED_JSON = [MEMO, MEMO]
 BUFFER = MEMO_HEX + MEMO_HEX + _ARRAY_END_MARKER.hex().upper()
 
 
-class TestSerializedList(TestCase):
+class TestSTArray(TestCase):
     maxDiff = 1000
 
     def test_from_value(self):
-        serialized_list = SerializedList.from_value(EXPECTED_JSON)
+        serialized_list = STArray.from_value(EXPECTED_JSON)
         self.assertEqual(BUFFER, str(serialized_list))
 
     def test_from_parser(self):
         parser = BinaryParser(BUFFER)
-        serialized_list = SerializedList.from_parser(parser)
+        serialized_list = STArray.from_parser(parser)
         self.assertEqual(BUFFER, str(serialized_list))
 
     def test_from_value_to_json(self):
-        serialized_list = SerializedList.from_value(EXPECTED_JSON)
+        serialized_list = STArray.from_value(EXPECTED_JSON)
         actual_json = serialized_list.to_json()
         self.assertEqual(actual_json[0], actual_json[1])
         self.assertEqual(actual_json, EXPECTED_JSON)
 
     def test_from_parser_to_json(self):
         parser = BinaryParser(BUFFER)
-        serialized_list = SerializedList.from_parser(parser)
+        serialized_list = STArray.from_parser(parser)
         self.assertEqual(serialized_list.to_json(), EXPECTED_JSON)
 
     def test_from_value_non_list(self):
         obj = 123
         with self.assertRaises(XRPLBinaryCodecException):
-            SerializedList.from_value(obj)
+            STArray.from_value(obj)
 
     def test_from_value_bad_list(self):
         obj = [123]
         with self.assertRaises(XRPLBinaryCodecException):
-            SerializedList.from_value(obj)
+            STArray.from_value(obj)
 
     def test_raises_invalid_value_type(self):
         invalid_value = 1
         self.assertRaises(
             XRPLBinaryCodecException,
-            SerializedList.from_value,
+            STArray.from_value,
             invalid_value,
         )
