e6d47e74edff0ae9bd279db2fb4f5cfbff60383a
Mayukha Vadari <mvadari@ripple.com>
2022-06-15 12:17:25 -0400

    fix another bug

--- xrpl/models/requests/sign_and_submit.py
+++ xrpl/models/requests/sign_and_submit.py
@@ -101,7 +101,7 @@ class SignAndSubmit(Submit):
         """
         return_dict = super().to_dict()
         del return_dict["transaction"]
-        return_dict["tx_json"] = self.transaction.to_dict()
+        return_dict["tx_json"] = self.transaction.to_xrpl()
         return return_dict
 
     def _get_errors(self: SignAndSubmit) -> Dict[str, str]:
