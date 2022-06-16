b73c249a1cf92a0e0fa91a12d6404fbb409143cb
Mayukha Vadari <mvadari@ripple.com>
2022-06-16 15:45:09 -0400

    use to_xrpl instead of to_dict

--- xrpl/models/requests/sign_for.py
+++ xrpl/models/requests/sign_for.py
@@ -83,7 +83,7 @@ class SignFor(Request):
         """
         return_dict = super().to_dict()
         del return_dict["transaction"]
-        return_dict["tx_json"] = self.transaction.to_dict()
+        return_dict["tx_json"] = self.transaction.to_xrpl()
         return return_dict
 
     def _get_errors(self: SignFor) -> Dict[str, str]:
