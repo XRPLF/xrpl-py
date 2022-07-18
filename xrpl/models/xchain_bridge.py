2c020db6c2d1c084e056cf72ac0f2c8680eb538b
Mayukha Vadari <mvadari@ripple.com>
2022-07-18 15:38:09 +0100

    rename Bridge -> XChainBridge in models

--- xrpl/models/xchain_bridge.py
+++ xrpl/models/xchain_bridge.py
@@ -1,4 +1,4 @@
-"""A Bridge represents a cross-chain bridge."""
+"""A XChainBridge represents a cross-chain bridge."""
 
 from __future__ import annotations
 
@@ -14,8 +14,8 @@ from xrpl.models.utils import require_kwargs_on_init
 
 @require_kwargs_on_init
 @dataclass(frozen=True)
-class Bridge(BaseModel):
-    """A Bridge represents a cross-chain bridge."""
+class XChainBridge(BaseModel):
+    """A XChainBridge represents a cross-chain bridge."""
 
     locking_chain_door: str
     locking_chain_issue: Union[Literal["XRP"], IssuedCurrency]
