43a658139f3799fefeebd403cec67396b0c1641c
Mayukha Vadari <mvadari@ripple.com>
2022-07-18 15:38:09 +0100

    rename Bridge -> XChainBridge in models

--- xrpl/models/__init__.py
+++ xrpl/models/__init__.py
@@ -1,7 +1,6 @@
 """Top-level exports for the models package."""
 from xrpl.models import amounts, currencies, requests, transactions
 from xrpl.models.amounts import *  # noqa: F401, F403
-from xrpl.models.bridge import Bridge
 from xrpl.models.currencies import *  # noqa: F401, F403
 from xrpl.models.exceptions import XRPLModelException
 from xrpl.models.path import Path, PathStep
@@ -9,6 +8,7 @@ from xrpl.models.requests import *  # noqa: F401, F403
 from xrpl.models.response import Response
 from xrpl.models.transactions import *  # noqa: F401, F403
 from xrpl.models.transactions.pseudo_transactions import *  # noqa: F401, F403
+from xrpl.models.xchain_bridge import XChainBridge
 
 __all__ = [
     "XRPLModelException",
@@ -21,8 +21,8 @@ __all__ = [
     "transactions",
     *transactions.__all__,
     *transactions.pseudo_transactions.__all__,
-    "Bridge",
     "Path",
     "PathStep",
     "Response",
+    "XChainBridge",
 ]
--- xrpl/models/transactions/xchain_create_bridge.py
+++ xrpl/models/transactions/xchain_create_bridge.py
@@ -4,11 +4,11 @@ from dataclasses import dataclass, field
 from typing import Optional
 
 from xrpl.models.amounts import Amount
-from xrpl.models.bridge import Bridge
 from xrpl.models.required import REQUIRED
 from xrpl.models.transactions.transaction import Transaction
 from xrpl.models.transactions.types import TransactionType
 from xrpl.models.utils import require_kwargs_on_init
+from xrpl.models.xchain_bridge import XChainBridge
 
 
 @require_kwargs_on_init
@@ -16,7 +16,7 @@ from xrpl.models.utils import require_kwargs_on_init
 class XChainCreateBridge(Transaction):
     """Represents a XChainCreateBridge transaction."""
 
-    bridge: Bridge = REQUIRED  # type: ignore
+    xchain_bridge: XChainBridge = REQUIRED  # type: ignore
 
     signature_reward: Amount = REQUIRED  # type: ignore
 
