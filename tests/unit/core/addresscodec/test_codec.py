1c78c1056a2df40ae92ac665706317f76fdaf4c5
Mayukha Vadari <mvadari@ripple.com>
2022-07-26 14:36:37 +0200

    add wrong prefix test

--- tests/unit/core/addresscodec/test_codec.py
+++ tests/unit/core/addresscodec/test_codec.py
@@ -102,6 +102,16 @@ class TestCodec(TestCase):
         self.assertEqual(decode_result, hex_string_bytes)
         self.assertEqual(encoding_type, CryptoAlgorithm.ED25519)
 
+    def test_seed_decode_secp256k1_wrong_prefix(self):
+        encoded_string = "sEdV19BLfeQeKdEXyYA4NhjPJe6XBfG"
+
+        self.assertRaises(
+            addresscodec.XRPLAddressCodecException,
+            addresscodec.decode_seed,
+            encoded_string,
+            CryptoAlgorithm.SECP256K1,
+        )
+
     def test_seed_encode_decode_too_small(self):
         hex_string = "CF2DE378FBDD7E2EE87D486DFB5A7B"
         hex_string_bytes = bytes.fromhex(hex_string)
