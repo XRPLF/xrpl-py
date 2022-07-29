8be206fdf85fd32216d9eca402839d47b7f8495e
Mayukha Vadari <mvadari@ripple.com>
2022-07-29 23:29:28 +0200

    fix existing tests w/updated rippled, add AccountCreate test

--- tests/unit/core/binarycodec/test_main.py
+++ tests/unit/core/binarycodec/test_main.py
@@ -337,8 +337,8 @@ class TestMainFixtures(TestCase):
         test_binary = test["binary"]
         test_json = test["json"]
         with self.subTest(test_binary=test_binary, test_json=test_json):
-            self.assertEqual(encode(test_json), test_binary)
             self.assertEqual(decode(test_binary), test_json)
+            self.assertEqual(encode(test_json), test_binary)
 
     def _check_xaddress_jsons(self, test):
         x_json = test["xjson"]
