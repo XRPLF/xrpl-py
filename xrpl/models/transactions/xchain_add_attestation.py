47e11bd3b08db612ca42658ca3a69cfacc7b80c6
Mayukha Vadari <mvadari@ripple.com>
2022-08-26 16:17:15 +0200

    one more fix

--- xrpl/models/transactions/xchain_add_attestation.py
+++ xrpl/models/transactions/xchain_add_attestation.py
@@ -113,6 +113,8 @@ class XChainCreateAccountAttestationBatchElement(BaseModel):
 
     signature: str = REQUIRED  # type: ignore
 
+    signature_reward: Amount = REQUIRED  # type: ignore
+
     was_locking_chain_send: Union[Literal[0, 1]] = REQUIRED  # type: ignore
 
     xchain_account_create_count: str = REQUIRED  # type: ignore
