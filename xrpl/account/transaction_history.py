f9483a70de5906af8d6b1fc52106f98c2c56273f
Jackson Mills <jmills@ripple.com>
2022-08-31 13:55:29 -0700

    Add alternate function

--- xrpl/account/transaction_history.py
+++ xrpl/account/transaction_history.py
@@ -1,6 +1,6 @@
 """High-level methods to obtain information about account transaction history."""
 import asyncio
-from typing import Any, Dict, List, Optional
+from typing import Any, Dict, List, Optional, Tuple
 
 from xrpl.asyncio.account import transaction_history
 from xrpl.clients.sync_client import SyncClient
@@ -24,6 +24,35 @@ def get_latest_transaction(account: str, client: SyncClient) -> Response:
     return asyncio.run(transaction_history.get_latest_transaction(account, client))
 
 
+def get_account_transactions_with_marker(
+    address: str, client: SyncClient, marker: Optional[Any] = None
+) -> Tuple[List[Dict[str, Any]], Any]:
+    """
+    Query the ledger for a list of transactions that involved a given account.
+
+    Args:
+        address: the account to query.
+        client: the network client used to make network calls.
+        marker: fetches the next set of data from the server. The type of
+            marker is intentionally undefined in the spec and is chosen
+            by each server.
+
+    Returns:
+        The most recent set of transactions for this address from the marker, along
+        with a new marker if there are more results to fetch from the server. Passing
+        the marker back in will fetch the next set of results. If there are no more
+        results, the marker will be None.
+
+    Raises:
+        XRPLRequestFailureException: if the transaction fails.
+    """
+    return asyncio.run(
+        transaction_history.get_account_transactions_with_marker(
+            address, client, marker
+        )
+    )
+
+
 def get_account_transactions(
     address: str, client: SyncClient, marker: Optional[Any] = None
 ) -> List[Dict[str, Any]]:
@@ -45,6 +74,32 @@ def get_account_transactions(
     """
     return asyncio.run(
         transaction_history.get_account_transactions(address, client, marker)
+    )
+
+
+def get_account_payment_transactions_with_marker(
+    address: str, client: SyncClient, marker: Optional[Any] = None
+) -> Tuple[List[Dict[str, Any]], Any]:
+    """
+    Query the ledger for a list of payment transactions that involved a given account.
+
+    Args:
+        address: the account to query.
+        client: the network client used to make network calls.
+        marker: fetches the next set of data from the server. The type of
+            marker is intentionally undefined in the spec and is chosen
+            by each server.
+
+    Returns:
+        The payment transaction history for the address, along with a marker if there
+        are more results to fetch from the server. Passing the marker back in will
+        fetch the next set of results. If there are no more results, the marker will
+        be None.
+    """
+    return asyncio.run(
+        transaction_history.get_account_payment_transactions_with_marker(
+            address, client, marker
+        )
     )
 
 
--- xrpl/asyncio/account/transaction_history.py
+++ xrpl/asyncio/account/transaction_history.py
@@ -1,5 +1,5 @@
 """High-level methods to obtain information about account transaction history."""
-from typing import Any, Dict, List, Optional, cast
+from typing import Any, Dict, List, Optional, Tuple, cast
 
 from xrpl.asyncio.clients import Client, XRPLRequestFailureException
 from xrpl.core.addresscodec import is_valid_xaddress, xaddress_to_classic_address
@@ -32,6 +32,40 @@ async def get_latest_transaction(account: str, client: Client) -> Response:
     return response
 
 
+async def get_account_transactions_with_marker(
+    address: str, client: Client, marker: Optional[Any] = None
+) -> Tuple[List[Dict[str, Any]], Any]:
+    """
+    Query the ledger for a list of transactions that involved a given account.
+
+    Args:
+        address: the account to query.
+        client: the network client used to make network calls.
+        marker: fetches the next set of data from the server. The type of
+            marker is intentionally undefined in the spec and is chosen
+            by each server.
+
+    Returns:
+        The most recent set of transactions for this address from the marker, along
+        with a new marker if there are more results to fetch from the server. Passing
+        the marker back in will fetch the next set of results. If there are no more
+        results, the marker will be None.
+
+    Raises:
+        XRPLRequestFailureException: if the transaction fails.
+    """
+    if is_valid_xaddress(address):
+        address, _, _ = xaddress_to_classic_address(address)
+    request = AccountTx(account=address, marker=marker)
+    response = await client.request_impl(request)
+    if not response.is_successful():
+        raise XRPLRequestFailureException(response.result)
+    return (
+        cast(List[Dict[str, Any]], response.result["transactions"]),
+        response.result.get("marker"),
+    )
+
+
 async def get_account_transactions(
     address: str, client: Client, marker: Optional[Any] = None
 ) -> List[Dict[str, Any]]:
@@ -51,13 +85,38 @@ async def get_account_transactions(
     Raises:
         XRPLRequestFailureException: if the transaction fails.
     """
-    if is_valid_xaddress(address):
-        address, _, _ = xaddress_to_classic_address(address)
-    request = AccountTx(account=address, marker=marker)
-    response = await client.request_impl(request)
-    if not response.is_successful():
-        raise XRPLRequestFailureException(response.result)
-    return cast(List[Dict[str, Any]], response.result["transactions"])
+    (transactions, _) = await get_account_payment_transactions_with_marker(
+        address, client, marker
+    )
+    return transactions
+
+
+async def get_account_payment_transactions_with_marker(
+    address: str, client: Client, marker: Optional[Any] = None
+) -> Tuple[List[Dict[str, Any]], Any]:
+    """
+    Query the ledger for a list of payment transactions that involved a given account.
+
+    Args:
+        address: the account to query.
+        client: the network client used to make network calls.
+        marker: fetches the next set of data from the server. The type of
+            marker is intentionally undefined in the spec and is chosen
+            by each server.
+
+    Returns:
+        The most recent set of payment transactions for this address from the marker,
+        along with a new marker if there are more results to fetch from the server.
+        Passing the marker back in will fetch the next set of results. If there are
+        no more results, the marker will be None.
+    """
+    (all_transactions, marker) = await get_account_transactions_with_marker(
+        address, client, marker
+    )
+    return (
+        [tx for tx in all_transactions if tx["tx"]["TransactionType"] == "Payment"],
+        marker,
+    )
 
 
 async def get_account_payment_transactions(
@@ -76,5 +135,7 @@ async def get_account_payment_transactions(
     Returns:
         The payment transaction history for the address.
     """
-    all_transactions = await get_account_transactions(address, client, marker)
-    return [tx for tx in all_transactions if tx["tx"]["TransactionType"] == "Payment"]
+    (transactions, _) = await get_account_payment_transactions_with_marker(
+        address, client, marker
+    )
+    return transactions
