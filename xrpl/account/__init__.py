f5ea08b3bb950e689bea313eccd7846a215f55f7
Jackson Mills <jmills@ripple.com>
2022-08-31 14:46:00 -0700

    Fix Sphinx

--- xrpl/account/__init__.py
+++ xrpl/account/__init__.py
@@ -8,7 +8,9 @@ from xrpl.account.main import (
 )
 from xrpl.account.transaction_history import (
     get_account_payment_transactions,
+    get_account_payment_transactions_with_marker,
     get_account_transactions,
+    get_account_transactions_with_marker,
     get_latest_transaction,
 )
 
@@ -18,7 +20,9 @@ __all__ = [
     "get_account_root",
     "get_account_info",
     "get_account_payment_transactions",
+    "get_account_payment_transactions_with_marker",
     "get_account_transactions",
+    "get_account_transactions_with_marker",
     "does_account_exist",
     "get_latest_transaction",
 ]
