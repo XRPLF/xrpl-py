6c084802e60725e7f83adda56819931219526432
Connor Chen <67431889+connorjchen@users.noreply.github.com>
2022-10-31 13:55:00 -0400

    remove amm devnet support

--- tests/integration/sugar/test_wallet.py
+++ tests/integration/sugar/test_wallet.py
@@ -82,12 +82,6 @@ class TestWallet(IntegrationTestCase):
         ) as client:
             await generate_faucet_wallet_and_fund_again(self, client)
 
-    async def test_generate_faucet_wallet_amm_devnet_async_websockets(self):
-        async with AsyncWebsocketClient(
-            "wss://amm.devnet.rippletest.net:51233"
-        ) as client:
-            await generate_faucet_wallet_and_fund_again(self, client)
-
     async def test_generate_faucet_wallet_hooks_v2_testnet_async_websockets(self):
         async with AsyncWebsocketClient(
             "wss://hooks-testnet-v2.xrpl-labs.com"
--- tests/unit/asyn/wallet/test_wallet_generation.py
+++ tests/unit/asyn/wallet/test_wallet_generation.py
@@ -1,7 +1,6 @@
 from unittest import TestCase
 
 from xrpl.asyncio.wallet.wallet_generation import (
-    _AMM_DEV_FAUCET_URL,
     _DEV_FAUCET_URL,
     _HOOKS_V2_TEST_FAUCET_URL,
     _NFT_DEV_FAUCET_URL,
@@ -40,14 +39,6 @@ class TestWallet(TestCase):
         json_client_url = "https://xls20-sandbox.rippletest.net:51234"
         ws_client_url = "ws://xls20-sandbox.rippletest.net:51233"
         expected_faucet = _NFT_DEV_FAUCET_URL
-
-        self.assertEqual(get_faucet_url(json_client_url), expected_faucet)
-        self.assertEqual(get_faucet_url(ws_client_url), expected_faucet)
-
-    def test_get_faucet_wallet_amm_dev(self):
-        json_client_url = "https://amm.devnet.rippletest.net:51233"
-        ws_client_url = "wss://amm.devnet.rippletest.net:51233"
-        expected_faucet = _AMM_DEV_FAUCET_URL
 
         self.assertEqual(get_faucet_url(json_client_url), expected_faucet)
         self.assertEqual(get_faucet_url(ws_client_url), expected_faucet)
