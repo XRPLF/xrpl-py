c544ad146112fdec50eb6545cae6fc54a95fd70e
Connor Chen <67431889+connorjchen@users.noreply.github.com>
2022-11-04 14:40:31 -0400

    Change name of master wallet in snippet

--- snippets/multisigning.py
+++ snippets/multisigning.py
@@ -14,7 +14,7 @@ from xrpl.wallet import generate_faucet_wallet
 
 client = JsonRpcClient("https://s.altnet.rippletest.net:51234/")
 
-wallet_master = generate_faucet_wallet(client, debug=True)
+master_wallet = generate_faucet_wallet(client, debug=True)
 signer_wallet_1 = generate_faucet_wallet(client, debug=True)
 signer_wallet_2 = generate_faucet_wallet(client, debug=True)
 
@@ -23,12 +23,12 @@ signer_entries = [
     SignerEntry(account=signer_wallet_2.classic_address, signer_weight=1),
 ]
 signer_list_set_tx = SignerListSet(
-    account=wallet_master.classic_address,
+    account=master_wallet.classic_address,
     signer_quorum=2,
     signer_entries=signer_entries,
 )
 signed_signer_list_set_tx = safe_sign_and_autofill_transaction(
-    signer_list_set_tx, wallet_master, client
+    signer_list_set_tx, master_wallet, client
 )
 signed_list_set_tx_response = send_reliable_submission(
     signed_signer_list_set_tx, client
@@ -39,7 +39,7 @@ print(signed_list_set_tx_response)
 # Now that we've set up multisigning, let's try using it to submit an AccountSet
 # transaction.
 account_set_tx = AccountSet(
-    account=wallet_master.classic_address, domain=str_to_hex("example.com")
+    account=master_wallet.classic_address, domain=str_to_hex("example.com")
 )
 autofilled_account_set_tx = autofill(account_set_tx, client, len(signer_entries))
 print("AccountSet transaction is ready to be multisigned")
