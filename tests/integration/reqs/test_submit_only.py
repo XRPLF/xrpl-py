2e154fd4b64827daa5185c52453731bb06d4a8f3
Connor Chen <67431889+connorjchen@users.noreply.github.com>
2022-12-08 12:04:17 -0500

    Fix integ test

--- tests/integration/reqs/test_submit_only.py
+++ tests/integration/reqs/test_submit_only.py
@@ -1,7 +1,7 @@
 from tests.integration.integration_test_case import IntegrationTestCase
-from tests.integration.it_utils import JSON_RPC_CLIENT, test_async_and_sync
+from tests.integration.it_utils import test_async_and_sync
 from tests.integration.reusable_values import WALLET
-from xrpl.account import get_next_valid_seq_number
+from xrpl.asyncio.account import get_next_valid_seq_number
 from xrpl.asyncio.transaction import (
     safe_sign_and_autofill_transaction as safe_sign_and_autofill_transaction_async,
 )
@@ -10,25 +10,24 @@ from xrpl.models.amounts import IssuedCurrencyAmount
 from xrpl.models.requests import SubmitOnly
 from xrpl.models.transactions import OfferCreate
 
-TX = OfferCreate(
-    account=WALLET.classic_address,
-    sequence=get_next_valid_seq_number(WALLET.classic_address, JSON_RPC_CLIENT),
-    last_ledger_sequence=get_next_valid_seq_number(
-        WALLET.classic_address, JSON_RPC_CLIENT
-    )
-    + 10,
-    taker_gets="13100000",
-    taker_pays=IssuedCurrencyAmount(
-        currency="USD",
-        issuer=WALLET.classic_address,
-        value="10",
-    ),
-)
-
 
 class TestSubmitOnly(IntegrationTestCase):
-    @test_async_and_sync(globals())
+    @test_async_and_sync(globals(), ["xrpl.account.get_next_valid_seq_number"])
     async def test_basic_functionality(self, client):
+        TX = OfferCreate(
+            account=WALLET.classic_address,
+            sequence=await get_next_valid_seq_number(WALLET.classic_address, client),
+            last_ledger_sequence=await get_next_valid_seq_number(
+                WALLET.classic_address, client
+            )
+            + 10,
+            taker_gets="13100000",
+            taker_pays=IssuedCurrencyAmount(
+                currency="USD",
+                issuer=WALLET.classic_address,
+                value="10",
+            ),
+        )
         transaction = await safe_sign_and_autofill_transaction_async(TX, WALLET, client)
         tx_json = transaction.to_xrpl()
         tx_blob = encode(tx_json)
