6a3d08931bf75b86ca65f1b67f6d8c73e4a603ce
Mayukha Vadari <mvadari@ripple.com>
2022-12-12 12:03:12 -0500

    add validations for XChainCreateBridge

---
+++ tests/unit/models/transactions/test_xchain_create_bridge.py
@@ -0,0 +1,132 @@
+from unittest import TestCase
+
+from xrpl.models import (
+    XRP,
+    IssuedCurrency,
+    XChainBridge,
+    XChainCreateBridge,
+    XRPLModelException,
+)
+
+_ACCOUNT = "r9LqNeG6qHxjeUocjvVki2XR35weJ9mZgQ"
+_ACCOUNT2 = "rpZc4mVfWUif9CRoHRKKcmhu1nx2xktxBo"
+_FEE = "0.00001"
+_SEQUENCE = 19048
+
+_ISSUER = "rGWrZyQqhTp9Xu7G5Pkayo7bXjH4k4QYpf"
+
+_GENESIS = "rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh"
+
+
+class TestXChainCreateBridge(TestCase):
+    def test_successful_xrp_xrp_bridge(self):
+        bridge = XChainBridge(
+            locking_chain_door=_ACCOUNT,
+            locking_chain_issue=XRP(),
+            issuing_chain_door=_GENESIS,
+            issuing_chain_issue=XRP(),
+        )
+        XChainCreateBridge(
+            account=_ACCOUNT,
+            fee=_FEE,
+            sequence=_SEQUENCE,
+            xchain_bridge=bridge,
+            signature_reward="200",
+            min_account_create_amount="1000000",
+        )
+
+    def test_successful_iou_iou_bridge(self):
+        bridge = XChainBridge(
+            locking_chain_door=_ACCOUNT,
+            locking_chain_issue=IssuedCurrency(currency="USD", issuer=_ISSUER),
+            issuing_chain_door=_ACCOUNT2,
+            issuing_chain_issue=IssuedCurrency(currency="USD", issuer=_ACCOUNT2),
+        )
+        XChainCreateBridge(
+            account=_ACCOUNT,
+            fee=_FEE,
+            sequence=_SEQUENCE,
+            xchain_bridge=bridge,
+            signature_reward="200",
+        )
+
+    def test_same_door_accounts(self):
+        bridge = XChainBridge(
+            locking_chain_door=_ACCOUNT,
+            locking_chain_issue=IssuedCurrency(currency="USD", issuer=_ISSUER),
+            issuing_chain_door=_ACCOUNT,
+            issuing_chain_issue=IssuedCurrency(currency="USD", issuer=_ACCOUNT),
+        )
+        with self.assertRaises(XRPLModelException):
+            XChainCreateBridge(
+                account=_ACCOUNT,
+                fee=_FEE,
+                sequence=_SEQUENCE,
+                xchain_bridge=bridge,
+                signature_reward="200",
+            )
+
+    def test_xrp_iou_bridge(self):
+        bridge = XChainBridge(
+            locking_chain_door=_ACCOUNT,
+            locking_chain_issue=XRP(),
+            issuing_chain_door=_ACCOUNT,
+            issuing_chain_issue=IssuedCurrency(currency="USD", issuer=_ACCOUNT),
+        )
+        with self.assertRaises(XRPLModelException):
+            XChainCreateBridge(
+                account=_ACCOUNT,
+                fee=_FEE,
+                sequence=_SEQUENCE,
+                xchain_bridge=bridge,
+                signature_reward="200",
+            )
+
+    def test_iou_xrp_bridge(self):
+        bridge = XChainBridge(
+            locking_chain_door=_ACCOUNT,
+            locking_chain_issue=IssuedCurrency(currency="USD", issuer=_ISSUER),
+            issuing_chain_door=_ACCOUNT,
+            issuing_chain_issue=XRP(),
+        )
+        with self.assertRaises(XRPLModelException):
+            XChainCreateBridge(
+                account=_ACCOUNT,
+                fee=_FEE,
+                sequence=_SEQUENCE,
+                xchain_bridge=bridge,
+                signature_reward="200",
+            )
+
+    def test_account_not_in_bridge(self):
+        bridge = XChainBridge(
+            locking_chain_door=_ACCOUNT,
+            locking_chain_issue=XRP(),
+            issuing_chain_door=_ACCOUNT2,
+            issuing_chain_issue=XRP(),
+        )
+        with self.assertRaises(XRPLModelException):
+            XChainCreateBridge(
+                account=_GENESIS,
+                fee=_FEE,
+                sequence=_SEQUENCE,
+                xchain_bridge=bridge,
+                signature_reward="200",
+            )
+
+    def test_successful_iou_iou_min_account_create_amount(self):
+        bridge = XChainBridge(
+            locking_chain_door=_ACCOUNT,
+            locking_chain_issue=IssuedCurrency(currency="USD", issuer=_ISSUER),
+            issuing_chain_door=_ACCOUNT2,
+            issuing_chain_issue=IssuedCurrency(currency="USD", issuer=_ACCOUNT2),
+        )
+        with self.assertRaises(XRPLModelException):
+            XChainCreateBridge(
+                account=_ACCOUNT,
+                fee=_FEE,
+                sequence=_SEQUENCE,
+                xchain_bridge=bridge,
+                signature_reward="200",
+                min_account_create_amount="1000000",
+            )
--- xrpl/models/transactions/xchain_create_bridge.py
+++ xrpl/models/transactions/xchain_create_bridge.py
@@ -1,9 +1,12 @@
 """Model for a XChainCreateBridge transaction type."""
 
+from __future__ import annotations
+
 from dataclasses import dataclass, field
-from typing import Optional
+from typing import Dict, Optional
 
 from xrpl.models.amounts import Amount
+from xrpl.models.currencies import XRP
 from xrpl.models.required import REQUIRED
 from xrpl.models.transactions.transaction import Transaction
 from xrpl.models.transactions.types import TransactionType
@@ -26,3 +29,33 @@ class XChainCreateBridge(Transaction):
         default=TransactionType.XCHAIN_CREATE_BRIDGE,
         init=False,
     )
+
+    def _get_errors(self: XChainCreateBridge) -> Dict[str, str]:
+        errors = super()._get_errors()
+
+        bridge = self.xchain_bridge
+
+        if bridge.locking_chain_door == bridge.issuing_chain_door:
+            errors[
+                "xchain_bridge"
+            ] = "Cannot have the same door accounts on the locking and issuing chain."
+
+        if self.account not in [bridge.locking_chain_door, bridge.issuing_chain_door]:
+            errors[
+                "account"
+            ] = "Account must be either locking chain door or issuing chain door."
+
+        if (bridge.locking_chain_issue == XRP()) != (
+            bridge.issuing_chain_issue == XRP()
+        ):
+            errors["issue"] = "Bridge must be XRP-XRP or IOU-IOU."
+
+        if (
+            self.min_account_create_amount is not None
+            and bridge.locking_chain_issue != XRP()
+        ):
+            errors[
+                "min_account_create_amount"
+            ] = "Cannot have MinAccountCreateAmount if bridge is IOU-IOU."
+
+        return errors
