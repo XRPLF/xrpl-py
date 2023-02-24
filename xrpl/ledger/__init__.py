776ccb98e88712f0ec6e16610073302254bd8fa3
Denis Angell <dangell@transia.co>
2023-02-24 16:21:02 -0500

    add get_network_id func

--- xrpl/ledger/__init__.py
+++ xrpl/ledger/__init__.py
@@ -3,10 +3,12 @@ from xrpl.ledger.main import (
     get_fee,
     get_latest_open_ledger_sequence,
     get_latest_validated_ledger_sequence,
+    get_network_id,
 )
 
 __all__ = [
     "get_latest_validated_ledger_sequence",
     "get_fee",
     "get_latest_open_ledger_sequence",
+    "get_network_id",
 ]
