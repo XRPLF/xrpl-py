4bb3a31012566fe9322f85dfd875c49147b4addb
Mayukha Vadari <mvadari@ripple.com>
2023-05-03 12:55:45 -0400

    add XChainCreateBridge integration test

---
+++ tests/integration/transactions/test_xchain_create_bridge.py
@@ -0,0 +1,32 @@
+from tests.integration.integration_test_case import IntegrationTestCase
+from tests.integration.it_utils import (
+    MASTER_ACCOUNT,
+    fund_wallet_async,
+    submit_transaction_async,
+    test_async_and_sync,
+)
+from xrpl.models import XRP, XChainBridge, XChainCreateBridge
+from xrpl.wallet import Wallet
+
+
+class TestXChainCreateBridge(IntegrationTestCase):
+    @test_async_and_sync(globals())
+    async def test_basic_functionality(self, client):
+        door_wallet = Wallet.create()
+        await fund_wallet_async(door_wallet)
+        response = await submit_transaction_async(
+            XChainCreateBridge(
+                account=door_wallet.classic_address,
+                xchain_bridge=XChainBridge(
+                    locking_chain_door=door_wallet.classic_address,
+                    locking_chain_issue=XRP(),
+                    issuing_chain_door=MASTER_ACCOUNT,
+                    issuing_chain_issue=XRP(),
+                ),
+                signature_reward="200",
+                min_account_create_amount="10000000",
+            ),
+            door_wallet,
+        )
+        self.assertTrue(response.is_successful())
+        self.assertEqual(response.result["engine_result"], "tesSUCCESS")
