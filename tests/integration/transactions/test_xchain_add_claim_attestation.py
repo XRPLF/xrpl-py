3fabf6b3a05862be821825d9bb2d2456e9cd51fd
Mayukha Vadari <mvadari@ripple.com>
2023-05-03 15:44:46 -0400

    add XChainAddClaimAttestation integration test

---
+++ tests/integration/transactions/test_xchain_add_claim_attestation.py
@@ -0,0 +1,81 @@
+from tests.integration.integration_test_case import IntegrationTestCase
+from tests.integration.it_utils import submit_transaction_async, test_async_and_sync
+from tests.integration.reusable_values import BRIDGE, DESTINATION, WITNESS_WALLET
+from xrpl.core.binarycodec import encode
+from xrpl.core.keypairs import sign
+from xrpl.models import AccountInfo, XChainAddClaimAttestation, XChainCreateClaimID
+from xrpl.utils import xrp_to_drops
+from xrpl.wallet import Wallet
+
+
+class TestXChainAddClaimAttestation(IntegrationTestCase):
+    @test_async_and_sync(globals())
+    async def test_basic_functionality(self, client):
+        other_chain_source = Wallet.create().classic_address
+        claim_id_response = await submit_transaction_async(
+            XChainCreateClaimID(
+                account=DESTINATION.classic_address,
+                xchain_bridge=BRIDGE.xchain_bridge,
+                signature_reward=BRIDGE.signature_reward,
+                other_chain_source=other_chain_source,
+            ),
+            DESTINATION,
+            client,
+        )
+
+        nodes = claim_id_response.result["meta"]["AffectedNodes"]
+        created_nodes = [
+            node["CreatedNode"] for node in nodes if "CreatedNode" in node.keys()
+        ]
+        claim_ids_ledger_entries = [
+            node
+            for node in created_nodes
+            if node["LedgerEntryType"] == "XChainOwnedClaimID"
+        ]
+        assert len(claim_ids_ledger_entries) == 1, len(claim_ids_ledger_entries)
+        xchain_claim_id = claim_ids_ledger_entries[0]["NewFields"]["XChainClaimID"]
+
+        account_info1 = await client.request(
+            AccountInfo(account=DESTINATION.classic_address)
+        )
+        initial_balance = int(account_info1.result["account_data"]["Balance"])
+        amount = xrp_to_drops(300)
+
+        attestation_to_sign = {
+            "XChainBridge": BRIDGE.to_xrpl()["XChainBridge"],
+            "OtherChainSource": other_chain_source,
+            "Amount": amount,
+            "AttestationRewardAccount": WITNESS_WALLET.classic_address,
+            "WasLockingChainSend": 0,
+            "XChainClaimID": xchain_claim_id,
+            "Destination": DESTINATION.classic_address,
+        }
+        encoded_attestation = encode(attestation_to_sign)
+        attestation_signature = sign(
+            encoded_attestation,
+            WITNESS_WALLET.private_key,
+        )
+
+        response = await submit_transaction_async(
+            XChainAddClaimAttestation.from_xrpl(
+                {
+                    "Account": WITNESS_WALLET.classic_address,
+                    "AttestationSignerAccount": WITNESS_WALLET.classic_address,
+                    **attestation_to_sign,
+                    "PublicKey": WITNESS_WALLET.public_key,
+                    "Signature": attestation_signature,
+                }
+            ),
+            WITNESS_WALLET,
+            client,
+        )
+        self.assertTrue(response.is_successful())
+        self.assertEqual(response.result["meta"]["TransactionResult"], "tesSUCCESS")
+
+        account_info2 = await client.request(
+            AccountInfo(account=DESTINATION.classic_address)
+        )
+        final_balance = int(account_info2.result["account_data"]["Balance"])
+        self.assertEqual(
+            final_balance, initial_balance + int(amount) - int(BRIDGE.signature_reward)
+        )
