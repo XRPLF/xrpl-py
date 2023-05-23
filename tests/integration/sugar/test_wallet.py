6ec61b9632621dbc713f83f4e4e7aa29a4ec8bd3
Justin Reynolds <justinr1234@gmail.com>
2023-05-23 14:55:58 -0500

    fix: hooks v3 testnet

--- tests/integration/sugar/test_wallet.py
+++ tests/integration/sugar/test_wallet.py
@@ -121,12 +121,12 @@ class TestWallet(IntegrationTestCase):
         ) as client:
             await generate_faucet_wallet_and_fund_again(self, client)
 
-    async def test_generate_faucet_wallet_hooks_v2_testnet_async_websockets(self):
+    async def test_generate_faucet_wallet_hooks_v3_testnet_async_websockets(self):
         async with AsyncWebsocketClient(
-            "wss://hooks-testnet-v2.xrpl-labs.com"
+            "wss://hooks-testnet-v3.xrpl-labs.com"
         ) as client:
             global time_of_last_hooks_faucet_call
-            # Wait at least 10 seconds since last call to hooks v2 testnet faucet
+            # Wait at least 10 seconds since last call to hooks v3 testnet faucet
             time_since_last_hooks_call = time.time() - time_of_last_hooks_faucet_call
             if time_since_last_hooks_call < 10:
                 time.sleep(11 - time_since_last_hooks_call)
@@ -142,12 +142,12 @@ class TestWallet(IntegrationTestCase):
             balance = int(result.result["account_data"]["Balance"])
             self.assertTrue(balance > 0)
 
-    # Named different from test_generate_faucet_wallet_hooks_v2_testnet_async_websockets
-    # so the test runs far from each other since hooks v2 testnet faucet
+    # Named different from test_generate_faucet_wallet_hooks_v3_testnet_async_websockets
+    # so the test runs far from each other since hooks v3 testnet faucet
     # requires 10 seconds between calls
-    async def test_fund_given_wallet_hooks_v2_testnet_async_websockets(self):
+    async def test_fund_given_wallet_hooks_v3_testnet_async_websockets(self):
         async with AsyncWebsocketClient(
-            "wss://hooks-testnet-v2.xrpl-labs.com"
+            "wss://hooks-testnet-v3.xrpl-labs.com"
         ) as client:
             global time_of_last_hooks_faucet_call
             wallet = Wallet("sEdSigMti9uJFCnrkwsB3LJRGkVZHVA", 0)
@@ -158,7 +158,7 @@ class TestWallet(IntegrationTestCase):
             )
             balance = int(result.result["account_data"]["Balance"])
 
-            # Wait at least 10 seconds since last call to hooks v2 testnet faucet
+            # Wait at least 10 seconds since last call to hooks v3 testnet faucet
             time_since_last_hooks_call = time.time() - time_of_last_hooks_faucet_call
             if time_since_last_hooks_call < 10:
                 time.sleep(11 - time_since_last_hooks_call)
