335b769258062c9489b359d1d4303ba8f0a3acac
JST5000 <aim4math@gmail.com>
2023-06-15 15:25:06 -0700

    Update `autofill_and_sign` and `sign_and_submit`

--- tests/integration/sugar/test_transaction.py
+++ tests/integration/sugar/test_transaction.py
@@ -144,8 +144,8 @@ class TestTransaction(IntegrationTestCase):
                 fee=FEE,
                 destination=DESTINATION,
             ),
-            WALLET,
             client,
+            WALLET,
             check_fee=False,
         )
 
@@ -275,7 +275,7 @@ class TestSubmitAndWait(IntegrationTestCase):
             destination=DESTINATION,
         )
         payment_transaction_signed = await autofill_and_sign(
-            payment_transaction, WALLET, client
+            payment_transaction, client, WALLET
         )
         await accept_ledger_async(delay=1)
         response = await submit_and_wait(payment_transaction_signed, client)
@@ -301,7 +301,7 @@ class TestSubmitAndWait(IntegrationTestCase):
             destination=DESTINATION,
         )
         payment_transaction_signed = await autofill_and_sign(
-            payment_transaction, WALLET, client
+            payment_transaction, client, WALLET
         )
         await accept_ledger_async(delay=1)
         payment_transaction_signed_blob = encode(payment_transaction_signed.to_xrpl())
