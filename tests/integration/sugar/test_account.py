d1a46bf3141a68f596166b3bbffd38fe17e56f5e
JST5000 <aim4math@gmail.com>
2023-07-05 10:04:34 -0700

    Add classic_address

--- tests/integration/sugar/test_account.py
+++ tests/integration/sugar/test_account.py
@@ -24,7 +24,7 @@ class TestAccount(IntegrationTestCase):
 
     @test_async_and_sync(globals(), ["xrpl.account.does_account_exist"])
     async def test_does_account_exist_false(self, client):
-        address = Wallet.create()
+        address = Wallet.create().classic_address
         self.assertFalse(await does_account_exist(address, client))
 
     @test_async_and_sync(globals(), ["xrpl.account.does_account_exist"])
