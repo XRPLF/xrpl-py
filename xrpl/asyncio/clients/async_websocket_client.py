749b90bd2227cdc89be6902a5ca6f2157cad6d28
ledhed2222 <ledhed2222@users.noreply.github.com>
2023-10-04 21:39:24 -0400

    improve websocket docs and bump websockets lib version (#545)
    Adds more detail to the docs section of the async websocket client to better explain how to use it, especially for people who are coming from JS.
    
    Updates WS library
    
    ---------
    
    Co-authored-by: justinr1234 <justinr1234@gmail.com>
    Co-authored-by: jonathanlei <jonathanlei98@gmail.com>

--- xrpl/asyncio/clients/async_websocket_client.py
+++ xrpl/asyncio/clients/async_websocket_client.py
@@ -56,61 +56,152 @@ class AsyncWebsocketClient(AsyncClient, WebsocketBase):
     using the ``asyncio`` library to listen to incoming
     messages and do something with them, but the above will
     work fine if you want to listen indefinitely. This is how
-    you can use a Task to listen to messages without blocking::
+    you can use a Task to listen to messages without blocking. Note this
+    example can be copied directly into a file and run with no changes::
 
         import asyncio
 
         from xrpl.asyncio.clients import AsyncWebsocketClient
         from xrpl.models import Subscribe, Unsubscribe, StreamParameter
 
-        URL = "wss://some-url-to-connect-to"
+        URL = "wss://s.altnet.rippletest.net:51233"
 
 
-        async def listener(client):
+        def on_open():
+            print("I have opened a connection!")
+
+
+        async def on_message(client):
             async for message in client:
-                # do something with a message
+                # do something with a message - we'll just print
+                print(message)
+
+
+        def on_close():
+            print("I have closed a connection cleanly!")
+
+
+        def on_error():
+            print("An error occurred!")
+
 
         async def main():
+            # we'll use this to determine if the websocket closed cleanly or
+            # not
+            error_happened = False
+
             async with AsyncWebsocketClient(URL) as client:
-                # set up the `listener` function as a Task
-                # so that it doesn't wait for a response, but
-                # will "awaken" whenever the `asyncio` event
-                # loop toggles to it.
-                asyncio.create_task(listener(client))
+                try:
+                    # here you'll run any code that should happen immediately
+                    # after the connection is made. this is equivalent to the
+                    # javascript 'open' event
+                    on_open()
 
-                # now, the `listener` function will run as if
-                # it were "in the background", doing whatever you
-                # want as soon as it has a message.
+                    # set up the `on_message` function as a Task
+                    # so that it doesn't wait for a response, but
+                    # will "awaken" whenever the `asyncio` event
+                    # loop toggles to it. this is equivalent to the javascript
+                    # 'message' event
+                    asyncio.create_task(on_message(client))
 
-                # now let's subscribe to something. in this case,
-                # we can just use `send` instead of `request`
-                # because we don't really care about the response
-                # since the listener will also get it.
-                await client.send(Subscribe(
-                    streams=[StreamParameter.LEDGER],
-                ))
+                    # now, the `on_message` function will run as if
+                    # it were "in the background", doing whatever you
+                    # want as soon as it has a message.
 
-                # in the meantime, you can continue to do whatever
-                # you want and the python `asyncio` event loop
-                # will toggle between your code and the listener
-                # as messages are ready. let's just sleep. note,
-                # you need to use `asyncio.sleep` within
-                # async code instead of `time.sleep`, otherwise
-                # you will block all the waiting tasks instead of
-                # just this code path.
-                await asyncio.sleep(5)
+                    # now let's subscribe to something. in this case,
+                    # we can just use `send` instead of `request`
+                    # because we don't really care about the response
+                    # since the `on_message` handler will also get it.
+                    await client.send(Subscribe(
+                        streams=[StreamParameter.LEDGER],
+                    ))
+                    print("Subscribed to the ledger!")
 
-                # now that we're done, we can unsubscribe if
-                # we like
-                await client.send(Unsubscribe(
-                    streams=[StreamParameter.LEDGER],
-                ))
+                    # in the meantime, you can continue to do whatever
+                    # you want and the python `asyncio` event loop
+                    # will toggle between your code and the listener
+                    # as messages are ready. let's just sleep. note,
+                    # you need to use `asyncio.sleep` within
+                    # async code instead of `time.sleep`, otherwise
+                    # you will block all the waiting tasks instead of
+                    # just this code path.
+                    await asyncio.sleep(50)
+
+                    # now that we're done, we can unsubscribe if
+                    # we like
+                    await client.send(Unsubscribe(
+                        streams=[StreamParameter.LEDGER],
+                    ))
+                    print("Unsubscribed from the ledger!")
+                except:
+                    # if you wish you perform some logic when the websocket
+                    # connection closes due to error, you can catch and run
+                    # whatever you need to here. this is equivalent to the
+                    # javascript 'error' event
+                    error_happened = True
+                    on_error()
             # now, outside of the context, the client is closed.
-            # the listener task will now never receive a new message.
+            # the `on_message` task will now never receive a new message. you
+            # can now run any code you need to run after the connection is
+            # closed. this is equivalent to the javascript 'close' event
+            if not error_happened:
+                on_close()
+
 
         if __name__ == "__main__":
             # remember to run your entire program within a
             # `asyncio.run` call.
+            asyncio.run(main())
+
+
+    If you need to ensure that you reconnect your websockets whenever they
+    disconnect, you can create a supervisor like the example below. Note this
+    example can be copied and run directly with no changes::
+
+        import asyncio
+
+        from xrpl.asyncio.clients import AsyncWebsocketClient
+        from xrpl.models import Subscribe, StreamParameter
+
+        URL = "wss://s.altnet.rippletest.net:51233"
+
+
+        async def websocket_supervisor():
+            # whenever the websocket disconnects, for any reason, the loop
+            # will restart, reconnect, and set everything up again.
+            while True:
+                try:
+                    await long_websocket_task()
+                except:
+                    print("Lost connection! Reconnecting")
+
+
+        async def on_message(client):
+            async for message in client:
+                print(message)
+
+
+        async def long_websocket_task():
+            async with AsyncWebsocketClient(URL) as client:
+                # set up a listener task
+                listener = asyncio.create_task(on_message(client))
+
+                # subscribe to the ledger
+                await client.send(Subscribe(
+                    streams=[StreamParameter.LEDGER],
+                ))
+
+                # sleep infinitely until the connection closes on us
+                while client.is_open():
+                    await asyncio.sleep(0)
+                listener.cancel()
+
+
+        async def main():
+            await websocket_supervisor()
+
+
+        if __name__ == "__main__":
             asyncio.run(main())
     """
 
