d7541d5bd2bc884e4d0488d7bc34d22730a5d723
Mayukha Vadari <mvadari@gmail.com>
2023-10-23 12:31:16 -0400

    fix model, add more validation

--- xrpl/models/transactions/did_set.py
+++ xrpl/models/transactions/did_set.py
@@ -2,12 +2,17 @@
 
 from __future__ import annotations
 
+import re
 from dataclasses import dataclass, field
-from typing import Optional
+from typing import Dict, Optional, Pattern
+
+from typing_extensions import Final
 
 from xrpl.models.transactions.transaction import Transaction
 from xrpl.models.transactions.types import TransactionType
 from xrpl.models.utils import require_kwargs_on_init
+
+HEX_REGEX: Final[Pattern[str]] = re.compile("[A-F0-9]+")
 
 
 @require_kwargs_on_init
@@ -15,11 +20,30 @@ from xrpl.models.utils import require_kwargs_on_init
 class DIDSet(Transaction):
     """Represents a DIDSet transaction."""
 
-    attestation: Optional[str] = None
     did_document: Optional[str] = None
+    data: Optional[str] = None
     uri: Optional[str] = None
 
     transaction_type: TransactionType = field(
         default=TransactionType.DID_SET,
         init=False,
     )
+
+    def _get_errors(self: DIDSet) -> Dict[str, str]:
+        errors = super()._get_errors()
+
+        def _process_field(name: str, value: Optional[str]) -> None:
+            if value is not None:
+                error_strs = []
+                if not bool(HEX_REGEX.fullmatch(value)):
+                    error_strs.append("is not hex")
+                if len(value) > 256:
+                    error_strs.append("is too long")
+                if len(error_strs) > 0:
+                    errors[name] = f"`{name}` " + " and ".join(error_strs) + "."
+
+        _process_field("did_document", self.did_document)
+        _process_field("data", self.data)
+        _process_field("uri", self.uri)
+
+        return errors
