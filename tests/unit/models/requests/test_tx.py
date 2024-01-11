e7e3134225fff7e415a136efeaf9fde2b8d27b3d
Chenna Keshava <ckeshavabs@gmail.com>
2024-01-11 12:32:23 -0800

    fix DGE-2049: add CTID field in the tx command request

---
+++ tests/unit/models/requests/test_tx.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+
+from xrpl.models.exceptions import XRPLModelException
+from xrpl.models.requests import Tx
+
+_TRANSACTION_HASH = "C53ECF838647FA5A4C780377025FEC7999AB4182590510CA461444B207AB74A9"
+_CTID = "C005523E00000000"
+
+
+class TestTx(TestCase):
+    def test_invalid_input__ctid_and_txnhash(self):
+        with self.assertRaises(XRPLModelException):
+            Tx(transaction=_TRANSACTION_HASH, ctid=_CTID)
+
+    # Note: This test merely verifies the semantic correctness of the Request.
+    # It does not verify if the specified CTID exists.
+    def test_valid_ctid(self):
+        request = Tx(ctid=_CTID)
+        self.assertTrue(request.is_valid())
+
+    def test_valid_txn_hash(self):
+        request = Tx(transaction=_TRANSACTION_HASH)
+        self.assertTrue(request.is_valid())
--- xrpl/models/requests/tx.py
+++ xrpl/models/requests/tx.py
@@ -3,12 +3,12 @@ The tx method retrieves information on a single transaction.
 
 `See tx <https://xrpl.org/tx.html>`_
 """
+from __future__ import annotations
 
 from dataclasses import dataclass, field
-from typing import Optional
+from typing import Dict, Optional
 
 from xrpl.models.requests.request import Request, RequestMethod
-from xrpl.models.required import REQUIRED
 from xrpl.models.utils import require_kwargs_on_init
 
 
@@ -17,18 +17,28 @@ from xrpl.models.utils import require_kwargs_on_init
 class Tx(Request):
     """
     The tx method retrieves information on a single transaction.
+    The Request must contain either transaction or CTID parameter, but not both.
 
     `See tx <https://xrpl.org/tx.html>`_
     """
 
     method: RequestMethod = field(default=RequestMethod.TX, init=False)
-    transaction: str = REQUIRED  # type: ignore
-    """
-    This field is required.
-
-    :meta hide-value:
-    """
-
+    transaction: Optional[str] = None
     binary: bool = False
     min_ledger: Optional[int] = None
     max_ledger: Optional[int] = None
+    ctid: Optional[str] = None
+
+    def _get_errors(self: Tx) -> Dict[str, str]:
+        errors = super()._get_errors()
+        if not self._has_only_one_input():
+            errors[
+                "Tx"
+            ] = "Must have only one of `ctid` or `transaction`, but not both."
+        return errors
+
+    def _has_only_one_input(self: Tx) -> bool:
+        present_items = [
+            item for item in [self.transaction, self.ctid] if item is not None
+        ]
+        return len(present_items) == 1
