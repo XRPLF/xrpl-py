a430f1f59ef9d092e024e60bd506432173f6acf2
Chenna Keshava <ckeshavabs@gmail.com>
2024-01-18 16:01:54 -0800

    include integration tests for verifying the currency codes

--- tests/integration/transactions/test_trust_set.py
+++ tests/integration/transactions/test_trust_set.py
@@ -27,3 +27,51 @@ class TestTrustSet(IntegrationTestCase):
             client,
         )
         self.assertTrue(response.is_successful())
+
+    @test_async_and_sync(globals())
+    async def test_special_chars_curr_codes(self, client):
+        issuer_wallet = Wallet.create()
+        response = await sign_and_reliable_submission_async(
+            TrustSet(
+                account=WALLET.address,
+                flags=TrustSetFlag.TF_SET_NO_RIPPLE,
+                limit_amount=IssuedCurrencyAmount(
+                    issuer=issuer_wallet.address,
+                    currency="$$$",
+                    value="100",
+                ),
+            ),
+            WALLET,
+            client,
+        )
+        self.assertTrue(response.is_successful())
+
+        response = await sign_and_reliable_submission_async(
+            TrustSet(
+                account=WALLET.address,
+                flags=TrustSetFlag.TF_SET_NO_RIPPLE,
+                limit_amount=IssuedCurrencyAmount(
+                    issuer=issuer_wallet.address,
+                    currency="^%#",
+                    value="100",
+                ),
+            ),
+            WALLET,
+            client,
+        )
+        self.assertTrue(response.is_successful())
+
+        response = await sign_and_reliable_submission_async(
+            TrustSet(
+                account=WALLET.address,
+                flags=TrustSetFlag.TF_SET_NO_RIPPLE,
+                limit_amount=IssuedCurrencyAmount(
+                    issuer=issuer_wallet.address,
+                    currency="a1@",
+                    value="100",
+                ),
+            ),
+            WALLET,
+            client,
+        )
+        self.assertTrue(response.is_successful())
