e0c59b7362508fe0b7bdd8c715eedf53e016d22f
Omar Khan <khancodegt@gmail.com>
2024-01-25 12:55:07 -0500

    update Check

--- xrpl/models/ledger_objects/check.py
+++ xrpl/models/ledger_objects/check.py
@@ -18,20 +18,88 @@ class Check(LedgerObject):
     """The model for the `Check` Ledger Object"""
 
     account: str = REQUIRED  # type: ignore
+    """
+    The sender of the Check. Cashing the Check debits this address's balance.
+    This field is required.
+    """
+
     destination: str = REQUIRED  # type: ignore
-    # always 0
-    flags: int = REQUIRED  # type: ignore
-    owner_node: str = REQUIRED  # type: ignore
-    previous_txn_id: str = REQUIRED  # type: ignore
-    previous_txn_lgr_seq: int = REQUIRED  # type: ignore
-    send_max: Union[str, IssuedCurrencyAmount] = REQUIRED  # type: ignore
-    sequence: int = REQUIRED  # type: ignore
+    """
+    The intended recipient of the Check. Only this address can cash the Check, using a
+    `CheckCash` transaction. This field is required.
+    """
+
     destination_node: Optional[str] = None
+    """
+    A hint indicating which page of the destination's owner directory links to this
+    object, in case the directory consists of multiple pages.
+    """
+
     destination_tag: Optional[int] = None
+    """
+    An arbitrary tag to further specify the destination for this Check, such as a
+    hosted recipient at the destination address.
+    """
+
     expiration: Optional[int] = None
+    """
+    Indicates the time after which this Check is considered expired. See Specifying
+    Time for details.
+    """
+
     invoice_id: Optional[str] = None
+    """
+    Arbitrary 256-bit hash provided by the sender as a specific reason or identifier
+    for this Check.
+    """
+
+    owner_node: str = REQUIRED  # type: ignore
+    """
+    A hint indicating which page of the sender's owner directory links to this object,
+    in case the directory consists of multiple pages. This field is required.
+    """
+
+    previous_txn_id: str = REQUIRED  # type: ignore
+    """
+    The identifying hash of the transaction that most recently modified this object.
+    This field is required.
+    """
+
+    previous_txn_lgr_seq: int = REQUIRED  # type: ignore
+    """
+    The index of the ledger that contains the transaction that most recently modified
+    this object. This field is required.
+    """
+
+    send_max: Union[str, IssuedCurrencyAmount] = REQUIRED  # type: ignore
+    """
+    The maximum amount of currency this Check can debit the sender. If the Check is
+    successfully cashed, the destination is credited in the same currency for up to
+    this amount. This field is required.
+    """
+
+    sequence: int = REQUIRED  # type: ignore
+    """
+    The sequence number of the `CheckCreate` transaction that created this check.
+    This field is required.
+    """
+
     source_tag: Optional[int] = None
+    """
+    An arbitrary tag to further specify the source for this Check, such as a hosted
+    recipient at the sender's address.
+    """
+
+    flags: int = REQUIRED  # type: ignore
+    """
+    Flags is always 0 since there are no flags defined for Check entries.
+    """
+
     ledger_entry_type: LedgerEntryType = field(
         default=LedgerEntryType.CHECK,
         init=False,
     )
+    """
+    The value 0x0043, mapped to the string `Check`, indicates that this object is a
+    Check object.
+    """
