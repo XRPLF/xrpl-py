587d3e7beec3a925569fc0e4d2e5241679b0887f
Chenna Keshava <ckeshavabs@gmail.com>
2024-02-15 15:12:28 -0800

    use a context manager in KW_ONLY unit tests. Makes the code much more readable

--- tests/unit/models/test_utils.py
+++ tests/unit/models/test_utils.py
@@ -3,22 +3,12 @@ from unittest import TestCase
 from xrpl.models.currencies import IssuedCurrency
 from xrpl.models.exceptions import XRPLModelException
 from xrpl.models.requests import AccountInfo
-from xrpl.models.transactions import AccountSet, AccountSetAsfFlag, Payment, PaymentFlag
+from xrpl.models.transactions import Payment, PaymentFlag
 from xrpl.models.utils import _is_kw_only_attr_defined_in_dataclass
 
 _ACCOUNT = "r9LqNeG6qHxjeUocjvVki2XR35weJ9mZgQ"
 _FEE = "0.00001"
 _SEQUENCE = 19048
-_DOMAIN = "asjcsodafsaid0f9asdfasdf"
-_TRANSACTION = AccountSet(
-    account=_ACCOUNT,
-    fee=_FEE,
-    set_flag=AccountSetAsfFlag.ASF_DISALLOW_XRP,
-    domain=_DOMAIN,
-    sequence=_SEQUENCE,
-)
-
-_SEED = "randomsecretseedforakey"
 
 currency = "BTC"
 issuer = "r9LqNeG6qHxjeUocjvVki2XR35weJ9mZgQ"
@@ -27,31 +17,32 @@ _DESTINATION = "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn"
 _XRP_AMOUNT = "10000"
 
 
+class _KW_only_test_context_manager:
+    def __init__(self):
+        # Newer versions of Python returns a TypeError, unlike the older versions
+        self.error_type = (
+            TypeError if _is_kw_only_attr_defined_in_dataclass() else XRPLModelException
+        )
+
+    # Depending on the version of Python's interpreter, the correct exception type is
+    # used for validation
+    def __enter__(self):
+        return self.error_type
+
+    def __exit__(self, type, value, traceback):
+        # upon exit, there is no file or resource to gracefully close
+        pass
+
+
 class TestUtils(TestCase):
     def test_kwargs_req(self):
-        if not _is_kw_only_attr_defined_in_dataclass():
-            with self.assertRaises(XRPLModelException):
-                IssuedCurrency(currency, issuer)
-        else:
-            with self.assertRaises(TypeError):
+        with _KW_only_test_context_manager() as exception_type:
+            with self.assertRaises(exception_type):
                 IssuedCurrency(currency, issuer)
 
     def test_throws_if_positional_args_mixed_with_non_positional_args(self):
-        if not _is_kw_only_attr_defined_in_dataclass():
-            with self.assertRaises(XRPLModelException):
-                Payment(
-                    20,
-                    True,
-                    account=_ACCOUNT,
-                    fee=_FEE,
-                    sequence=_SEQUENCE,
-                    amount=_XRP_AMOUNT,
-                    send_max=_XRP_AMOUNT,
-                    destination=_DESTINATION,
-                    flags=PaymentFlag.TF_PARTIAL_PAYMENT,
-                )
-        else:
-            with self.assertRaises(TypeError):
+        with _KW_only_test_context_manager() as exception_type:
+            with self.assertRaises(exception_type):
                 Payment(
                     20,
                     True,
@@ -65,15 +56,8 @@ class TestUtils(TestCase):
                 )
 
     def test_positional_args_in_model_constructor_throws(self):
-        if not _is_kw_only_attr_defined_in_dataclass():
-            with self.assertRaises(XRPLModelException):
-                AccountInfo(
-                    "invalidInput",
-                    [1, 2, "example invalid positional arg"],
-                    account=_ACCOUNT,
-                )
-        else:
-            with self.assertRaises(TypeError):
+        with _KW_only_test_context_manager() as exception_type:
+            with self.assertRaises(exception_type):
                 AccountInfo(
                     "invalidInput",
                     [1, 2, "example invalid positional arg"],
