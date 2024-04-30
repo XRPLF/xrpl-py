e811c415f87fcc4495aa5ce8e176e419fe9dec54
Chenna Keshava <ckeshavabs@gmail.com>
2024-04-29 17:08:59 -0700

    [WIP] updates to the SetOracle integration test

--- tests/integration/transactions/test_set_oracle.py
+++ tests/integration/transactions/test_set_oracle.py
@@ -8,9 +8,11 @@ from tests.integration.it_utils import (
 from tests.integration.reusable_values import WALLET
 from xrpl.models import AccountObjects, AccountObjectType, OracleSet
 from xrpl.models.response import ResponseStatus
+from xrpl.models.transactions.set_oracle import PriceData
+from xrpl.utils import str_to_hex
 
-_PROVIDER = "chainlink"
-_ASSET_CLASS = "currency"
+_PROVIDER = str_to_hex("provider")
+_ASSET_CLASS = str_to_hex("currency")
 
 
 class TestSetOracle(IntegrationTestCase):
@@ -22,7 +24,14 @@ class TestSetOracle(IntegrationTestCase):
             provider=_PROVIDER,
             asset_class=_ASSET_CLASS,
             last_update_time=int(time.time()),
-            price_data_series=[],
+            price_data_series=[
+                PriceData(
+                    base_asset="XRP", quote_asset="USD", asset_price=740, scale=1
+                ),
+                PriceData(
+                    base_asset="BTC", quote_asset="EUR", asset_price=100, scale=2
+                ),
+            ],
         )
         response = await sign_and_reliable_submission_async(tx, WALLET, client)
         self.assertEqual(response.status, ResponseStatus.SUCCESS)
@@ -33,5 +42,4 @@ class TestSetOracle(IntegrationTestCase):
             AccountObjects(account=WALLET.address, type=AccountObjectType.ORACLE)
         )
 
-        # CK: Use a more stringent check to validate the Oracle object
         self.assertEqual(len(account_objects_response.result["account_objects"]), 1)
