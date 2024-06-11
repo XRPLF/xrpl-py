a5c6c957ed3b34652aebc200846ae3e1df039ddb
Zhiyuan Wang <96991820+Kassaking7@users.noreply.github.com>
2024-06-11 14:42:16 -0400

    Add nfts_by_issuer data type (#707)
    * add nfts_by_issuer
    
    * update Changelog
    
    * added nfts_by_issuer to Request's get_method
    
    * reformat changelog and add required comment for nfts_by_issuer.py
    
    ---------
    
    Co-authored-by: Kassaking <kassaking7@gmail.com>

--- xrpl/models/requests/request.py
+++ xrpl/models/requests/request.py
@@ -62,7 +62,7 @@ class RequestMethod(str, Enum):
     NFT_SELL_OFFERS = "nft_sell_offers"
     NFT_INFO = "nft_info"  # clio only
     NFT_HISTORY = "nft_history"  # clio only
-
+    NFTS_BY_ISSUER = "nfts_by_issuer"  # clio only
     # subscription methods
     SUBSCRIBE = "subscribe"
     UNSUBSCRIBE = "unsubscribe"
@@ -179,7 +179,8 @@ class Request(BaseModel):
             return xrpl.models.requests.NFTInfo
         if method == RequestMethod.NFT_HISTORY:
             return xrpl.models.requests.NFTHistory
-
+        if method == RequestMethod.NFTS_BY_ISSUER:
+            return xrpl.models.requests.NFTsByIssuer
         parsed_name = "".join([word.capitalize() for word in method.split("_")])
         if parsed_name in xrpl.models.requests.__all__:
             return cast(Type[Request], getattr(xrpl.models.requests, parsed_name))
