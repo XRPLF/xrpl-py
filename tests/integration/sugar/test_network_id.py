f136db7fca98db1b968d0997a1c1756b4b9fc658
Chenna Keshava B S <ckeshavabs@gmail.com>
2024-07-08 15:24:14 -0700

    revert the changes pertaining to introducing network ID in the config file

---
+++ tests/integration/sugar/test_network_id.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+
+from xrpl.asyncio.transaction.main import _RESTRICTED_NETWORKS
+from xrpl.clients import WebsocketClient
+from xrpl.models.transactions import AccountSet
+from xrpl.transaction import autofill
+
+_FEE = "0.00001"
+
+
+# TODO: move to test_transaction and use the standard integration test setup
+class TestNetworkID(TestCase):
+    # Autofill should override tx networkID for network with ID > 1024
+    # and build_version from 1.11.0 or later.
+    def test_networkid_override(self):
+        with WebsocketClient("wss://hooks-testnet-v3.xrpl-labs.com") as client:
+            tx = AccountSet(
+                account="rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh",
+                fee=_FEE,
+                domain="www.example.com",
+            )
+            tx_autofilled = autofill(tx, client)
+            self.assertGreaterEqual(client.network_id, _RESTRICTED_NETWORKS)
+            self.assertEqual(tx_autofilled.network_id, client.network_id)
