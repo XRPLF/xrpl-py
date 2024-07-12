7127172b8f9302eb3539c1b4579ab485cbbbdb01
Kassaking7 <1830604455@qq.com>
2024-07-12 15:21:58 -0400

    create DNFT support

--- xrpl/models/transactions/types/transaction_type.py
+++ xrpl/models/transactions/types/transaction_type.py
@@ -29,6 +29,7 @@ class TransactionType(str, Enum):
     NFTOKEN_CANCEL_OFFER = "NFTokenCancelOffer"
     NFTOKEN_CREATE_OFFER = "NFTokenCreateOffer"
     NFTOKEN_MINT = "NFTokenMint"
+    NFTOKEN_MODIFY = "NFTokenModify"
     OFFER_CANCEL = "OfferCancel"
     OFFER_CREATE = "OfferCreate"
     ORACLE_SET = "OracleSet"
