505172829af9a46fe02c574f4bee3193d18a2e97
Omar Khan <khancodegt@gmail.com>
2024-07-19 19:25:13 -0400

    add MPTokenIssuanceCreate

---
+++ tests/unit/models/transactions/test_mptoken_issuance_create.py
@@ -0,0 +1,45 @@
+# from sys import maxsize
+from unittest import TestCase
+
+from xrpl.models.exceptions import XRPLModelException
+from xrpl.models.transactions import MPTokenIssuanceCreate
+from xrpl.models.transactions.mptoken_issuance_create import MPTokenIssuanceCreateFlag
+
+_ACCOUNT = "r9LqNeG6qHxjeUocjvVki2XR35weJ9mZgQ"
+
+
+class TestMPTokenIssuanceCreate(TestCase):
+    def test_tx_is_valid(self):
+        tx = MPTokenIssuanceCreate(
+            account=_ACCOUNT,
+            maximum_amount="0x7fffffffffffffff",
+            asset_scale=2,
+            transfer_fee=1,
+            flags=2,
+            mptoken_metadata="http://xrpl.org".encode("utf-8").hex(),
+        )
+        self.assertTrue(tx.is_valid())
+
+    def test_mptoken_metadata_empty_string(self):
+        with self.assertRaises(XRPLModelException) as error:
+            MPTokenIssuanceCreate(
+                account=_ACCOUNT,
+                flags=MPTokenIssuanceCreateFlag.TF_MPT_CAN_LOCK,
+                mptoken_metadata="",
+            )
+        self.assertEqual(
+            error.exception.args[0],
+            "{'mptoken_metadata': 'Field must not be empty string.'}",
+        )
+
+    def test_mptoken_metadata_not_hex(self):
+        with self.assertRaises(XRPLModelException) as error:
+            MPTokenIssuanceCreate(
+                account=_ACCOUNT,
+                flags=MPTokenIssuanceCreateFlag.TF_MPT_CAN_LOCK,
+                mptoken_metadata="http://xrpl.org",
+            )
+        self.assertEqual(
+            error.exception.args[0],
+            "{'mptoken_metadata': 'Field must be in hex format.'}",
+        )
