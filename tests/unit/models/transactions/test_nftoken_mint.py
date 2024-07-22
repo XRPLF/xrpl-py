1fa153255217cbfedc7b354db143c40a4b3a517a
xVet <vet@xrp.vet>
2024-07-22 04:16:24 +0200

    modify ntoken_mint

--- tests/unit/models/transactions/test_nftoken_mint.py
+++ tests/unit/models/transactions/test_nftoken_mint.py
@@ -5,6 +5,7 @@ from xrpl.models.exceptions import XRPLModelException
 from xrpl.models.transactions import NFTokenMint
 
 _ACCOUNT = "r9LqNeG6qHxjeUocjvVki2XR35weJ9mZgQ"
+_ANOTHER_ACCOUNT = "rsA2LpzuawewSBQXkiju3YQTMzW13pAAdW"
 _FEE = "0.00001"
 _SEQUENCE = 19048
 
@@ -38,4 +39,36 @@ class TestNFTokenMint(TestCase):
                 sequence=_SEQUENCE,
                 nftoken_taxon=0,
                 uri=_ACCOUNT * 1000,
+            )
+
+    def test_nftoken_mint_sell_offer_with_zero_amount(self):
+        with self.assertRaises(XRPLModelException):
+            NFTokenMint(
+                account=_ACCOUNT,
+                fee=_FEE,
+                sequence=_SEQUENCE,
+                nftoken_taxon=0,
+                amount="0",
+            )
+
+    def test_destination_is_account(self):
+        with self.assertRaises(XRPLModelException):
+            NFTokenMint(
+                account=_ACCOUNT,
+                destination=_ACCOUNT,
+                fee=_FEE,
+                sequence=_SEQUENCE,
+                nftoken_taxon=0,
+                amount="1",
+            )
+
+    def test_nftoken_mint_sell_offer_with_negative_amount(self):
+        with self.assertRaises(XRPLModelException):
+            NFTokenMint(
+                account=_ACCOUNT,
+                destination=_ANOTHER_ACCOUNT,
+                fee=_FEE,
+                sequence=_SEQUENCE,
+                nftoken_taxon=0,
+                amount="-1",
             )
