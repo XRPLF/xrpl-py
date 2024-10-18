0a33121ba029f684ca6139dbb3b0624ad1f95d31
Chenna Keshava B S <ckeshavabs@gmail.com>
2024-10-17 20:34:19 -0700

    Files pertaining to DepositPreauth transaction are included in this commit
    Deposit_preauth: array length checks on the authcreds and unauthcreds fields

--- tests/integration/transactions/test_deposit_preauth.py
+++ tests/integration/transactions/test_deposit_preauth.py
@@ -6,6 +6,8 @@ from tests.integration.it_utils import (
 from tests.integration.reusable_values import WALLET
 from xrpl.models.response import ResponseStatus
 from xrpl.models.transactions import DepositPreauth
+from xrpl.models.transactions.deposit_preauth import Credential
+from xrpl.utils import str_to_hex
 
 ACCOUNT = WALLET.address
 ADDRESS = "rEhxGqkqPPSxQ3P25J66ft5TwpzV14k2de"
@@ -28,6 +30,32 @@ class TestDepositPreauth(IntegrationTestCase):
         deposit_preauth = DepositPreauth(
             account=ACCOUNT,
             unauthorize=ADDRESS,
+        )
+        response = await sign_and_reliable_submission_async(
+            deposit_preauth, WALLET, client
+        )
+        self.assertEqual(response.status, ResponseStatus.SUCCESS)
+
+    @test_async_and_sync(globals())
+    async def test_credentials_array_input_fields(self, client):
+        sample_credentials = [
+            Credential(issuer=ADDRESS, credential_type=str_to_hex("SampleCredType"))
+        ]
+
+        # Test the authorize_credentials input field
+        deposit_preauth = DepositPreauth(
+            account=ACCOUNT,
+            authorize_credentials=sample_credentials,
+        )
+        response = await sign_and_reliable_submission_async(
+            deposit_preauth, WALLET, client
+        )
+        self.assertEqual(response.status, ResponseStatus.SUCCESS)
+
+        # Test the unauthorize_credentials input field
+        deposit_preauth = DepositPreauth(
+            account=ACCOUNT,
+            unauthorize_credentials=sample_credentials,
         )
         response = await sign_and_reliable_submission_async(
             deposit_preauth, WALLET, client
