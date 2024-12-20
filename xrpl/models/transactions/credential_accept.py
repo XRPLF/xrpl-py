24dcb32d213da21d0a65a5da1199143852f7624e
Chenna Keshava B S <21219765+ckeshava@users.noreply.github.com>
2024-12-20 08:58:28 +0530

    Update xrpl/models/transactions/credential_accept.py
    Co-authored-by: Omar Khan <khancodegt@gmail.com>

--- xrpl/models/transactions/credential_accept.py
+++ xrpl/models/transactions/credential_accept.py
@@ -40,7 +40,7 @@ class CredentialAccept(Transaction):
 
     credential_type: str = REQUIRED  # type: ignore
     """
-    A (hex-encoded) value to identify the type of credential from the issuer.
+    A hex-encoded value to identify the type of credential from the issuer.
     """
 
     def _get_errors(self: Self) -> Dict[str, str]:
