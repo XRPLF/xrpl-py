6966ae71c18dbc363dfa58187d873eb4bfa1ecbe
Chenna Keshava B S <ckeshavabs@gmail.com>
2025-02-27 15:53:13 -0800

    add integ test for VaultClawback transaction

--- tests/integration/transactions/test_sav.py
+++ tests/integration/transactions/test_sav.py
@@ -6,7 +6,9 @@ from tests.integration.it_utils import (
 )
 from tests.integration.reusable_values import WALLET
 from xrpl.models import (
+    Payment,
     TrustSet,
+    VaultClawback,
     VaultCreate,
     VaultDelete,
     VaultDeposit,
@@ -14,7 +16,7 @@ from xrpl.models import (
     VaultWithdraw,
 )
 from xrpl.models.amounts.issued_currency_amount import IssuedCurrencyAmount
-from xrpl.models.currencies import XRP, IssuedCurrency
+from xrpl.models.currencies import IssuedCurrency
 from xrpl.models.requests import AccountObjects
 from xrpl.models.requests.account_objects import AccountObjectType
 from xrpl.models.response import ResponseStatus
@@ -29,22 +31,36 @@ class TestSingleAssetVault(IntegrationTestCase):
         vault_owner = Wallet.create()
         await fund_wallet_async(vault_owner)
 
-        # # Prerequisites: Set up the IOU trust lines
-        # tx = TrustSet(
-        #     account=vault_owner.address,
-        #     limit_amount=IssuedCurrencyAmount(
-        #         currency="USD", issuer=WALLET.address, value="1000"
-        #     ),
-        # )
-        # response = await sign_and_reliable_submission_async(tx, vault_owner, client)
-        # self.assertEqual(response.status, ResponseStatus.SUCCESS)
-        # self.assertEqual(response.result["engine_result"], "tesSUCCESS")
+        issuer_wallet = Wallet.create()
+        await fund_wallet_async(issuer_wallet)
+
+        # Step-0.a: Prerequisites: Set up the IOU trust line
+        tx = TrustSet(
+            account=WALLET.address,
+            limit_amount=IssuedCurrencyAmount(
+                currency="USD", issuer=issuer_wallet.address, value="1000"
+            ),
+        )
+        response = await sign_and_reliable_submission_async(tx, WALLET, client)
+        self.assertEqual(response.status, ResponseStatus.SUCCESS)
+        self.assertEqual(response.result["engine_result"], "tesSUCCESS")
+
+        # Step-0.b: Send the payment of IOUs from issuer_wallet to WALLET
+        tx = Payment(
+            account=issuer_wallet.address,
+            amount=IssuedCurrencyAmount(
+                currency="USD", issuer=issuer_wallet.address, value="1000"
+            ),
+            destination=WALLET.address,
+        )
+        response = await sign_and_reliable_submission_async(tx, issuer_wallet, client)
+        self.assertEqual(response.status, ResponseStatus.SUCCESS)
+        self.assertEqual(response.result["engine_result"], "tesSUCCESS")
 
         # Step-1: Create a vault
         tx = VaultCreate(
             account=vault_owner.address,
-            asset=XRP(),
-            # asset=IssuedCurrency(currency="USD", issuer=WALLET.address),
+            asset=IssuedCurrency(currency="USD", issuer=issuer_wallet.address),
             # TODO: This throws a Number::normalize 1 exception in rippled, why ??
             # Possible errors in serialization of Number type
             # asset_maximum="1000",
@@ -75,34 +91,45 @@ class TestSingleAssetVault(IntegrationTestCase):
         tx = VaultDeposit(
             account=WALLET.address,
             vault_id=VAULT_ID,
-            amount="10",
-            # amount=IssuedCurrencyAmount(
-            #     currency="USD", issuer=WALLET.address, value="10"
-            # ),
+            amount=IssuedCurrencyAmount(
+                currency="USD", issuer=issuer_wallet.address, value="10"
+            ),
         )
         response = await sign_and_reliable_submission_async(tx, WALLET, client)
         self.assertEqual(response.status, ResponseStatus.SUCCESS)
         self.assertEqual(response.result["engine_result"], "tesSUCCESS")
 
-        # Execute a VaultWithdraw transaction
+        # Step-4: Execute a VaultWithdraw transaction
         tx = VaultWithdraw(
             account=WALLET.address,
             vault_id=VAULT_ID,
-            amount="10",
-            # amount=IssuedCurrencyAmount(
-            #     currency="USD", issuer=WALLET.address, value="10"
-            # ),
+            amount=IssuedCurrencyAmount(
+                currency="USD", issuer=issuer_wallet.address, value="9"
+            ),
         )
         response = await sign_and_reliable_submission_async(tx, WALLET, client)
         self.assertEqual(response.status, ResponseStatus.SUCCESS)
         self.assertEqual(response.result["engine_result"], "tesSUCCESS")
 
-        # TODO: Execute a VaultClawback transaction
+        # Step-5: Execute a VaultClawback transaction from issuer_wallet
+        tx = VaultClawback(
+            holder=WALLET.address,
+            account=issuer_wallet.address,
+            vault_id=VAULT_ID,
+            # Note: Although the amount is specified as 9, 1 unit of the IOU will be
+            # clawed back, because that is the remaining balance in the vault
+            amount=IssuedCurrencyAmount(
+                currency="USD", issuer=issuer_wallet.address, value="9"
+            ),
+        )
+        response = await sign_and_reliable_submission_async(tx, issuer_wallet, client)
+        self.assertEqual(response.status, ResponseStatus.SUCCESS)
+        self.assertEqual(response.result["engine_result"], "tesSUCCESS")
 
-        # Delete the Vault with VaultDelete transaction
+        # Step-6: Delete the Vault with VaultDelete transaction
         tx = VaultDelete(
             account=vault_owner.address,
-            vault_id=account_objects_response.result["account_objects"][0]["index"],
+            vault_id=VAULT_ID,
         )
         response = await sign_and_reliable_submission_async(tx, vault_owner, client)
         self.assertEqual(response.status, ResponseStatus.SUCCESS)
