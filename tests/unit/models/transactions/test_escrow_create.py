09f98af7dcdebd9b5fec70bc09d3d5f2bdd3803e
Mayukha Vadari <mvadari@gmail.com>
2025-03-11 14:13:12 -0400

    add EscrowCancel

--- tests/unit/models/transactions/test_escrow_create.py
+++ tests/unit/models/transactions/test_escrow_create.py
@@ -5,6 +5,30 @@ from xrpl.models.transactions import EscrowCreate
 
 
 class TestEscrowCreate(TestCase):
+    def test_all_fields_valid(self):
+        account = "r9LqNeG6qHxjeUocjvVki2XR35weJ9mZgQ"
+        amount = "amount"
+        cancel_after = 3
+        destination = "destination"
+        destination_tag = 1
+        fee = "0.00001"
+        finish_after = 2
+        finish_function = "abcdef"
+        condition = "abcdef"
+
+        escrow_create = EscrowCreate(
+            account=account,
+            amount=amount,
+            destination=destination,
+            destination_tag=destination_tag,
+            fee=fee,
+            cancel_after=cancel_after,
+            finish_after=finish_after,
+            finish_function=finish_function,
+            condition=condition,
+        )
+        self.assertTrue(escrow_create.is_valid())
+
     def test_final_after_less_than_cancel_after(self):
         account = "r9LqNeG6qHxjeUocjvVki2XR35weJ9mZgQ"
         amount = "amount"
@@ -12,7 +36,6 @@ class TestEscrowCreate(TestCase):
         finish_after = 2
         destination = "destination"
         fee = "0.00001"
-        sequence = 19048
 
         with self.assertRaises(XRPLModelException):
             EscrowCreate(
@@ -22,5 +45,24 @@ class TestEscrowCreate(TestCase):
                 destination=destination,
                 fee=fee,
                 finish_after=finish_after,
+            )
+
+    def test_no_finish(self):
+        account = "r9LqNeG6qHxjeUocjvVki2XR35weJ9mZgQ"
+        amount = "amount"
+        cancel_after = 1
+        destination = "destination"
+        destination_tag = 1
+        fee = "0.00001"
+        sequence = 19048
+
+        with self.assertRaises(XRPLModelException):
+            EscrowCreate(
+                account=account,
+                amount=amount,
+                destination=destination,
+                destination_tag=destination_tag,
+                fee=fee,
+                cancel_after=cancel_after,
                 sequence=sequence,
             )
