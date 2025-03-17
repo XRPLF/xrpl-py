0bf9e2dd36c7f4b677329b42bd29a417cae9f306
Chenna Keshava B S <ckeshavabs@gmail.com>
2025-03-17 14:18:51 -0700

    Fix #810 -- Fix the get_latest_open_ledger_sequence method

--- tests/integration/reqs/test_ledger.py
+++ tests/integration/reqs/test_ledger.py
@@ -1,5 +1,6 @@
 from tests.integration.integration_test_case import IntegrationTestCase
 from tests.integration.it_utils import test_async_and_sync
+from xrpl.asyncio.ledger import get_latest_open_ledger_sequence
 from xrpl.models.requests import Ledger
 
 
@@ -8,3 +9,12 @@ class TestLedger(IntegrationTestCase):
     async def test_basic_functionality(self, client):
         response = await client.request(Ledger())
         self.assertTrue(response.is_successful())
+
+    @test_async_and_sync(
+        globals(),
+        ["xrpl.ledger.get_latest_open_ledger_sequence"],
+        use_testnet=True,
+    )
+    async def test_fetch_current_ledger(self, client):
+        response = await get_latest_open_ledger_sequence(client)
+        self.assertTrue(isinstance(response, int))
