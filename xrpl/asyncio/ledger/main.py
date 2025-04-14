8d3d93225105d73a0dfd94d35c85b679fdc3dcc7
Chenna Keshava B S <ckeshavabs@gmail.com>
2025-04-14 09:58:56 -0700

    get_open_ledger_seq: early return in case of failures

--- xrpl/asyncio/ledger/main.py
+++ xrpl/asyncio/ledger/main.py
@@ -51,18 +51,19 @@ async def get_latest_open_ledger_sequence(client: Client) -> int:
     """
     response = await client._request_impl(Ledger(ledger_index="current"))
 
-    # validate that the `current` ledger is retrieved
-    if response.result["ledger"]["closed"] is True:
-        raise XRPLException("`closed` key found while requesting current ledger")
-    if response.result["validated"] is True:
-        raise XRPLException("`validated` key found while requesting current ledger")
-    if "ledger_index" in response.result:
-        raise XRPLException("`ledger_index` key is not relevant in current ledger")
-    if "ledger_current_index" not in response.result:
-        raise XRPLException(
-            "`ledger_current_index` key must be present in current ledger"
-        )
     if response.is_successful():
+        # validate that the `current` ledger is retrieved
+        if response.result["ledger"]["closed"] is True:
+            raise XRPLException("`closed` key found while requesting current ledger")
+        if response.result["validated"] is True:
+            raise XRPLException("`validated` key found while requesting current ledger")
+        if "ledger_index" in response.result:
+            raise XRPLException("`ledger_index` key is not relevant in current ledger")
+        if "ledger_current_index" not in response.result:
+            raise XRPLException(
+                "`ledger_current_index` key must be present in current ledger"
+            )
+
         return cast(int, response.result["ledger_current_index"])
 
     raise XRPLRequestFailureException(response.result)
