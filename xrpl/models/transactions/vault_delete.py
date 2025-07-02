4e728b5a877ad224b176f32653111e6a6ecd58e9
Chenna Keshava B S <21219765+ckeshava@users.noreply.github.com>
2025-07-02 14:11:50 -0700

    Update xrpl/models/transactions/vault_delete.py
    Co-authored-by: Omar Khan <khancodegt@gmail.com>

--- xrpl/models/transactions/vault_delete.py
+++ xrpl/models/transactions/vault_delete.py
@@ -10,7 +10,7 @@ from xrpl.models.transactions.transaction import Transaction
 from xrpl.models.transactions.types import TransactionType
 from xrpl.models.utils import KW_ONLY_DATACLASS, require_kwargs_on_init
 
-_MAX_VAULT_ID_LENGTH = 32 * 2
+_MAX_VAULT_ID_LENGTH = 64
 
 
 @require_kwargs_on_init
