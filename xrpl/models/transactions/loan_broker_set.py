029d65c275fe3f36acfc0cf4b43c19f3a35906e5
Chenna Keshava B S <ckeshavabs@gmail.com>
2025-09-17 17:01:23 -0700

    add unit tests and validation for loan_broker_set txn

--- xrpl/models/transactions/loan_broker_set.py
+++ xrpl/models/transactions/loan_broker_set.py
@@ -3,7 +3,9 @@
 from __future__ import annotations  # Requires Python 3.7+
 
 from dataclasses import dataclass, field
-from typing import Optional
+from typing import Dict, Optional
+
+from typing_extensions import Self
 
 from xrpl.models.required import REQUIRED
 from xrpl.models.transactions.transaction import Transaction
@@ -60,3 +62,47 @@ class LoanBrokerSet(Transaction):
         default=TransactionType.LOAN_BROKER_SET,
         init=False,
     )
+
+    MAX_DATA_PAYLOAD_LENGTH = 256 * 2
+    MAX_MANAGEMENT_FEE_RATE = 10_000
+    MAX_COVER_RATE_MINIMUM = 100_000
+    MAX_COVER_RATE_LIQUIDATION = 100_000
+    MAX_DEBT_MAXIMUM = 9223372036854775807
+
+    def _get_errors(self: Self) -> Dict[str, str]:
+        errors = super()._get_errors()
+
+        if self.data is not None and len(self.data) > self.MAX_DATA_PAYLOAD_LENGTH:
+            errors["LoanBrokerSet:data"] = "Data must be less than 256 bytes."
+
+        if self.management_fee_rate is not None and (
+            self.management_fee_rate < 0
+            or self.management_fee_rate > self.MAX_MANAGEMENT_FEE_RATE
+        ):
+            errors["LoanBrokerSet:management_fee_rate"] = (
+                "Management fee rate must be between 0 and 10_000 inclusive."
+            )
+
+        if self.cover_rate_minimum is not None and (
+            self.cover_rate_minimum < 0
+            or self.cover_rate_minimum > self.MAX_COVER_RATE_MINIMUM
+        ):
+            errors["LoanBrokerSet:cover_rate_minimum"] = (
+                "Cover rate minimum must be between 0 and 100_000 inclusive."
+            )
+
+        if self.cover_rate_liquidation is not None and (
+            self.cover_rate_liquidation < 0
+            or self.cover_rate_liquidation > self.MAX_COVER_RATE_LIQUIDATION
+        ):
+            errors["LoanBrokerSet:cover_rate_liquidation"] = (
+                "Cover rate liquidation must be between 0 and 100_000 inclusive."
+            )
+        if self.debt_maximum is not None and (
+            int(self.debt_maximum) < 0 or int(self.debt_maximum) > self.MAX_DEBT_MAXIMUM
+        ):
+            errors["LoanBrokerSet:debt_maximum"] = (
+                "Debt maximum must not be negative or greater than 9223372036854775807."
+            )
+
+        return errors
