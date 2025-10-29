b78b1b6b49caf4835d72fe983311e1ad9a715f94
Mayukha Vadari <mvadari@gmail.com>
2025-10-29 13:23:10 -0400

    fix typo

--- tools/generate_definitions.py
+++ tools/generate_definitions.py
@@ -325,7 +325,7 @@ _add_line('  "TRANSACTION_TYPES": {')
 # Example line:
 # TRANSACTION(ttCHECK_CREATE, 16, CheckCreate, ({
 tx_hits = re.findall(
-    r"^ *TRANSACTION\(tt[A-Z_]+[ \n]*,* ([0-9]+)[ \n]*,[ \n]*([A-Za-z]+).*$",
+    r"^ *TRANSACTION\(tt[A-Z_]+[ \n]*,[ \n]*([0-9]+)[ \n]*,[ \n]*([A-Za-z]+).*$",
     transactions_file,
     re.MULTILINE,
 )
