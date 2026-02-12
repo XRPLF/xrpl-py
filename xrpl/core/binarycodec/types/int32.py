c353e9d4924a58f92fc3a4de832bf3b84110e0f8
Mayukha Vadari <mvadari@gmail.com>
2026-02-12 12:10:44 -0500

    improve int32 integration

--- xrpl/core/binarycodec/types/int32.py
+++ xrpl/core/binarycodec/types/int32.py
@@ -1,71 +1,71 @@
-"""Class for serializing and deserializing a signed 32-bit integer."""
+"""Class for serializing and deserializing a signed 32-bit integer.
+See `UInt Fields <https://xrpl.org/serialization.html#int-fields>`_
+"""
 
 from __future__ import annotations
 
-from typing import Optional, Type
+from typing import Optional, Type, Union
 
 from typing_extensions import Final, Self
 
 from xrpl.core.binarycodec.binary_wrappers.binary_parser import BinaryParser
 from xrpl.core.binarycodec.exceptions import XRPLBinaryCodecException
-from xrpl.core.binarycodec.types.serialized_type import SerializedType
+from xrpl.core.binarycodec.types.int import Int
 
 _WIDTH: Final[int] = 4  # 32 / 8
 
 
-class Int32(SerializedType):
-    """Class for serializing and deserializing a signed 32-bit integer."""
+class Int32(Int):
+    """
+    Class for serializing and deserializing a signed 32-bit integer.
+    See `Int Fields <https://xrpl.org/serialization.html#int-fields>`_
+    """
 
     def __init__(self: Self, buffer: bytes = bytes(_WIDTH)) -> None:
         """Construct a new Int32 type from a ``bytes`` value."""
         super().__init__(buffer)
 
-    @property
-    def value(self: Self) -> int:
-        """Get the value of the Int32 represented by `self.buffer`."""
-        return int.from_bytes(self.buffer, byteorder="big", signed=True)
-
     @classmethod
     def from_parser(
         cls: Type[Self], parser: BinaryParser, _length_hint: Optional[int] = None
     ) -> Self:
-        """Construct a new Int32 type from a BinaryParser."""
+        """
+        Construct a new Int32 type from a BinaryParser.
+
+        Args:
+            parser: A BinaryParser to construct a Int32 from.
+
+        Returns:
+            The Int32 constructed from parser.
+        """
         return cls(parser.read(_WIDTH))
 
     @classmethod
-    def from_value(cls: Type[Self], value: int) -> Self:
-        """Construct a new Int32 type from an integer."""
-        if not isinstance(value, int):
+    def from_value(cls: Type[Self], value: Union[str, int]) -> Self:
+        """
+        Construct a new Int32 type from a number.
+
+        Args:
+            value: The number to construct a Int32 from.
+
+        Returns:
+            The Int32 constructed from value.
+
+        Raises:
+            XRPLBinaryCodecException: If a Int32 could not be constructed from value.
+        """
+        if not isinstance(value, (str, int)):
             raise XRPLBinaryCodecException(
-                f"Invalid type to construct Int32: expected int, "
-                f"received {value.__class__.__name__}."
+                "Invalid type to construct a Int32: expected str or int,"
+                " received {value.__class__.__name__}."
             )
-        return cls(value.to_bytes(_WIDTH, byteorder="big", signed=True))
 
-    def to_json(self: Self) -> int:
-        """Convert the Int32 to JSON (returns the integer value)."""
-        return self.value
+        if isinstance(value, int):
+            value_bytes = (value).to_bytes(_WIDTH, byteorder="big", signed=True)
+            return cls(value_bytes)
 
-    def __eq__(self: Self, other: object) -> bool:
-        """Determine whether two Int32 objects are equal."""
-        if isinstance(other, int):
-            return self.value == other
-        if isinstance(other, Int32):
-            return self.value == other.value
-        return NotImplemented
+        if isinstance(value, str) and value.isdigit():
+            value_bytes = (int(value)).to_bytes(_WIDTH, byteorder="big", signed=True)
+            return cls(value_bytes)
 
-    def __lt__(self: Self, other: object) -> bool:
-        """Determine whether this Int32 is less than another."""
-        if isinstance(other, int):
-            return self.value < other
-        if isinstance(other, Int32):
-            return self.value < other.value
-        return NotImplemented
-
-    def __gt__(self: Self, other: object) -> bool:
-        """Determine whether this Int32 is greater than another."""
-        if isinstance(other, int):
-            return self.value > other
-        if isinstance(other, Int32):
-            return self.value > other.value
-        return NotImplemented
+        raise XRPLBinaryCodecException("Cannot construct Int32 from given value")
